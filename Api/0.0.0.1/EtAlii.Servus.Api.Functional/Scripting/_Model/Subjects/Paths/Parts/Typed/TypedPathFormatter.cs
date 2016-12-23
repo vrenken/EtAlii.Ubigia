0746adcac3275a40e716f2c548605750dceac754
Peter Vrenken <vrenken@live.nl>
2016-12-23 15:36:10 +0000

    Corrected Regex to really match letters only for Words and first and last names Imported from Changeset 4368

--- Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/_Model/Subjects/Paths/Parts/Typed/TypedPathFormatter.cs
+++ Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/_Model/Subjects/Paths/Parts/Typed/TypedPathFormatter.cs
@@ -24,7 +24,7 @@
         public static class Text
         {
             public static TypedPathFormatter WordFormatter => _wordFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _wordFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("WORD", @"^\w+$"));
+            private static readonly Lazy<TypedPathFormatter> _wordFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("WORD", @"^\p{L}+$"));
             public static TypedPathFormatter NumberFormatter => _numberFormatter.Value;
             private static readonly Lazy<TypedPathFormatter> _numberFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("NUMBER", @"^[0123456789]+$"));
         }
@@ -32,9 +32,9 @@
         public static class Name
         {
             public static TypedPathFormatter FirstNameFormatter => _firstNameFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _firstNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("FIRSTNAME", @"^\w+$"));
+            private static readonly Lazy<TypedPathFormatter> _firstNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("FIRSTNAME", @"^\p{L}+$"));
             public static TypedPathFormatter LastNameFormatter => _lastNameFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _lastNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("LASTNAME", @"^\w+$"));
+            private static readonly Lazy<TypedPathFormatter> _lastNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("LASTNAME", @"^\p{L}+$"));
         }
 
         public static TypedPathFormatter[] All => _all.Value;
