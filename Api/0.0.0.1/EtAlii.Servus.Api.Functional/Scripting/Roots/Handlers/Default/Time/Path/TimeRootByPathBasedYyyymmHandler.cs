de703d92712d4aed18bb9a5501f619e3573ccbda
Peter Vrenken <vrenken@live.nl>
2016-12-28 19:59:13 +0000

    Working time based root handlers Imported from Changeset 4388

--- Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/Roots/Handlers/Default/Time/Path/TimeRootByPathBasedYyyymmHandler.cs
+++ Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/Roots/Handlers/Default/Time/Path/TimeRootByPathBasedYyyymmHandler.cs
@@ -3,14 +3,17 @@ namespace EtAlii.Servus.Api.Functional
     using System;
     using System.Linq;
 
-    public class TimeRootByPathBasedYyyymmHandler : IRootHandler
+    internal class TimeRootByPathBasedYyyymmHandler : IRootHandler
     {
 
         public PathSubjectPart[] Template { get { return _template; } }
         private readonly PathSubjectPart[] _template;
+        private readonly ITimePreparer _timePreparer;
 
-        public TimeRootByPathBasedYyyymmHandler()
+        public TimeRootByPathBasedYyyymmHandler(ITimePreparer timePreparer)
         {
+            _timePreparer = timePreparer;
+
             _template = new PathSubjectPart[] {
                     new TypedPathSubjectPart(TypedPathFormatter.Time.YearFormatter), new IsParentOfPathSubjectPart(),
                     new TypedPathSubjectPart(TypedPathFormatter.Time.MonthFormatter)
@@ -19,11 +22,27 @@ namespace EtAlii.Servus.Api.Functional
 
         public void Process(IRootContext context, PathSubjectPart[] match, PathSubjectPart[] rest, ExecutionScope scope, IObserver<object> output)
         {
-            var parts = new PathSubjectPart[] { new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart("Time"), new IsParentOfPathSubjectPart() }
-                .Concat(match)
-                .Concat(rest)
-                .ToArray();
+            var year = Int32.Parse(match[0].ToString());
+            var month = Int32.Parse(match[2].ToString());
+
+            var time = new DateTime(year, month, 1);
+            _timePreparer.Prepare(context, scope, time);
+
+            var parts = new PathSubjectPart[] 
+                {
+                    new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart("Time"),
+                    new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(year.ToString("D4")),
+                    new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(month.ToString("D2")),
+                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // day
+                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // hour
+                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // minute
+                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // second
+                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // millisecond
+                }
+                 .Concat(rest)
+                 .ToArray();
             var path = new AbsolutePathSubject(parts);
+
             context.Converter.Convert(path, scope, output);
         }
     }
--- Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/Roots/Handlers/Default/Time/Regex/Combined/TimeRootByRegexBasedYyyymmddhhmmssmmmHandler.cs
+++ Api/0.0.0.1/EtAlii.Servus.Api.Functional/Scripting/Roots/Handlers/Default/Time/Regex/Combined/TimeRootByRegexBasedYyyymmddhhmmssmmmHandler.cs
@@ -3,30 +3,32 @@ namespace EtAlii.Servus.Api.Functional
     using System;
     using System.Linq;
 
-    internal class TimeRootByRegexBasedSeparatedYyyymmddhhmmHandler : IRootHandler
+    internal class TimeRootByRegexBasedYyyymmddhhmmssmmmHandler : IRootHandler
     {
 
         public PathSubjectPart[] Template { get { return _template; } }
         private readonly PathSubjectPart[] _template;
         private readonly ITimePreparer _timePreparer;
 
-        public TimeRootByRegexBasedSeparatedYyyymmddhhmmHandler(ITimePreparer timePreparer)
+        public TimeRootByRegexBasedYyyymmddhhmmssmmmHandler(ITimePreparer timePreparer)
         {
             _timePreparer = timePreparer;
 
-            _template = new PathSubjectPart[] { new RegexPathSubjectPart(@"\d{4}-\d{2}-\d{2} \d{2}:\d{2}") };
+            _template = new PathSubjectPart[] { new RegexPathSubjectPart(@"\d{4}\d{2}\d{2}\d{2}\d{2}\d{2}\d3") };
         }
 
         public void Process(IRootContext context, PathSubjectPart[] match, PathSubjectPart[] rest, ExecutionScope scope, IObserver<object> output)
         {
             var timeString = match[0].ToString();
             var year = Int32.Parse(timeString.Substring(0, 4));
-            var month = Int32.Parse(timeString.Substring(5, 2));
-            var day = Int32.Parse(timeString.Substring(8, 2));
-            var hour = Int32.Parse(timeString.Substring(11, 2));
-            var minute = Int32.Parse(timeString.Substring(14, 2));
+            var month = Int32.Parse(timeString.Substring(4, 2));
+            var day = Int32.Parse(timeString.Substring(6, 2));
+            var hour = Int32.Parse(timeString.Substring(8, 2));
+            var minute = Int32.Parse(timeString.Substring(10, 2));
+            var second = Int32.Parse(timeString.Substring(12, 2));
+            var millisecond = Int32.Parse(timeString.Substring(14, 3));
 
-            var time = new DateTime(year, month, day, hour, minute, 0, 0);
+            var time = new DateTime(year, month, day, hour, minute, second, millisecond);
             _timePreparer.Prepare(context, scope, time);
 
             var parts = new PathSubjectPart[]
@@ -37,8 +39,8 @@ namespace EtAlii.Servus.Api.Functional
                     new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(day.ToString("D2")),
                     new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(hour.ToString("D2")),
                     new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(minute.ToString("D2")),
-                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // second
-                    new IsParentOfPathSubjectPart(), new WildcardPathSubjectPart("*"), // millisecond
+                    new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(second.ToString("D2")),
+                    new IsParentOfPathSubjectPart(), new ConstantPathSubjectPart(millisecond.ToString("D3")),
                 }
                 .Concat(rest)
                 .ToArray();
