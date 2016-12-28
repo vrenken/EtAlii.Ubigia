301f0de0a3d70278787cf4241a7477df47edb74b
Peter Vrenken <vrenken@live.nl>
2016-12-28 22:42:27 +0000

    Removed obsolete test methods Imported from Changeset 4393

--- Api/0.0.0.1/EtAlii.Servus.Api.Shared.Tests/1. Logical/ILogicalTestContext.cs
+++ Api/0.0.0.1/EtAlii.Servus.Api.Shared.Tests/1. Logical/ILogicalTestContext.cs
@@ -8,12 +8,6 @@ namespace EtAlii.Servus.Api.Logical.Tests
         Task AddDays(ILogicalContext context, IEditableEntry monthEntry, int days);
         Task<IEditableEntry> CreateHierarchy(ILogicalContext context, IEditableEntry parent, params string[] hierarchy);
         Task<TimeAddResult> AddYearMonth(ILogicalContext context);
-        Task AddTime(ILogicalContext context, int year, int month, int day, int hour, int minute, int second);
-        Task AddTime(ILogicalContext context, int year, int month, int day, int hour, int minute);
-        Task AddTime(ILogicalContext context, int year, int month, int day, int hour);
-        Task AddTime(ILogicalContext context, int year, int month, int day);
-        Task AddTime(ILogicalContext context, int year, int month);
-        Task AddTime(ILogicalContext context, int year);
 
         Task<string> AddYearMonthDayHourMinute(ILogicalContext logicalContext);
 
