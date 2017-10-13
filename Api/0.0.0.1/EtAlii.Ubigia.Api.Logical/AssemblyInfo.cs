46c7192dc569cd83c0dd94a180f4488379ddf7c3
Peter Vrenken <peter.vrenken@atos.net>
2017-10-13 23:56:10 +0200

    Made logical api projects netstandard2.0

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32")]
+
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Logical.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32.Tests")]
+
+[assembly: InternalsVisibleTo("SpaceBrowser")]
+
+
+
+
