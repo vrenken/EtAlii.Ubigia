fd8f1f7de990530c764cf5d08f96ce14a7f09597
Peter Vrenken <peter.vrenken@atos.net>
2017-10-17 09:52:20 +0200

    migrate to netstandard2.0

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Diagnostics")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Transport.SignalR.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Transport.WebApi.Tests")]
