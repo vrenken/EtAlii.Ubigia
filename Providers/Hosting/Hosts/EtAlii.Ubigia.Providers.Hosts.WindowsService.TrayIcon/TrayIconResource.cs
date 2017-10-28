700833205bf53f065e61815568dfd73d2b81ee12
Peter Vrenken <peter.vrenken@atos.net>
2017-10-29 00:59:34 +0200

    improvements to provider hosting

--- Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.TrayIcon/TrayIconResource.cs
+++ Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.TrayIcon/TrayIconResource.cs
@@ -2,8 +2,8 @@
 {
     public static class TrayIconResource
     {
-        public const string Stopped = "/Icon-Logo-Black.ico";
-        public const string Errored = "/Icon-Logo-Red.ico";
-        public const string Running = "/Icon-Logo-White-Shaded.ico";
+        public const string Stopped = "Icon-Logo-Black.ico";
+        public const string Errored = "Icon-Logo-Red.ico";
+        public const string Running = "Icon-Logo-White-Shaded.ico";
     }
 }
--- Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.TrayIcon/_Scaffolding/TrayIconProviderScaffolding.cs
+++ Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.TrayIcon/_Scaffolding/TrayIconProviderScaffolding.cs
@@ -8,11 +8,6 @@
         {
             container.Register<ITaskbarIconViewModel, TaskbarIconViewModel>();
             container.Register<ITaskbarIcon, TaskbarIcon>();
-            container.RegisterInitializer<ITaskbarIcon>(taskbarIcon =>
-            {
-                taskbarIcon.DataContext = container.GetInstance<ITaskbarIconViewModel>();
-            });
-
             container.RegisterInitializer<IProviderHost>(provider =>
             {
                 container.GetInstance<ITaskbarIconViewModel>().Initialize((ITrayIconProviderHost)provider);
