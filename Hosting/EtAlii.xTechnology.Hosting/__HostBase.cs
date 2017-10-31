9cb5fa66d9959d4241b201c622aac170c8ba6824
Peter Vrenken <peter.vrenken@atos.net>
2017-10-31 20:10:56 +0100

    Removed hostbase

---
+++ Hosting/EtAlii.xTechnology.Hosting/__HostBase.cs
@@ -0,0 +1,28 @@
+﻿//namespace EtAlii.xTechnology.Hosting
+//{
+
+//    public abstract class HostBase : IHost
+//    {
+//        private readonly IServiceManager _serviceManager;
+
+//        public IHostConfiguration Configuration { get; }
+
+//        protected HostBase(
+//            IHostConfiguration configuration, 
+//            IServiceManager serviceManager)
+//        {
+//            Configuration = configuration;
+//            _serviceManager = serviceManager;
+//        }
+
+//        public virtual void Start()
+//        {
+//            _serviceManager.Start();
+//        }
+
+//        public virtual void Stop()
+//        {
+//            _serviceManager.Stop();
+//        }
+//    }
+//}
