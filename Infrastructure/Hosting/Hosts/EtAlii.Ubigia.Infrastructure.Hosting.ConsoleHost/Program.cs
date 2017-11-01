1593c901d2b02b479d133b079a3037efba9e9f91
Peter Vrenken <peter.vrenken@atos.net>
2017-11-02 00:24:41 +0100

    Start of split of hosts

---
+++ Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.ConsoleHost/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.ConsoleHost
+{
+    using System;
+    using System.Configuration;
+    using EtAlii.Ubigia.Infrastructure.Fabric;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using EtAlii.Ubigia.Infrastructure.Hosting;
+    using EtAlii.Ubigia.Infrastructure.Logical;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.SignalR;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Api.Admin;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Api.User;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Diagnostics;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Portal.Admin;
+    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Portal.User;
+    using EtAlii.Ubigia.Storage;
+    using EtAlii.xTechnology.Diagnostics;
+    using EtAlii.xTechnology.Hosting;
+
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application. 
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Starting Ubigia infrastructure...");
+
+            var diagnostics = new DiagnosticsFactory().CreateDisabled("EtAlii", "EtAlii.Ubigia.Infrastructure");
+
+            var exeConfiguration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            // Create a storage instance.
+            var storageConfigurationSection = (IStorageConfigurationSection)exeConfiguration.GetSection("ubigia/storage");
+            var storageConfiguration = storageConfigurationSection
+                .ToStorageConfiguration();
+            var storage = new StorageFactory().Create(storageConfiguration);
+
+            // Fetch the Infrastructure configuration.
+            var infrastructureConfigurationSection = (IInfrastructureConfigurationSection)exeConfiguration.GetSection("ubigia/infrastructure");
+            var infrastructureConfiguration = infrastructureConfigurationSection.ToInfrastructureConfiguration();
+
+            // Create fabric instance.
+            var fabricConfiguration = new FabricContextConfiguration()
+                .Use(storage);
+            var fabric = new FabricContextFactory().Create(fabricConfiguration);
+
+            // Create logical context instance.
+            var logicalConfiguration = new LogicalContextConfiguration()
+                .Use(fabric)
+                .Use(infrastructureConfiguration.Name, infrastructureConfiguration.Address);
+            var logicalContext = new LogicalContextFactory().Create(logicalConfiguration);
+
+            // Create a Infrastructure instance.
+            infrastructureConfiguration = infrastructureConfiguration
+                .UseWebApi(diagnostics) // TODO: Web API usage should also be configured in the configuration section.
+                .UseWebApiAdminApi()
+                .UseWebApiAdminPortal()
+                .UseWebApiUserApi()
+                .UseWebApiUserPortal()
+                .UseSignalRApi()
+                .Use(logicalContext);
+            var infrastructure = new InfrastructureFactory().Create(infrastructureConfiguration);
+
+            // Create a host instance.
+            var hostConfigurationSection = (IHostConfigurationSection)exeConfiguration.GetSection("ubigia/host");
+            var hostConfiguration = hostConfigurationSection.ToHostConfiguration()
+                .UseInfrastructure(storage, infrastructure);
+
+            EtAlii.xTechnology.Hosting.Program.Start(hostConfiguration);
+        }
+    }
+}
