7c3869819a0163c73ad62a085e405c102b4516f0
Peter Vrenken <peter.vrenken@atos.net>
2017-11-05 12:01:56 +0100

    Start of menuitem extraction

---
+++ Hosting/EtAlii.xTechnology.Hosting.TrayIconHost/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace EtAlii.xTechnology.Hosting
+{
+    using System.Windows.Input;
+    using System.ComponentModel;
+    using EtAlii.xTechnology.Mvvm;
+
+    public class MenuItemViewModel : BindableBase, INotifyPropertyChanged
+    {
+        public ICommand Command { get; }
+
+        public string Header { get; }
+        public MenuItemViewModel[] Items { get; }
+
+        public MenuItemViewModel(string header, MenuItemViewModel[] items = null)
+        {
+            Header = header;
+            Items = items ?? new MenuItemViewModel[0];
+        }
+        public MenuItemViewModel(string header, ICommand command)
+        {
+            Command = command;
+            Header = header;
+            Items = new MenuItemViewModel[0];
+        }
+
+    }
+}
