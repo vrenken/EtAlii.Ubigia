f0257b1f3be416ead9d07717f820aab2e8223b31
Peter Vrenken <peter.vrenken@atos.net>
2017-11-09 21:23:49 +0100

    Corrected namespaces

--- Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/ServiceLogic.cs
+++ Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/ServiceLogic.cs
@@ -3,19 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Configuration;
-    using EtAlii.Ubigia.Infrastructure.Fabric;
     using EtAlii.Ubigia.Infrastructure.Functional;
-    using EtAlii.Ubigia.Infrastructure.Hosting;
-    using EtAlii.Ubigia.Infrastructure.Logical;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.SignalR;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Api.Admin;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Api.User;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Diagnostics;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Portal.Admin;
-    using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Portal.User;
-    using EtAlii.Ubigia.Storage;
-    using EtAlii.xTechnology.Diagnostics;
-    using EtAlii.xTechnology.Hosting;
+    using EtAlii.Ubigia.Infrastructure.Hosting.Owin;
 
     public class ServiceLogic : IServiceLogic
     {
