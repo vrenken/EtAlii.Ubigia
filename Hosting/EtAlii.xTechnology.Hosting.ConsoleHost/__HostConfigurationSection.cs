bfbdd7adc00f1b73a4aa332e48100b627738afec
Peter Vrenken <peter.vrenken@atos.net>
2017-11-12 14:18:50 +0100

    Removed hostconfigurationsection

---
+++ Hosting/EtAlii.xTechnology.Hosting.ConsoleHost/__HostConfigurationSection.cs
@@ -0,0 +1,13 @@
+﻿//namespace EtAlii.xTechnology.Hosting
+//{
+//    using System.Configuration;
+
+//    public class HostConfigurationSection : ConfigurationSection, IHostConfigurationSection
+//    {
+//        public IHostConfiguration ToHostConfiguration()
+//        {
+//            var configuration = new HostConfiguration();
+//            return configuration;
+//        }
+//    }
+//}
