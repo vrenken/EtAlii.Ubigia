d07bde6f495f323cf3103980f55983087afe25bf
Peter Vrenken <peter.vrenken@atos.net>
2017-11-12 14:26:25 +0100

    Move of webapplicationmanager into transport layer

--- Infrastructure/EtAlii.Ubigia.Infrastructure.Transport.Win32/IApplicationManager.cs
+++ Infrastructure/EtAlii.Ubigia.Infrastructure.Transport.Win32/IApplicationManager.cs
@@ -1,6 +1,6 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Owin
 {
-    public interface IApplicationManager
+    public interface IApplicationManager 
     {
         void Start(IComponentManager[] componentManagers);
         void Stop(IComponentManager[] componentManagers);
