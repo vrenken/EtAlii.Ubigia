153767857acef854318dfabacf6d1dd69ab1056d
Peter Vrenken <peter.vrenken@atos.net>
2017-11-12 10:35:53 +0100

    Made xTechnology.WindowsServiceHost generic

--- Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/IHostConfiguration.WindowsService.cs
+++ Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/IHostConfiguration.WindowsService.cs
@@ -3,7 +3,12 @@ namespace EtAlii.xTechnology.Hosting
 
     public static class IHostConfigurationWindowsServiceHostExtension
     {
-        public static IHostConfiguration UseWindowsServiceHost(this IHostConfiguration configuration)
+        public static IHostConfiguration UseWindowsServiceHost(
+            this IHostConfiguration configuration,
+            string shortServiceNameFormat, // Something like: "UIS${0}" Ubigia Infrastructure Service
+            string displayNameFormat, // example: "Ubigia Infrastructure Service ({0})"
+            string descriptionFormat // e.g. "Provides applications access to the Ubigia storage '{0}'"
+            )
         {
             var extensions = new IHostExtension[]
             {
--- Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/ServiceLogic.cs
+++ Hosting/EtAlii.xTechnology.Hosting.WindowsServiceHost/ServiceLogic.cs
@@ -3,15 +3,13 @@
     using System;
     using System.Collections.Generic;
     using System.Configuration;
-    using EtAlii.Ubigia.Infrastructure.Functional;
-    using EtAlii.Ubigia.Infrastructure.Hosting.Owin;
 
     public class ServiceLogic : IServiceLogic
     {
-        private const string _nameFormat = "UIS${0}"; // Ubigia Infrastructure Service
-        private const string _displayNameFormat = "Ubigia Infrastructure Service ({0})";
-        private const string _descriptionFormat = "Provides applications access to the Ubigia storage '{0}'";
-        
+        //private const string _nameFormat = "UIS${0}"; // Ubigia Infrastructure Service
+        //private const string _displayNameFormat = "Ubigia Infrastructure Service ({0})";
+        //private const string _descriptionFormat = "Provides applications access to the Ubigia storage '{0}'";
+
 
         // SERVINFSERV$SQLEXPRESS
         public string Name { get; }
@@ -22,18 +20,11 @@
 
         private readonly IHost _host;
 
-        public ServiceLogic()
+        public ServiceLogic(IHostConfiguration configuration)
         {
-            var exeConfiguration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            var configuration = new HostConfigurationBuilder().Build(sectionName => exeConfiguration.GetSection(sectionName), out IInfrastructure infrastructure);
-
             _host = new HostFactory<WindowsServiceHost>().Create(configuration);
-
-            var infrastructureName = infrastructure.Configuration.Name;
-            Name = String.Format(_nameFormat, infrastructureName).Replace(" ","_");
-            DisplayName = String.Format(_displayNameFormat, infrastructureName);
-            Description = String.Format(_descriptionFormat, infrastructureName);
         }
+
 
         public void Start(IEnumerable<string> args)
         {
