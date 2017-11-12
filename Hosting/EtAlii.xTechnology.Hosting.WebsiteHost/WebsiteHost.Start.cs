1ec34e7dfd36d6c74eb7666b3273fb8763e944a3
Peter Vrenken <peter.vrenken@atos.net>
2017-11-12 14:19:44 +0100

    Restructured websitehost

---
+++ Hosting/EtAlii.xTechnology.Hosting.WebsiteHost/WebsiteHost.Start.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Configuration;
+using EtAlii.xTechnology.Hosting;
+using Microsoft.Owin;
+
+//[assembly: OwinStartup(typeof(Startup))]
+
+namespace EtAlii.xTechnology.Hosting
+{
+    using global::Owin;
+
+    public partial class WebsiteHost
+    {
+        public static void Start(IHostConfiguration configuration)
+        {
+            // To use in webapplication
+            //var applicationManager = new ExistingApplicationManager(application);
+            //var configuration = new HostConfigurationBuilder()
+            //    .Build(sectionName => WebConfigurationManager.GetWebApplicationSection(sectionName), applicationManager)
+            //    .UseWebsiteHost();
+            //WebsiteHost.Start(configuration);
+
+            var host = new HostFactory<WebsiteHost>().Create(configuration);
+
+            // Start hosting both the infrastructure and the storage.
+            host.Start();
+
+        }
+    }
+}
