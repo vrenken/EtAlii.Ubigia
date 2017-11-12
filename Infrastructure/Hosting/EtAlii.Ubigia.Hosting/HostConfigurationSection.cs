27a55db03a58da6df142b2c32996858b579ee7b1
Peter Vrenken <peter.vrenken@atos.net>
2017-11-12 23:08:13 +0100

    Removed some hosting project from core ubigia solution

---
+++ Infrastructure/Hosting/EtAlii.Ubigia.Hosting/HostConfigurationSection.cs
@@ -0,0 +1,14 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Owin
+{
+    using System.Configuration;
+    using EtAlii.xTechnology.Hosting;
+
+    public class HostConfigurationSection : ConfigurationSection, IHostConfigurationSection
+    {
+        public IHostConfiguration ToHostConfiguration()
+        {
+            var configuration = new HostConfiguration();
+            return configuration;
+        }
+    }
+}
