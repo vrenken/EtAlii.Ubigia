18c65c286f1dc7f92f82da8135503b48b075b92e
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 00:56:27 +0100

    Revert "Removed old namespace"
    This reverts commit 60422cbad92022eeea0aab906e7de9aba7a26790.

---
+++ Infrastructure/Hosting/EtAlii.Ubigia.Hosting.Local.Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Local.UnitTests
+{
+    using EtAlii.Ubigia.Api.Transport;
+    using Xunit;
+
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var factory = new DataConnectionFactory();
+            //var connection = factory.CreateLocal();
+        }
+    }
+}
