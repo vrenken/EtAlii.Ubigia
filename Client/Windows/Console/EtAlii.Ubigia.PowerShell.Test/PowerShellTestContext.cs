1097fc3f590fefc99f66465129a4dfe462f9d2be
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 00:50:04 +0100

    Removed obsolete code

--- Client/Windows/Console/EtAlii.Ubigia.PowerShell.Test/PowerShellTestContext.cs
+++ Client/Windows/Console/EtAlii.Ubigia.PowerShell.Test/PowerShellTestContext.cs
@@ -9,7 +9,6 @@
     using EtAlii.Ubigia.Infrastructure.Fabric;
     using EtAlii.Ubigia.Infrastructure.Functional;
     using EtAlii.Ubigia.Infrastructure.Hosting;
-    using EtAlii.Ubigia.Infrastructure.Hosting.Owin;
     using EtAlii.Ubigia.Infrastructure.Logical;
     using EtAlii.Ubigia.Infrastructure.Transport.Owin.SignalR;
     using EtAlii.Ubigia.Infrastructure.Transport.Owin.WebApi.Api.Admin;
