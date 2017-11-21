b97c49a55241e20ed334de757072c995e2b077d1
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 00:50:04 +0100

    Removed obsolete code

--- Client/Windows/Console/EtAlii.Ubigia.PowerShell.Test/PowerShellTestHost.cs
+++ Client/Windows/Console/EtAlii.Ubigia.PowerShell.Test/PowerShellTestHost.cs
@@ -1,8 +1,6 @@
 ﻿namespace EtAlii.Ubigia.Hosting
 {
     using EtAlii.xTechnology.Logging;
-    using System;
-    using System.Threading.Tasks;
     using EtAlii.Ubigia.Infrastructure.Functional;
     using EtAlii.xTechnology.Hosting;
 
@@ -12,7 +10,6 @@
 
         public IInfrastructure Infrastructure { get; }
 
-        private readonly IServiceManager _serviceManager;
         private readonly ILogger _logger;
 
         public PowerShellTestHost(
@@ -20,48 +17,11 @@
             IHostConfiguration configuration,
             IInfrastructure infrastructure,
             ILogger logger)
+            : base(serviceManager)
         {
             Configuration = configuration;
             Infrastructure = infrastructure;
-            _serviceManager = serviceManager;
             _logger = logger;
-        }
-
-        public void Start()
-        {
-            State = HostState.Starting;
-            try
-            {
-                _serviceManager.Start();
-                State = HostState.Running;
-            }
-            catch (Exception e)
-            {
-                State = HostState.Error;
-                _logger.Critical("Fatal exception in infrastructure hosting", e);
-                _logger.Info("Restarting infrastructure hosting");
-                Task.Delay(2000);
-                Stop();
-                Task.Delay(1000);
-                Start();
-            }
-        }
-        public void Stop()
-        {
-            State = HostState.Stopping;
-
-            _serviceManager.Stop();
-
-            // End logging.
-            //Logger.EndSession(); // Disabled because of performance loss.
-
-            State = HostState.Stopped;
-        }
-        public void Shutdown()
-        {
-            Stop();
-
-            State = HostState.Shutdown;
         }
     }
 }
