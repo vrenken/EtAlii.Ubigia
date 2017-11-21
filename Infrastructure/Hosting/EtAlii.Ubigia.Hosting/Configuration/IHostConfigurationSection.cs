8040166687c3a2ef56e7c6af1408735095e8eacb
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 00:49:22 +0100

    Removed old files

--- Infrastructure/Hosting/EtAlii.Ubigia.Hosting/Configuration/IHostConfigurationSection.cs
+++
@@ -1,9 +0,0 @@
-namespace EtAlii.Ubigia.Infrastructure.Hosting.Owin
-{
-    using EtAlii.xTechnology.Hosting;
-
-    public interface IHostConfigurationSection
-    {
-        IHostConfiguration ToHostConfiguration();
-    }
-}
