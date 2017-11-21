caaaa17e8c3f55bdd923610e7edaf74e3d1c663e
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 00:53:26 +0100

    Renamed Owin hosting project

---
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Owin/Commands/HostCommandsFactory.cs
@@ -0,0 +1,46 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Owin
+{
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using EtAlii.xTechnology.Hosting;
+    using EtAlii.xTechnology.MicroContainer;
+
+    public class HostCommandsFactory
+    {
+        public HostCommandsFactory()
+        {
+        }
+
+        public IHostCommand[] Create(IInfrastructure infrastructure)
+        {
+            var container = new Container();
+            container.Register<IProcessStarter, ProcessStarter>();
+            container.Register<IWebsiteBrowser, WebsiteBrowser>();
+            container.Register<IInfrastructure>(() => infrastructure);
+
+            container.Register<IStartSpaceBrowserCommand, StartSpaceBrowserCommand>();
+            container.Register<IStartStorageBrowserCommand, StartStorageBrowserCommand>();
+
+            container.Register<IOpenAdminPortalCommand, OpenAdminPortalCommand>();
+            container.Register<IOpenUserPortalCommand, OpenUserPortalCommand>();
+
+            container.Register<IStartHostCommand, StartHostCommand>();
+            container.Register<IStopHostCommand, StopHostCommand>();
+            container.Register<IShutdownHostCommand, ShutdownHostCommand>();
+            container.Register<IConfigureFirewallRulesCommand, ConfigureFirewallRulesCommand>();
+            
+            return new IHostCommand[]
+            {
+                container.GetInstance<IStartHostCommand>(),
+                container.GetInstance<IStopHostCommand>(),
+                container.GetInstance<IConfigureFirewallRulesCommand>(),
+
+                container.GetInstance<IStartSpaceBrowserCommand>(),
+                container.GetInstance<IStartStorageBrowserCommand>(),
+                container.GetInstance<IOpenAdminPortalCommand>(),
+                container.GetInstance<IOpenUserPortalCommand>(),
+
+                container.GetInstance<IShutdownHostCommand>(),
+            };
+        }
+    }
+}
