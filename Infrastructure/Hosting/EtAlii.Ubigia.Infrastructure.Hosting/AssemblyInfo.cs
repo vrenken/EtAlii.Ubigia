5126438e5d21c3cc5679671c1125a11b02353335
Peter Vrenken <peter.vrenken@atos.net>
2017-11-22 01:00:51 +0100

    Revert "Revert "Removed old namespace""
    This reverts commit a306e0e332bccd70840ec01c2dbba45b6df831f0.

---
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Hosting.Tests")]
