1533bab3a7ba1c0d198ba5ba889cb8f1a5331f43
Peter Vrenken <vrenken@live.nl>
2017-12-28 00:00:06 +0100

    Added Asp.NET Core console host

---
+++ Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.ConsoleHost.AspNetCore/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.ConsoleHost
+{
+    using Microsoft.Extensions.Configuration;
+    using EtAlii.xTechnology.Hosting.AspNetCore;
+    using EtAlii.xTechnology.Hosting;
+
+
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application. 
+        /// </summary>
+        public static void Main()
+        {
+            var applicationConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("settings.json", optional: true)
+                //.AddXmlFile("app.config")
+                .Build();
+
+            var hostConfiguration = new HostConfigurationBuilder()
+                .Build(applicationConfiguration)
+                .UseConsoleHost();
+
+            ConsoleHost.Start(hostConfiguration);
+        }
+    }
+}
