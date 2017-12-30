401b153ecaa38dbaaac5f150fa595d0b83598067
Peter Vrenken <vrenken@live.nl>
2017-12-30 23:48:20 +0100

    Added debug routes controllers

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.AspNetCore/Controllers/AdminPortalController.cs
@@ -0,0 +1,8 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.AspNetCore
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public abstract class AdminPortalController : Controller
+    {
+    }
+}
