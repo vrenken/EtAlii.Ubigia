10515db0916098a11931fee069cae321a5ad0740
Peter Vrenken <vrenken@live.nl>
2017-12-30 23:49:44 +0100

    cnfigured user portal service and added base controller

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Portal.AspNetCore/Controllers/UserPortalController.cs
@@ -0,0 +1,8 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Portal.AspNetCore
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public abstract class UserPortalController : Controller
+    {
+    }
+}
