b22bf5cdedad536014b5eeb28d26ee1f6c5fa95f
Peter Vrenken <vrenken@live.nl>
2017-12-31 01:13:48 +0100

    Small rename

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.AspNetCore/Controllers/AdminPortalControllerService.cs
@@ -0,0 +1,27 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.AspNetCore
+{
+    using System.Diagnostics;
+    using System.Linq;
+    using EtAlii.Ubigia.Infrastructure.Transport.AspNetCore;
+    using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class AdminPortalControllerService : AspNetCoreServiceBase
+    {
+        public AdminPortalControllerService(IConfigurationSection configuration) 
+            : base(configuration)
+        {
+        }
+
+        protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.Admin.Portal.BaseUrl,
+                services => services.AddMvcForTypedController<AdminPortalController>(),
+                appBuilder => appBuilder.UseWelcomePage().UseMvc());
+        }
+    }
+}
