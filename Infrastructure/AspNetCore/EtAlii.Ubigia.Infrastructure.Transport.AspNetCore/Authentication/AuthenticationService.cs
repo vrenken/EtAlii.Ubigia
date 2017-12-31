d291d47ab48bf3251221cc7cf5af0302b33c3792
Peter Vrenken <vrenken@live.nl>
2017-12-31 15:14:26 +0100

    Committing files that won't change for a while

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/AuthenticationService.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/AuthenticationService.cs
@@ -1,20 +1,58 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
 {
+    using System.Linq;
+    using System.Text;
+    using EtAlii.Ubigia.Infrastructure.Functional;
     using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
 
-    public class AuthenticationService : ServiceBase
+    public class AuthenticationService : AspNetCoreServiceBase, IAuthenticationService
     {
-        public override void Start()
+        private readonly IConfigurationSection _configuration;
+
+        public AuthenticationService(IConfigurationSection configuration) : base(configuration)
         {
+            _configuration = configuration;
         }
 
-        public override void Stop()
+        protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
         {
-        }
+            var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
 
-        protected override void Initialize(IHost host, ISystem system, IModule[] moduleChain, out Status status)
-        {
-            status = new Status(nameof(AuthenticationService));
+            // Source: https://auth0.com/blog/securing-asp-dot-net-core-2-applications-with-jwts/
+            applicationBuilder.UseBranchWithServices(Port, "/authenticate",
+                services =>
+                {
+                    services
+                        .AddSingleton<IConfigurationSection>(_configuration)
+                        .AddSingleton<IAccountRepository>(infrastructure.Accounts)
+                        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(options =>
+                        {
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuer = true,
+                                ValidateAudience = true,
+                                ValidateLifetime = true,
+                                ValidateIssuerSigningKey = true,
+                                ValidIssuer = _configuration["Jwt:Issuer"],
+                                ValidAudience = _configuration["Jwt:Issuer"],
+                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
+                            };
+                        });
+                    services
+                        .AddMvcForTypedController<AuthenticateController>();
+                },
+                appBuilder =>
+                {
+                    appBuilder
+                        .UseAuthentication()
+                        .UseMvc();
+                });
         }
     }
 }
