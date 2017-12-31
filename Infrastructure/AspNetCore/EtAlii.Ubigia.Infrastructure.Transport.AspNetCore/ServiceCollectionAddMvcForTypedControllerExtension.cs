3bef2195d6221ea5fc7ddfb262f11db2de775885
Peter Vrenken <vrenken@live.nl>
2017-12-31 01:06:49 +0100

    Added some MVC helper classes

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/ServiceCollectionAddMvcForTypedControllerExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+    using System.Linq;
+    using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceCollectionAddMvcForTypedControllerExtension
+    {
+        public static IMvcBuilder AddMvcForTypedController<TController>(this IServiceCollection services)
+            where TController : ControllerBase
+        {
+            return services
+                .AddMvc()
+                .AddApplicationPart(typeof(TController).Assembly)
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Remove(manager.FeatureProviders.OfType<ControllerFeatureProvider>().Single());
+                    manager.FeatureProviders.Add(new TypedControllerFeatureProvider<TController>());
+                });
+
+        }
+
+    }
+}
