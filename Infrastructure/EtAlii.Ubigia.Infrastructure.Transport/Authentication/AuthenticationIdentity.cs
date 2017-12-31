47c5834e422d6cea55ca0df46eb99622ce58c675
Peter Vrenken <vrenken@live.nl>
2017-12-31 15:01:14 +0100

    Moved AuthenticationIdentity

---
+++ Infrastructure/EtAlii.Ubigia.Infrastructure.Transport/Authentication/AuthenticationIdentity.cs
@@ -0,0 +1,18 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport
+{
+    using System.Security.Principal;
+
+    public class AuthenticationIdentity : GenericIdentity
+    {
+        public AuthenticationIdentity(string name, string password)
+            : base(name, "Basic")
+        {
+            Password = password;
+        }
+
+        /// <summary>
+        /// Basic Auth Password for custom authentication
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
