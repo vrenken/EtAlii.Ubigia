b48760801eca8d5ee2986d9f852cdec8db4046de
Peter Vrenken <vrenken@live.nl>
2018-01-02 23:52:32 +0100

    new signalr client project

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.SignalR.AspNetCore/Data/HubConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.SignalR
+{
+    using System.Net.Http;
+    using Microsoft.AspNetCore.SignalR.Client;
+    using Microsoft.AspNetCore.Sockets.Client;
+    using Newtonsoft.Json;
+
+    public class HubConnectionFactory
+    {
+        public HubConnection Create(string address, HttpClientHandler httpClientHandler)
+        {
+            return new HubConnectionBuilder()
+                .WithUrl(address)
+                .WithMessageHandler(httpClientHandler)
+                .WithJsonProtocol((JsonSerializer) new SerializerFactory().Create())
+                .Build();
+        }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.SignalR.AspNetCore/Data/ISignalRTransport.cs
@@ -0,0 +1,13 @@
+namespace EtAlii.Ubigia.Api.Transport.SignalR
+{
+    using Microsoft.AspNet.SignalR.Client;
+    using Microsoft.AspNet.SignalR.Client.Http;
+
+    public interface ISignalRTransport
+    {
+        IHttpClient HttpClient { get; }
+        HubConnection HubConnection { get; }
+
+        string AuthenticationToken { get; set; }
+    }
+}
