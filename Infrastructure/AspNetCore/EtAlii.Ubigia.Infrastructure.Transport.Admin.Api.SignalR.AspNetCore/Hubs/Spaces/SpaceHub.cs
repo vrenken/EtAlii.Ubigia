c8ea969ff33791ad06ae671504432d98c2011a93
Peter Vrenken <vrenken@live.nl>
2018-01-02 14:52:50 +0100

    Added SignalR hubs

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore/Hubs/Spaces/SpaceHub.cs
@@ -0,0 +1,124 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using EtAlii.Ubigia.Api;
+    using EtAlii.Ubigia.Api.Transport;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class SpaceHub : Hub
+    {
+        private readonly ISpaceRepository _items;
+
+        public SpaceHub(ISpaceRepository items)
+        {
+            _items = items;
+        }
+
+
+        // Get all spaces for the specified accountid
+        public IEnumerable<Space> GetForAccount(Guid accountId)
+        {
+            IEnumerable<Space> response;
+            try
+            {
+                response = _items.GetAll(accountId);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space GET client request", e);
+            }
+            return response;
+        }
+
+        public Space GetForAccount(Guid accountId, string spaceName)
+        {
+            Space response;
+            try
+            {
+                response = _items.Get(accountId, spaceName);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space GET client request", e);
+            }
+            return response;
+        }
+
+        // Get all Items
+        public IEnumerable<Space> GetAll()
+        {
+            IEnumerable<Space> response;
+            try
+            {
+                response = _items.GetAll();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space GET client request", e);
+            }
+            return response;
+        }
+
+        // Get Item by id
+        public Space Get(Guid spaceId)
+        {
+            Space response;
+            try
+            {
+                response = _items.Get(spaceId);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space GET client request", e);
+            }
+            return response;
+        }
+
+        // Add item
+        public Space Post(Space item, string spaceTemplate)
+        {
+            Space response;
+            try
+            {
+                var template = SpaceTemplate.All.Single(t => t.Name == spaceTemplate);
+                response = _items.Add(item, template);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space POST client request", e);
+            }
+            return response;
+        }
+
+        // Update Item by id
+        public Space Put(Guid spaceId, Space space)
+        {
+            Space response;
+            try
+            {
+                response = _items.Update(spaceId, space);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space PUT client request", e);
+            }
+            return response;
+        }
+
+        // Delete Item by id
+        public void Delete(Guid spaceId)
+        {
+            try
+            {
+                _items.Remove(spaceId);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Space DELETE client request", e);
+            }
+        }
+    }
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/Hubs/Entries/EntryHub.Get.cs
@@ -0,0 +1,52 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using EtAlii.Ubigia.Api;
+
+    public partial class EntryHub : HubBase
+    {
+        public Entry GetSingle(Identifier entryId, EntryRelation entryRelations = EntryRelation.None)
+        {
+            Entry response;
+            try
+            {
+                response = _items.Get(entryId, entryRelations);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Entry GET client request", e);
+            }
+            return response;
+        }
+
+        public IEnumerable<Entry> GetMultiple(IEnumerable<Identifier> entryIds, EntryRelation entryRelations = EntryRelation.None)
+        {
+            Entry[] response;
+            try
+            {
+                response = entryIds.Select(entryId => _items.Get(entryId, entryRelations)).ToArray();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a Entries GET client request", e);
+            }
+            return response;
+        }
+
+        public IEnumerable<Entry> GetRelated(Identifier entryId, EntryRelation entriesWithRelation, EntryRelation entryRelations = EntryRelation.None)
+        {
+            IEnumerable<Entry> response;
+            try
+            {
+                response = _items.GetRelated(entryId, entriesWithRelation, entryRelations);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to serve a related Entries GET client request", e);
+            }
+            return response;
+        }
+    }
+}
