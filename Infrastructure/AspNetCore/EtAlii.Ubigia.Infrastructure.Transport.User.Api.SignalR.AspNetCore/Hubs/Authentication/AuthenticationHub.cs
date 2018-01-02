03ac68106edcff40a2f6ea029ab1265ef69fe9a6
Peter Vrenken <vrenken@live.nl>
2018-01-02 14:52:50 +0100

    Added SignalR hubs

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/Hubs/Authentication/AuthenticationHub.cs
@@ -0,0 +1,37 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore
+{
+    using EtAlii.Ubigia.Api;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class AuthenticationHub : Hub
+    {
+        private readonly IStorageRepository _storageRepository;
+
+        private readonly ISignalRAuthenticationVerifier _authenticationVerifier;
+        private readonly ISignalRAuthenticationTokenVerifier _authenticationTokenVerifier;
+
+        public AuthenticationHub(
+            ISignalRAuthenticationVerifier authenticationVerifier, 
+            ISignalRAuthenticationTokenVerifier authenticationTokenVerifier, 
+            IStorageRepository storageRepository)
+        {
+            _authenticationVerifier = authenticationVerifier;
+            _authenticationTokenVerifier = authenticationTokenVerifier;
+            _storageRepository = storageRepository;
+        }
+
+        public string Authenticate(string accountName, string password, string hostIdentifier)
+        {
+            return _authenticationVerifier.Verify(accountName, password, hostIdentifier);
+        }
+
+        public Storage GetLocalStorage()
+        {
+            var authenticationToken = Context.Headers.Get("Authentication-Token");
+            _authenticationTokenVerifier.Verify(authenticationToken, null);
+
+            return _storageRepository.GetLocal();
+        }
+    }
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/Hubs/HubBase.cs
@@ -0,0 +1,31 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class HubBase : Hub
+    {
+        private readonly ISignalRAuthenticationTokenVerifier _authenticationTokenVerifier;
+
+        public HubBase(ISignalRAuthenticationTokenVerifier authenticationTokenVerifier)
+        {
+            _authenticationTokenVerifier = authenticationTokenVerifier;
+        }
+
+        public override Task OnConnected()
+        {
+            var authenticationToken = Context.Headers.Get("Authentication-Token");
+            _authenticationTokenVerifier.Verify(authenticationToken, null);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            var authenticationToken = Context.Headers.Get("Authentication-Token");
+            _authenticationTokenVerifier.Verify(authenticationToken, null);
+
+            return base.OnReconnected();
+        }
+    }
+}
