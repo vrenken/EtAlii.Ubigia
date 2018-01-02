e256d9b979e1e3a71c696e167a6e484d7c45c678
Peter Vrenken <vrenken@live.nl>
2018-01-02 19:58:11 +0100

    Introduced SignalRHub constants

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore/SignalRHub.cs
@@ -0,0 +1,11 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore
+{
+    public static class SignalRHub
+    {
+        public const string Authentication = "AuthenticationHub";
+
+        public const string Account = "AccountHub";
+        public const string Storage = "StorageHub";
+        public const string Space = "SpaceHub";
+    }
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/SignalRHub.cs
@@ -0,0 +1,13 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore
+{
+    public static class SignalRHub
+    {
+        public const string Authentication = "AuthenticationHub";
+
+        public const string Root = "RootHub";
+        public const string Property = "PropertiesHub";
+        public const string Entry = "EntryHub";
+        public const string Content = "ContentHub";
+        public const string ContentDefinition = "ContentDefinitionHub";
+    }
+}
