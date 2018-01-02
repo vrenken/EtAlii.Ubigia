ef208e1f458ab94f519280e7c1dd7e23346b0775
Peter Vrenken <vrenken@live.nl>
2018-01-02 19:55:59 +0100

    Move of SimpleAuthentication*

--- Infrastructure/EtAlii.Ubigia.Infrastructure.SignalR/Api/User/Authentication/Verification/ISignalRAuthenticationTokenVerifier.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Owin.SignalR
-{
-    public interface ISignalRAuthenticationTokenVerifier
-    {
-        void Verify(string authenticationTokenAsString, string requiredRole);
-    }
-}
--- Infrastructure/EtAlii.Ubigia.Infrastructure.SignalR/Api/User/Authentication/Verification/ISignalRAuthenticationVerifier.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Owin.SignalR
-{
-    public interface ISignalRAuthenticationVerifier
-    {
-        string Verify(string accountName, string password, string hostIdentifier);
-    }
-}
