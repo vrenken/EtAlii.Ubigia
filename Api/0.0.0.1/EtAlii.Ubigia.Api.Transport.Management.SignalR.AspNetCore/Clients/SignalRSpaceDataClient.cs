3d74b2fac8b36afab673f61df01b8216bb3eea7d
Peter Vrenken <vrenken@live.nl>
2018-01-03 01:13:00 +0100

    new signalr management client

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.SignalR.AspNetCore/Clients/SignalRSpaceDataClient.cs
@@ -0,0 +1,89 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.Management.SignalR
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Transport;
+    using EtAlii.Ubigia.Api.Transport.SignalR;
+    using Microsoft.AspNetCore.SignalR.Client;
+
+    public sealed class SignalRSpaceDataClient : ISpaceDataClient<ISignalRStorageTransport>
+    {
+        private HubConnection _connection;
+        private readonly IHubProxyMethodInvoker _invoker;
+
+        public SignalRSpaceDataClient(IHubProxyMethodInvoker invoker)
+        {
+            _invoker = invoker;
+        }
+
+        public async Task<Space> Add(Guid accountId, string spaceName, SpaceTemplate template)
+        {
+            var space = new Space
+            {
+                Name = spaceName,
+                AccountId = accountId,
+            };
+            return await _invoker.Invoke<Space>(_connection, SignalRHub.Space, "Post", space, template.Name);
+        }
+
+        public async Task Remove(Guid spaceId)
+        {
+            await _invoker.Invoke(_connection, SignalRHub.Space, "Delete", spaceId);
+        }
+
+        public async Task<Space> Change(Guid spaceId, string spaceName)
+        {
+            var space = new Space
+            {
+                Id = spaceId,
+                Name = spaceName,
+            };
+            return await _invoker.Invoke<Space>(_connection, SignalRHub.Space, "Put", spaceId, space);
+        }
+
+        public async Task<Space> Get(Guid accountId, string spaceName)
+        {
+            return await _invoker.Invoke<Space>(_connection, SignalRHub.Space, "GetForAccount", accountId, spaceName);
+        }
+
+        public async Task<Space> Get(Guid spaceId)
+        {
+            return await _invoker.Invoke<Space>(_connection, SignalRHub.Space, "Get", spaceId);
+        }
+
+        public async Task<IEnumerable<Space>> GetAll(Guid accountId)
+        {
+            return await _invoker.Invoke<IEnumerable<Space>>(_connection, SignalRHub.Space, "GetAllForAccount", accountId);
+        }
+
+        public async Task Connect(IStorageConnection storageConnection)
+        {
+            await Connect((IStorageConnection<ISignalRStorageTransport>) storageConnection);
+        }
+
+        public async Task Disconnect(IStorageConnection storageConnection)
+        {
+            await Disconnect((IStorageConnection<ISignalRStorageTransport>) storageConnection);
+        }
+
+        public async Task Connect(IStorageConnection<ISignalRStorageTransport> storageConnection)
+        {
+            await Task.Run(() =>
+            {
+                _connection = new HubConnectionFactory().Create(storageConnection.Storage.Address + "/" + SignalRHub.Space, storageConnection.Transport.HttpClientHandler);
+                //_proxy = storageConnection.Transport.HubConnection.CreateHubProxy(SignalRHub.Space);
+            });
+        }
+
+        public async Task Disconnect(IStorageConnection<ISignalRStorageTransport> storageConnection)
+        {
+            await _connection.DisposeAsync();
+            _connection = null;
+            //await Task.Run(() =>
+            //{
+            //    _proxy = null;
+            //});
+        }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.SignalR.AspNetCore/SignalRStorageConnection.cs
@@ -0,0 +1,19 @@
+namespace EtAlii.Ubigia.Api.Transport.Management.SignalR
+{
+    using EtAlii.Ubigia.Api.Transport;
+    using EtAlii.Ubigia.Api.Transport.SignalR;
+
+    internal class SignalRStorageConnection : StorageConnection<ISignalRStorageTransport>, ISignalRStorageConnection
+    {
+        public SignalRStorageConnection(
+            IStorageTransport transport, 
+            IStorageConnectionConfiguration configuration, 
+            IStorageContext storages, 
+            ISpaceContext spaces, 
+            IAccountContext accounts,
+            IAuthenticationContext authentication) 
+            : base(transport, configuration, storages, spaces, accounts, authentication)
+        {
+        }
+    }
+}
