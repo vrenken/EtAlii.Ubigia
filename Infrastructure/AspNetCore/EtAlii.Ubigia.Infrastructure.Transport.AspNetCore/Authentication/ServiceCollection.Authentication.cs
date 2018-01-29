b235d79cd228f28a5add6bcc9a93910a12e0eab4
Peter Vrenken <vrenken@live.nl>
2018-01-29 23:43:17 +0100

    Reusing code.

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/ServiceCollection.Authentication.cs
@@ -0,0 +1,110 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+	using System.IdentityModel.Tokens.Jwt;
+	using System.Security.Claims;
+	using System.Threading.Tasks;
+	using EtAlii.Ubigia.Infrastructure.Functional;
+	using Microsoft.AspNetCore.Authentication.JwtBearer;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.IdentityModel.Tokens;
+
+	public static partial class ServiceCollectionExtensions
+	{
+		private static readonly SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+		private static readonly JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
+
+		public static IServiceCollection AddInfrastructureHttpContextAuthentication(this IServiceCollection services, IInfrastructure infrastructure)
+		{
+			services
+				.TryAddSingleton<IAccountRepository>(infrastructure.Accounts)
+				.TryAddSingleton<IInfrastructureConfiguration>(infrastructure.Configuration)
+				.AddSingleton<IHttpContextAuthenticationVerifier, HttpContextAuthenticationVerifier>()
+				.AddSingleton<IHttpContextAuthenticationTokenVerifier, HttpContextAuthenticationTokenVerifier>()
+				.AddSingleton<IHttpContextAuthenticationIdentityProvider, DefaultHttpContextAuthenticationIdentityProvider>();
+
+			AddJwtBearer(services);
+
+			return services;
+		}
+
+		public static IServiceCollection AddInfrastructureSimpleAuthentication(this IServiceCollection services, IInfrastructure infrastructure)
+	    {
+		    services
+				//.AddSingleton<IDependencyResolver>(() => _signalRDependencyResolver);
+				//.AddSingleton<IParameterResolver, SignalRParameterResolver>()
+				.TryAddSingleton<IInfrastructureConfiguration>(infrastructure.Configuration)
+			    .AddSingleton<IAuthenticationTokenConverter, AuthenticationTokenConverter>()
+			    .AddSingleton<ISimpleAuthenticationVerifier, SimpleAuthenticationVerifier>()
+			    .AddSingleton<ISimpleAuthenticationTokenVerifier, SimpleAuthenticationTokenVerifier>();
+
+			//AddJwtBearer(services);
+
+			return services;
+	    }
+
+		private static void AddJwtBearer(IServiceCollection services)
+		{
+			// Source: https://github.com/aspnet/SignalR/blob/dev/samples/JwtSample/Startup.cs#L36
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+				{
+					policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+					policy.RequireClaim(ClaimTypes.NameIdentifier);
+				});
+			});
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters =
+						new TokenValidationParameters
+						{
+							LifetimeValidator = (before, expires, token, parameters) => expires > DateTime.UtcNow,
+							ValidateAudience = false,
+							ValidateIssuer = false,
+							ValidateActor = false,
+							ValidateLifetime = true,
+							IssuerSigningKey = SecurityKey
+						};
+					options.Events = new JwtBearerEvents
+					{
+						OnMessageReceived = context =>
+						{
+							var accessToken = context.Request.Query["access_token"];
+							if (!string.IsNullOrEmpty(accessToken) &&
+							    (context.HttpContext.WebSockets.IsWebSocketRequest || context.Request.Headers["Accept"] == "text/event-stream"))
+							{
+								context.Token = context.Request.Query["access_token"];
+							}
+							return Task.CompletedTask;
+						}
+					};
+				});
+		}
+
+
+		//.AddAuthentication(options =>
+		//{
+		//    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+		//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		//})
+		//.AddJwtBearer(options =>
+		//{
+		//    options.TokenValidationParameters = new TokenValidationParameters
+		//    {
+		//        ValidateIssuer = true,
+		//        ValidateAudience = true,
+		//        ValidateIssuerSigningKey = true,
+		//        ValidIssuer = _configuration["Jwt:Issuer"],
+		//        ValidAudience = _configuration["Jwt:Issuer"],
+		//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+		//        ValidateLifetime = true, //validate the expiration and not before values in the token
+		//        ClockSkew = TimeSpan.FromMinutes(1) //5 minute tolerance for the expiration date
+
+		//    };
+		//});
+
+	}
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/ServiceCollection.cs
@@ -0,0 +1,21 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.DependencyInjection.Extensions;
+
+	public static partial class ServiceCollectionExtensions
+	{
+		public static IServiceCollection TryAddSingleton<TService>(this IServiceCollection collection, TService instance) 
+			where TService : class
+		{
+			if (collection == null)
+				throw new ArgumentNullException(nameof(collection));
+			if ((object)instance == null)
+				throw new ArgumentNullException(nameof(instance));
+			ServiceDescriptor descriptor = ServiceDescriptor.Singleton(typeof(TService), (object)instance);
+			collection.TryAdd(descriptor);
+			return collection;
+		}
+	}
+}
