629a4a9b43216188b72672440132d4b2f1d5dfce
Peter Vrenken <vrenken@live.nl>
2018-01-29 14:41:13 +0100

    Small corrections in urls.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.SignalR.AspNetCore/Data/SignalRHub.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.SignalR.AspNetCore/Data/SignalRHub.cs
@@ -2,15 +2,15 @@
 {
     public static class SignalRHub
     {
-        public const string Authentication = "AuthenticationHub";
-        public const string Root = "RootHub";
-        public const string Property = "PropertiesHub";
-        public const string Entry = "EntryHub";
-        public const string Content = "ContentHub";
-        public const string ContentDefinition = "ContentDefinitionHub";
+        public const string Authentication = "Authentication";
+        public const string Root = "Root";
+        public const string Property = "Properties";
+        public const string Entry = "Entry";
+        public const string Content = "Content";
+        public const string ContentDefinition = "ContentDefinition";
 
-        public const string Account = "AccountHub";
-        public const string Storage = "StorageHub";
-        public const string Space = "SpaceHub";
+        public const string Account = "Account";
+        public const string Storage = "Storage";
+        public const string Space = "Space";
     }
 }
