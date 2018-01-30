bcd3457408ad2c367c01148624de42f6c2d8ccfe
Peter Vrenken <vrenken@live.nl>
2018-01-30 02:04:05 +0100

    admin api back operational

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore/AdminSignalRService.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore/AdminSignalRService.cs
@@ -1,20 +1,55 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.SignalR.AspNetCore
 {
+    using System.Linq;
+    using EtAlii.Ubigia.Api.Transport;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using EtAlii.Ubigia.Infrastructure.Transport.AspNetCore;
     using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
-    public class AdminSignalRService : ServiceBase
+    public class AdminSignalRService : AspNetCoreServiceBase
     {
-        public override void Start()
+        public AdminSignalRService(IConfigurationSection configuration) 
+            : base(configuration)
         {
         }
 
-        public override void Stop()
+        protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
         {
-        }
+            var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
 
-        protected override void Initialize(IHost host, ISystem system, IModule[] moduleChain, out Status status)
-        {
-            status = new Status(nameof(AdminSignalRService));
+            applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.Admin.Api.SignalR.BaseUrl,
+                services =>
+                {
+                    services
+                        .AddSingleton<IAccountRepository>(infrastructure.Accounts)
+                        .AddSingleton<ISpaceRepository>(infrastructure.Spaces)
+                        .AddSingleton<IStorageRepository>(infrastructure.Storages)
+
+						.AddInfrastructureSimpleAuthentication(infrastructure)
+						.AddInfrastructureSerialization()
+
+	                    .AddCors()
+                        .AddSignalR(options => SerializerFactory.Configure(options.JsonSerializerSettings) );
+                },
+                appBuilder =>
+                {
+                    appBuilder
+                        .UseCors(configuration =>
+                        {
+                            configuration.AllowAnyOrigin();
+                        })
+                        .UseSignalR(routes =>
+                        {
+                            routes.MapHub<AuthenticationHub>(SignalRHub.Authentication);
+
+                            routes.MapHub<StorageHub>(SignalRHub.Storage);
+                            routes.MapHub<SpaceHub>(SignalRHub.Space);
+                            routes.MapHub<AccountHub>(SignalRHub.Account);
+						});
+                });
         }
     }
 }
