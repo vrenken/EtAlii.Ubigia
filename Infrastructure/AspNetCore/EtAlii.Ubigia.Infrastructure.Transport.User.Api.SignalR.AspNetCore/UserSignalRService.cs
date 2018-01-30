64d9d99fe98ea524320fa8d1847ebd4715ff6689
Peter Vrenken <vrenken@live.nl>
2018-01-30 12:55:35 +0100

    First implementation of UserSignalRService

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/UserSignalRService.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore/UserSignalRService.cs
@@ -1,20 +1,59 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR.AspNetCore
 {
+    using System.Linq;
+    using EtAlii.Ubigia.Api.Transport;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+    using EtAlii.Ubigia.Infrastructure.Transport.AspNetCore;
     using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
-    public class UserSignalRService : ServiceBase
+    public class UserSignalRService : AspNetCoreServiceBase
     {
-        public override void Start()
+        public UserSignalRService(IConfigurationSection configuration) 
+            : base(configuration)
         {
         }
 
-        public override void Stop()
+        protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
         {
-        }
+            var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
 
-        protected override void Initialize(IHost host, ISystem system, IModule[] moduleChain, out Status status)
-        {
-            status = new Status(nameof(UserSignalRService));
+            applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.User.Api.SignalR.BaseUrl,
+                services =>
+                {
+                    services
+                        .AddSingleton<IRootRepository>(infrastructure.Roots)
+                        .AddSingleton<IEntryRepository>(infrastructure.Entries)
+                        .AddSingleton<IPropertiesRepository>(infrastructure.Properties)
+                        .AddSingleton<IContentRepository>(infrastructure.Content)
+                        .AddSingleton<IContentDefinitionRepository>(infrastructure.ContentDefinition)
+
+	                    .AddInfrastructureSimpleAuthentication(infrastructure)
+	                    .AddInfrastructureSerialization()
+
+						.AddCors()
+						.AddSignalR(options => SerializerFactory.Configure(options.JsonSerializerSettings));
+                },
+                appBuilder =>
+                {
+                    appBuilder
+                        .UseCors(configuration =>
+                        {
+                            configuration.AllowAnyOrigin(); 
+                        })
+                        .UseSignalR(routes =>
+                        {
+                            routes.MapHub<AuthenticationHub>(SignalRHub.Authentication);
+
+                            routes.MapHub<RootHub>(SignalRHub.Root);
+                            routes.MapHub<EntryHub>(SignalRHub.Entry);
+                            routes.MapHub<PropertiesHub>(SignalRHub.Property);
+                            routes.MapHub<ContentHub>(SignalRHub.Content);
+                            routes.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
+                        });
+                });
         }
     }
 }
