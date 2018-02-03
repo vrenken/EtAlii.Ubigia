15011fa5b619ca8e7145eea0b623f098fd9fb276
Peter Vrenken <vrenken@live.nl>
2018-02-03 21:14:24 +0100

    Remove of duplicate code

---
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Owin/__Commands/Admin/Control/ShutdownHostCommand.cs
@@ -0,0 +1,23 @@
+﻿//namespace EtAlii.Ubigia.Infrastructure.Hosting.Owin
+//{
+//    using EtAlii.xTechnology.Hosting;
+
+//    class ShutdownHostCommand : HostCommandBase, IShutdownHostCommand
+//    {
+//        public string Name => "Shutdown";
+
+//	    public ShutdownHostCommand(IHost host) : base(host)
+//	    {
+//	    }
+
+//		public void Execute()
+//        {
+//            Host.Shutdown();
+//        }
+
+//        protected override void OnHostStateChanged(State state)
+//        {
+//            CanExecute = state == State.Running || state == State.Stopped;
+//        }
+//    }
+//}
