4fe64defd566c8f29f39b0d43a0796c329198c03
Peter Vrenken <vrenken@live.nl>
2018-03-02 23:44:23 +0100

    Added first start of AspNetCore WebApiClients

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.WebApi.AspNetCore/Data/Clients/WebApiAuthenticationDataClient.cs
@@ -0,0 +1,29 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.WebApi
+{
+    using System.Threading.Tasks;
+
+    public partial class WebApiAuthenticationDataClient : IAuthenticationDataClient
+    {
+        private IWebApiSpaceConnection _connection;
+
+        public async Task Connect(ISpaceConnection spaceConnection)
+        {
+            await Connect((IWebApiSpaceConnection) spaceConnection);
+        }
+
+        public async Task Disconnect(ISpaceConnection spaceConnection)
+        {
+            await Disconnect((IWebApiSpaceConnection) spaceConnection);
+        }
+
+        private async Task Connect(IWebApiSpaceConnection connection)
+        {
+            await Task.Run(() => { _connection = connection; });
+        }
+
+        private async Task Disconnect(IWebApiSpaceConnection connection)
+        {
+            await Task.Run(() => { _connection = null; });
+        }
+    }
+}
