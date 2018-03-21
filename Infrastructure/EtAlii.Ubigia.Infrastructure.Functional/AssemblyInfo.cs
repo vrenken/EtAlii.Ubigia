4744681da4ca4af28885b3b3700f178f111e27c8
Peter Vrenken <vrenken@live.nl>
2018-03-21 18:24:51 +0100

    small correction to class visibility through internalsvisibleto.

--- Infrastructure/EtAlii.Ubigia.Infrastructure.Functional/AssemblyInfo.cs
+++ Infrastructure/EtAlii.Ubigia.Infrastructure.Functional/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Transport.Owin.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Transport.AspNetCore.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Hosting.Owin.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Hosting.AspNetCore.Tests")]
