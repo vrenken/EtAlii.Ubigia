5e91d71193458409ad8d014e00f4f5141407248a
Peter Vrenken <vrenken@live.nl>
2018-03-21 18:22:20 +0100

    Added EtAlii.Ubigia.Api.Transport.WebApi.AspNetCore

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.WebApi.AspNetCore/Data/Resolvers/Entries/IEntryInfoProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.WebApi
+{
+    public interface IEntryInfoProvider
+    {
+        Entry Entry { get; }
+        Identifier EntryId { get; }
+
+        Storage TargetStorage { get; }
+    }
+}
