c35bddc87eb18a4c6f9d142a5e47fe471ddf3c39
Peter Vrenken <vrenken@live.nl>
2018-04-01 01:59:21 +0200

    Added BSON inputformatter and outputformatter and made all formatting use SerializerFactory.Create().

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Serialization/PayloadMediaTypeInputFormatter.cs
@@ -0,0 +1,190 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+	using EtAlii.Ubigia.Api.Transport;
+	using Microsoft.AspNetCore.Mvc.Formatters;
+    using Microsoft.Net.Http.Headers;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Bson;
+
+    public class PayloadMediaTypeInputFormatter : InputFormatter//: BaseJsonMediaTypeFormatter
+	{
+        private static readonly Type OpenDictionaryType = typeof(Dictionary<,>);
+        private static readonly TypeInfo EnumerableTypeInfo = typeof(IEnumerable).GetTypeInfo();
+        private static readonly TypeInfo DictionaryTypeInfo = typeof(IDictionary).GetTypeInfo();
+
+        public static readonly MediaTypeHeaderValue MediaType = new MediaTypeHeaderValue("application/bson");
+
+		///// <summary>
+		///// Initializes a new instance of the <see cref="BsonMediaTypeFormatter"/> class.
+		///// </summary>
+		public PayloadMediaTypeInputFormatter()
+		{
+		    // Set default supported media type
+		    SupportedMediaTypes.Add(MediaType);
+
+		//    SerializerFactory.AddDefaultConverters(SerializerSettings.Converters);
+		}
+
+		///// <summary>
+		///// Initializes a new instance of the <see cref="BsonMediaTypeFormatter"/> class.
+		///// </summary>
+		///// <param name="formatter">The <see cref="BsonMediaTypeFormatter"/> instance to copy settings from.</param>
+		//protected PayloadMediaTypeFormatter(BsonMediaTypeFormatter formatter)
+		//    : base(formatter)
+		//{
+		//}
+
+		/// <inheritdoc />
+
+		protected override bool CanReadType(Type type)
+		{
+			return true;
+			//if (typeof(Contact).IsAssignableFrom(type) || typeof(IEnumerable<Contact>).IsAssignableFrom(type))
+			//{
+			//	return base.CanReadType(type);
+			//}
+			//return false;
+		}
+
+		public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+		{
+			var request = context.HttpContext.Request;
+			var result = ReadFromStream(context.ModelType, request.Body);//, Encoding.UTF8);
+			return await InputFormatterResult.SuccessAsync(result);
+		}
+
+		private object ReadFromStream(Type type, Stream readStream)//, Encoding effectiveEncoding)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (readStream == null)
+            {
+                throw new ArgumentNullException(nameof(readStream));
+            }
+
+            // Special-case for simple types: Deserialize a Dictionary with a single element named Value.
+            // Serialization created this Dictionary<string, object> to work around BSON restrictions: BSON cannot
+            // handle a top-level simple type.  NewtonSoft.Json throws a JsonWriterException with message "Error
+            // writing Binary value. BSON must start with an Object or Array. Path ''." when WriteToStream() is given
+            // such a value.
+            //
+            // Added clause for typeof(byte[]) needed because NewtonSoft.Json sometimes throws above Exception when
+            // WriteToStream() is given a byte[] value.  (Not clear where the bug lies and, worse, it doesn't reproduce
+            // reliably.)
+            //
+            // Request for typeof(object) may cause a simple value to round trip as a JObject.
+            if (IsSimpleType(type) || type == typeof(byte[]))
+            {
+                // Read as exact expected Dictionary<string, T> to ensure NewtonSoft.Json does correct top-level conversion.
+                var dictionaryType = OpenDictionaryType.MakeGenericType(typeof(string), type);
+                if (!(ReadFromStreamInternal(dictionaryType, readStream) is IDictionary dictionary))//, effectiveEncoding) is IDictionary dictionary))
+                {
+                    // Not valid since BaseJsonMediaTypeFormatter.ReadFromStream(Type, Stream, HttpContent, IFormatterLogger)
+                    // handles empty content and does not call ReadFromStream(Type, Stream, Encoding, IFormatterLogger)
+                    // in that case.
+                    var e1 = new InvalidOperationException("Missing data");
+                    e1.Data.Add("dictionaryType.Name", dictionaryType.Name);
+                    throw e1;
+                }
+
+                // Unfortunately IDictionary doesn't have TryGetValue()...
+                var firstKey = String.Empty;
+                foreach (DictionaryEntry item in dictionary)
+                {
+                    if (dictionary.Count == 1 && (item.Key as string) == "Value")
+                    {
+                        // Success
+                        return item.Value;
+                    }
+                    else
+                    {
+                        if (item.Key != null)
+                        {
+                            firstKey = item.Key.ToString();
+                        }
+
+                        break;
+                    }
+                }
+
+                var e2 = new InvalidOperationException("Unexpected Data");
+                e2.Data.Add("dictionary.Count", dictionary.Count);
+                e2.Data.Add("firstKey", firstKey);
+                throw e2;
+            }
+            else
+            {
+				return ReadFromStreamInternal(type, readStream);//, effectiveEncoding);
+            }
+        }
+
+	    private object ReadFromStreamInternal(Type type, Stream readStream)//, Encoding effectiveEncoding)
+	    {
+			using (var reader = this.CreateJsonReader(type, readStream))//, effectiveEncoding))
+		    {
+			    reader.CloseInput = false;
+			    var serializer = new SerializerFactory().Create();
+			    //var serializer = JsonSerializer.CreateDefault();
+			    return serializer.Deserialize(reader, type);
+		    }
+		}
+		/// <inheritdoc />
+		public JsonReader CreateJsonReader(Type type, Stream readStream)//, Encoding effectiveEncoding)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (readStream == null)
+            {
+                throw new ArgumentNullException(nameof(readStream));
+            }
+
+            //if (effectiveEncoding == null)
+            //{
+            //    throw new ArgumentNullException(nameof(effectiveEncoding));
+            //}
+
+            var reader = new BsonDataReader(new BinaryReader(readStream));//, effectiveEncoding));
+
+            try
+            {
+                // Special case discussed at http://stackoverflow.com/questions/16910369/bson-array-deserialization-with-json-net
+                // Dispensed with string (aka IEnumerable<char>) case above in ReadFromStream()
+                reader.ReadRootValueAsArray = EnumerableTypeInfo.IsAssignableFrom(type.GetTypeInfo()) && !DictionaryTypeInfo.IsAssignableFrom(type.GetTypeInfo());
+            }
+            catch
+            {
+                // Ensure instance is cleaned up in case of an issue
+                ((IDisposable)reader).Dispose();
+                throw;
+            }
+
+            return reader;
+        }
+
+        // Return true if Json.Net will likely convert value of given type to a Json primitive, not JsonArray nor
+        // JsonObject.
+        // To do: https://aspnetwebstack.codeplex.com/workitem/1467
+        private static bool IsSimpleType(Type type)
+        {
+            // Cannot happen.
+            // Contract.Assert(type != null);
+
+            var isSimpleType = type.GetTypeInfo().IsValueType || type == typeof(string);
+
+            return isSimpleType;
+        }
+	}
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Serialization/PayloadMediaTypeOutputFormatter.cs
@@ -0,0 +1,156 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+	using EtAlii.Ubigia.Api.Transport;
+	using Microsoft.AspNetCore.Mvc.Formatters;
+    using Microsoft.Net.Http.Headers;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Bson;
+
+    public class PayloadMediaTypeOutputFormatter : OutputFormatter//: BaseJsonMediaTypeFormatter
+	{
+        public static readonly MediaTypeHeaderValue MediaType = new MediaTypeHeaderValue("application/bson");
+
+		///// <summary>
+		///// Initializes a new instance of the <see cref="BsonMediaTypeFormatter"/> class.
+		///// </summary>
+		public PayloadMediaTypeOutputFormatter()
+		{
+		    // Set default supported media type
+		    SupportedMediaTypes.Add(MediaType);
+
+		//    SerializerFactory.AddDefaultConverters(SerializerSettings.Converters);
+		}
+
+		///// <summary>
+		///// Initializes a new instance of the <see cref="BsonMediaTypeFormatter"/> class.
+		///// </summary>
+		///// <param name="formatter">The <see cref="BsonMediaTypeFormatter"/> instance to copy settings from.</param>
+		//protected PayloadMediaTypeFormatter(BsonMediaTypeFormatter formatter)
+		//    : base(formatter)
+		//{
+		//}
+
+		/// <inheritdoc />
+
+		protected override bool CanWriteType(Type type)
+		{
+			return true;
+			//if (typeof(Contact).IsAssignableFrom(type) || typeof(IEnumerable<Contact>).IsAssignableFrom(type))
+			//{
+			//	return base.CanWriteType(type);
+			//}
+			//return false;
+		}
+
+		public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+		{
+			var response = context.HttpContext.Response;
+
+			//var stream = new MemoryStream();
+			WriteToStream(context.ObjectType, context.Object, response.Body);//stream);//, Encoding.UTF8);
+			//response.Body = stream;
+
+			await Task.CompletedTask;
+		}
+
+		public void WriteToStream(Type type, object value, Stream writeStream)//, Encoding effectiveEncoding)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (writeStream == null)
+            {
+                throw new ArgumentNullException(nameof(writeStream));
+            }
+
+            //if (effectiveEncoding == null)
+            //{
+            //    throw new ArgumentNullException(nameof(effectiveEncoding));
+            //}
+
+            if (value == null)
+            {
+                // Cannot serialize null at the top level.  Json.Net throws Newtonsoft.Json.JsonWriterException : Error
+                // writing Null value. BSON must start with an Object or Array. Path ''.  Fortunately
+                // BaseJsonMediaTypeFormatter.ReadFromStream(Type, Stream, HttpContent, IFormatterLogger) treats zero-
+                // length content as null or the default value of a struct.
+                return;
+            }
+
+            // See comments in ReadFromStream() above about this special case and the need to include byte[] in it.
+            // Using runtime type here because Json.Net will throw during serialization whenever it cannot handle the
+            // runtime type at the top level. For e.g. passed type may be typeof(object) and value may be a string.
+            var runtimeType = value.GetType();
+            if (IsSimpleType(runtimeType) || runtimeType == typeof(byte[]))
+            {
+                // Wrap value in a Dictionary with a single property named "Value" to provide BSON with an Object.  Is
+                // written out as binary equivalent of { "Value": value } JSON.
+                var temporaryDictionary = new Dictionary<string, object>
+                {
+                    { "Value", value },
+                };
+	            WriteToStreamInternal(typeof(Dictionary<string, object>), temporaryDictionary, writeStream);//, effectiveEncoding);
+			}
+			else
+            {
+
+				WriteToStreamInternal(type, value, writeStream);//, effectiveEncoding);
+            }
+        }
+
+	    private void WriteToStreamInternal(Type type, object value, Stream writeStream)//, Encoding effectiveEncoding)
+	    {
+			using (var writer = CreateJsonWriter(type, writeStream))//, effectiveEncoding))
+		    {
+			    writer.CloseOutput = false;
+			    var serializer = new SerializerFactory().Create();
+			    //var serializer = JsonSerializer.CreateDefault();
+			    serializer.Serialize(writer, value);
+			    writer.Flush();
+		    };
+		}
+
+		/// <inheritdoc />
+		public JsonWriter CreateJsonWriter(Type type, Stream writeStream)//, Encoding effectiveEncoding)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (writeStream == null)
+            {
+                throw new ArgumentNullException(nameof(writeStream));
+            }
+
+            //if (effectiveEncoding == null)
+            //{
+            //    throw new ArgumentNullException(nameof(effectiveEncoding));
+            //}
+
+            return new BsonDataWriter(new BinaryWriter(writeStream));//, effectiveEncoding));
+        }
+
+        // Return true if Json.Net will likely convert value of given type to a Json primitive, not JsonArray nor
+        // JsonObject.
+        // To do: https://aspnetwebstack.codeplex.com/workitem/1467
+        private static bool IsSimpleType(Type type)
+        {
+            // Cannot happen.
+            // Contract.Assert(type != null);
+
+            var isSimpleType = type.GetTypeInfo().IsValueType || type == typeof(string);
+
+            return isSimpleType;
+        }
+	}
+}
