85d6b0eb364391977a4a7e0919e05620f677a9bc
Peter Vrenken <vrenken@live.nl>
2018-04-01 12:14:18 +0200

    Added some comments for when the TestHttpClientFactory doesn't work

--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.AspNetCore.Tests/Rest/Client/TestHttpClientFactory.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.AspNetCore.Tests/Rest/Client/TestHttpClientFactory.cs
@@ -6,7 +6,7 @@
     using Microsoft.AspNetCore.TestHost;
 
 	internal class TestHttpClientFactory : IHttpClientFactory
-    {
+	{
         private readonly TestServer _testServer;
 
         public TestHttpClientFactory(TestServer testServer)
@@ -16,11 +16,23 @@
 
         public HttpClient Create(ICredentials credentials, string hostIdentifier, string authenticationToken)
         {
-            var handler = _testServer.CreateHandler();
-            var client = new HttpClient(new TestHttpClientMessageHandler(handler, credentials, hostIdentifier, authenticationToken));
+			var handler = _testServer.CreateHandler();
+			var client = new HttpClient(new TestHttpClientMessageHandler(handler, credentials, hostIdentifier, authenticationToken));
 
-            // Set the Accept header for BSON.
-            client.DefaultRequestHeaders.Accept.Add(PayloadMediaTypeFormatter.MediaType);
+	  //      var client = _testServer.CreateClient();
+			//if (credentials != null)
+	  //      {
+		 //       var cr = credentials.GetCredential(_testServer.BaseAddress, "Basic-Authentication");
+		 //       client.DefaultRequestHeaders.Add("Test-UserName", cr.UserName);
+		 //       client.DefaultRequestHeaders.Add("Test-Password", cr.Password);
+	  //      }
+
+	  //      client.DefaultRequestHeaders.Add("Host-Identifier", hostIdentifier);
+	  //      client.DefaultRequestHeaders.Add("Authentication-Token", authenticationToken);
+
+
+			// Set the Accept header for BSON.
+			client.DefaultRequestHeaders.Accept.Add(PayloadMediaTypeFormatter.MediaType);
 
             return client;
         }
