b3d88299d48d23c61a422555cd3148b2dd8010c9
Peter Vrenken <vrenken@live.nl>
2018-04-02 02:19:12 +0200

    Made switching between admin/system and normal user accounts possible

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.Rest.AspNetCore/Controllers/Authentication/AuthenticateController.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.Rest.AspNetCore/Controllers/Authentication/AuthenticateController.cs
@@ -1,19 +1,33 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.Rest.AspNetCore
 {
+	using System;
+	using System.Linq;
 	using EtAlii.Ubigia.Api.Transport;
+	using EtAlii.Ubigia.Infrastructure.Functional;
 	using EtAlii.Ubigia.Infrastructure.Transport.AspNetCore;
 	using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Primitives;
 
 	[Route(RelativeUri.Authenticate)]
     public class AuthenticateController : RestController
 	{
-        private readonly IHttpContextAuthenticationVerifier _authenticationVerifier;
+		private readonly IAccountRepository _items;
+		private readonly IHttpContextAuthenticationVerifier _authenticationVerifier;
+		private readonly IAuthenticationTokenConverter _authenticationTokenConverter;
+		private readonly IHttpContextResponseBuilder _responseBuilder;
 
-        public AuthenticateController(IHttpContextAuthenticationVerifier authenticationVerifier)
-        {
-            _authenticationVerifier = authenticationVerifier;
-        }
+		public AuthenticateController(
+			IAccountRepository items,
+			IHttpContextAuthenticationVerifier authenticationVerifier, 
+			IAuthenticationTokenConverter authenticationTokenConverter, 
+			IHttpContextResponseBuilder responseBuilder)
+		{
+			_items = items;
+			_authenticationVerifier = authenticationVerifier;
+			_authenticationTokenConverter = authenticationTokenConverter;
+			_responseBuilder = responseBuilder;
+		}
 
         [AllowAnonymous]
         [HttpGet]
@@ -22,5 +36,29 @@
             var result = _authenticationVerifier.Verify(HttpContext, this, Role.User, Role.System);
             return result;
         }
-    }
+
+		[AllowAnonymous]
+		[HttpGet]
+		public IActionResult Get([RequiredFromQuery]string accountName, [RequiredFromQuery(Name = "authenticationToken")] string value)
+		{
+			IActionResult response = null;
+			try
+			{
+				HttpContext.Request.Headers.TryGetValue("Authentication-Token", out StringValues stringValues);
+				var authenticationTokenAsString = stringValues.Single();
+				var authenticationToken = _authenticationTokenConverter.FromString(authenticationTokenAsString);
+
+				var account = _items.Get(authenticationToken.Name);
+				if (account.Roles.Any(role => role == Role.Admin || role == Role.System))
+				{
+					response = _responseBuilder.Build(HttpContext, this, accountName);
+				}
+			}
+			catch (Exception ex)
+			{
+				response = BadRequest(ex.Message);
+			}
+			return response;
+		}
+	}
 }
