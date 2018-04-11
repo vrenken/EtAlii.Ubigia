3b93bd0ea31ff632a217791733d36b1a8f0932cb
Peter Vrenken <vrenken@live.nl>
2018-04-11 20:10:29 +0200

    Made TransportTestContext more clever

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.SignalR.AspNetCore.Tests/ITransportTestContext.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.SignalR.AspNetCore.Tests/ITransportTestContext.cs
@@ -3,10 +3,11 @@ namespace EtAlii.Ubigia.Api.Transport.Management.Tests
     using EtAlii.Ubigia.Api.Tests;
     using EtAlii.Ubigia.Api.Transport.SignalR.Tests;
     using EtAlii.Ubigia.Api.Transport.Tests;
+    using EtAlii.Ubigia.Infrastructure.Hosting;
 
     internal class TransportTestContext
     {
-        public ITransportTestContext Create()
+        public ITransportTestContext<InProcessInfrastructureHostTestContext> Create()
         {
             return new TransportTestContextFactory().Create<SignalRTransportTestContext>();
         }
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.SignalR.AspNetCore.Tests/Host/__TransportHostTestContextFactory.cs
@@ -0,0 +1,12 @@
+﻿//namespace EtAlii.Ubigia.Api.Transport.Tests
+//{
+//    using EtAlii.Ubigia.Infrastructure.Hosting;
+
+//    public class TransportHostTestContextFactory : IHostTestContextFactory
+//    {
+//        public IHostTestContext Create()
+//        {
+//            return new HostTestContext<InProcessInfrastructureTestHost>();
+//        }
+//    }
+//}
