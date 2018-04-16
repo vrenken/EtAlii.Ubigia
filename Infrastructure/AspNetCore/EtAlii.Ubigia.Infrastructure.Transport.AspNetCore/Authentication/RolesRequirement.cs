e1b3412ab66ac79fb2cb887385e1b9f511851f52
Peter Vrenken <vrenken@live.nl>
2018-04-16 18:09:34 +0200

    Parts of the authentication attributes

---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/RolesRequirement.cs
@@ -0,0 +1,21 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+    using System.Net;
+	using System.Security.Claims;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+
+	internal class RolesRequirement : IAuthorizationRequirement
+	{
+		public readonly string[] RequiredRoles;
+
+		public RolesRequirement(string[] requiredRoles)
+		{
+			RequiredRoles = requiredRoles;
+		}
+	}
+}
---
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.AspNetCore/Authentication/RolesRequirementHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.AspNetCore
+{
+	using System;
+	using System.Linq;
+	using System.Net;
+	using System.Security.Claims;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.Filters;
+
+
+	internal class RolesRequirementHandler : AuthorizationHandler<RolesRequirement>, IAuthorizationRequirement
+	{
+		private readonly IHttpContextAuthenticationVerifier _verifier;
+
+		public RolesRequirementHandler(IHttpContextAuthenticationVerifier verifier)
+		{
+			_verifier = verifier;
+		}
+
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RolesRequirement requirement)
+		{
+			var status = _verifier.Verify(null, null, requirement.RequiredRoles);
+			if (status is OkResult)
+			{
+				context.Succeed(requirement);
+			}
+			else
+			{
+				context.Fail();
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}
