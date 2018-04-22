9a8a4ed728a99bdfb907a74c0b4aa282dedd8fc4
Peter Vrenken <vrenken@live.nl>
2018-04-22 15:19:00 +0200

    Added Grpc transport projects

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc.Tests/SignalRTransportTestContext.cs
@@ -0,0 +1,69 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.SignalR.Tests
+{
+	using System;
+	using System.Net.Http;
+	using System.Threading.Tasks;
+	using EtAlii.Ubigia.Api.Tests;
+	using EtAlii.Ubigia.Api.Transport.Diagnostics;
+    using EtAlii.Ubigia.Api.Transport.Management;
+    using EtAlii.Ubigia.Api.Transport.Management.Diagnostics;
+    using EtAlii.Ubigia.Api.Transport.Management.SignalR;
+    using EtAlii.Ubigia.Api.Transport.SignalR;
+    using EtAlii.Ubigia.Api.Transport.Tests;
+    using EtAlii.Ubigia.Infrastructure.Hosting;
+
+    public class SignalRTransportTestContext : TransportTestContextBase<InProcessInfrastructureHostTestContext>, ITransportTestContext<InProcessInfrastructureHostTestContext>
+    {
+        public override async Task<IDataConnection> CreateDataConnection(Uri address, string accountName, string accountPassword, string spaceName, bool openOnCreation, bool useNewSpace, SpaceTemplate spaceTemplate = null)
+        {
+            var diagnostics = TestDiagnostics.Create();
+
+			//var signalRHttpClient = new SignalRTestHttpClient(c => ((TestInfrastructure)Context.Host.Infrastructure).Server.Handler); 
+			var httpMessageHandlerFactory = new Func<HttpMessageHandler>(() => Context.Host.Server.CreateHandler());
+
+			var connectionConfiguration = new DataConnectionConfiguration()
+	            //.Use(SignalRTransportProvider.Create(signalRHttpClient))
+	            .Use(SignalRTransportProvider.Create(httpMessageHandlerFactory))
+                .Use(address)
+                .Use(accountName, spaceName, accountPassword)
+                .Use(diagnostics);
+            var connection = new DataConnectionFactory().Create(connectionConfiguration);
+
+            if (useNewSpace)
+            {
+                var managementConnection = await CreateManagementConnection();
+                var account = await managementConnection.Accounts.Get(accountName) ??
+                              await managementConnection.Accounts.Add(accountName, accountPassword, AccountTemplate.User);
+                await managementConnection.Spaces.Add(account.Id, spaceName, spaceTemplate ?? SpaceTemplate.Data);
+                await managementConnection.Close();
+            }
+
+            if (openOnCreation)
+            {
+                await connection.Open();
+            }
+            return connection;
+        }
+
+        public override async Task<IManagementConnection> CreateManagementConnection(Uri address, string account, string password, bool openOnCreation = true)
+        {
+            var diagnostics = TestDiagnostics.Create();
+	        //var signalRHttpClient = new SignalRTestHttpClient(c => ((TestInfrastructure)Context.Host.Infrastructure).Server.CreateHandler());
+	        var httpMessageHandlerFactory = new Func<HttpMessageHandler>(() => Context.Host.Server.CreateHandler());
+
+			var connectionConfiguration = new ManagementConnectionConfiguration()
+				//.Use(SignalRStorageTransportProvider.Create(signalRHttpClient))
+				//.Use(SignalRStorageTransportProvider.Create())
+				.Use(SignalRStorageTransportProvider.Create(httpMessageHandlerFactory))
+				.Use(address)
+                .Use(account, password)
+                .Use(diagnostics);
+            var connection = new ManagementConnectionFactory().Create(connectionConfiguration);
+            if (openOnCreation)
+            {
+                await connection.Open();
+            }
+            return connection;
+        }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc.Tests/TransportTestContext.cs
@@ -0,0 +1,15 @@
+// ReSharper disable once CheckNamespace
+namespace EtAlii.Ubigia.Api.Transport.Tests
+{
+    using EtAlii.Ubigia.Api.Tests;
+    using EtAlii.Ubigia.Api.Transport.SignalR.Tests;
+    using EtAlii.Ubigia.Infrastructure.Hosting;
+
+    public class TransportTestContext
+    {
+        public ITransportTestContext<InProcessInfrastructureHostTestContext> Create()
+        {
+            return new TransportTestContextFactory().Create<SignalRTransportTestContext>();
+        }
+    }
+}
