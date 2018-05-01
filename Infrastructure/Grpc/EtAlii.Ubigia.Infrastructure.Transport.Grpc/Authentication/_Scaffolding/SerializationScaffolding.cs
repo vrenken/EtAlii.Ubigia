c3ab6d8998393dc1c75a2c26af85244cd5ad52f5
Peter Vrenken <vrenken@live.nl>
2018-05-01 11:04:38 +0200

    Reverted Authentication components to Grpc based setup

---
+++ Infrastructure/Grpc/EtAlii.Ubigia.Infrastructure.Transport.Grpc/Authentication/_Scaffolding/SerializationScaffolding.cs
@@ -0,0 +1,14 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Grpc
+{
+	using EtAlii.Ubigia.Api.Transport;
+	using EtAlii.xTechnology.MicroContainer;
+
+	public class SerializationScaffolding : IScaffolding
+	{
+		public void Register(Container container)
+		{
+			// We need to use our in-house serialization. This to ensure that dictionaries, ulongs and floats are serialized correctly.
+			container.Register<ISerializer>(() => new SerializerFactory().Create());
+		}
+	}
+}
