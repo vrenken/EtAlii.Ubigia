816c92d7b216b112cfb69314f7ab154b69084e33
Peter Vrenken <vrenken@live.nl>
2018-05-09 21:12:38 +0200

    First move to shared hosttestcontext

--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Tests/Hosts/IHostTestContext.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Tests/Hosts/IHostTestContext.cs
@@ -1,29 +1,31 @@
-﻿//namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
-//{
-//	using System;
-//	using System.Threading.Tasks;
-//	using EtAlii.Ubigia.Infrastructure.Functional;
-//
-//    public partial interface IHostTestContext
-//    {
-//        string SystemAccountName { get; }
-//        string SystemAccountPassword { get; }
-//        string TestAccountName { get; }
-//        string TestAccountPassword { get; }
-//        string AdminAccountName { get; }
-//        string AdminAccountPassword { get; }
-//
-//        Uri HostAddress { get; }
-//        Uri ManagementServiceAddress { get; }
-//        Uri DataServiceAddress { get; }
-//        
-//        string HostName { get; }
-//
-//        void Start(bool useRandomPorts = false);
-//        void Stop();
-//        
-//        Task<ISystemConnection> CreateSystemConnection();
-//
-//        Task AddUserAccountAndSpaces(ISystemConnection connection, string accountName, string password, string[] spaceName);
-//    }
-//}
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Infrastructure.Functional;
+
+    public interface IHostTestContext
+    {
+        string SystemAccountName { get; }
+        string SystemAccountPassword { get; }
+        string TestAccountName { get; }
+        string TestAccountPassword { get; }
+        string AdminAccountName { get; }
+        string AdminAccountPassword { get; }
+
+        Uri HostAddress { get; }
+        Uri ManagementServiceAddress { get; }
+        Uri DataServiceAddress { get; }
+
+        string HostName { get; }
+
+        void Start(bool useRandomPorts = false);
+
+        //void Start(IHost host, IInfrastructure infrastructure);
+        void Stop();
+
+        Task<ISystemConnection> CreateSystemConnection();
+
+        Task AddUserAccountAndSpaces(ISystemConnection connection, string accountName, string password, string[] spaceNames);
+    }
+}
--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Tests/Hosts/IHostTestContextFactory.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Tests/Hosts/IHostTestContextFactory.cs
@@ -1,10 +1,10 @@
-﻿//namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
-//{
-//    public interface IHostTestContextFactory
-//    {
-//        THostTestContext Create<THostTestContext>()
-//            where THostTestContext : class, IHostTestContext, new();
-//
-//        IHostTestContext Create();
-//    }
-//}
+﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
+{
+    public interface IHostTestContextFactory
+    {
+        THostTestContext Create<THostTestContext>()
+            where THostTestContext : class, IHostTestContext, new();
+
+        IHostTestContext Create();
+    }
+}
