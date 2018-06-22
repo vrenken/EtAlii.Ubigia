2355010b7f73a7abf5b26b4181020717c1dc17a2
Peter Vrenken <vrenken@live.nl>
2018-06-23 01:32:26 +0200

    Base class

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.Grpc/Clients/GrpcManagementClientBase.cs
@@ -0,0 +1,30 @@
+﻿namespace EtAlii.Ubigia.Api.Transport.Management.Grpc
+{
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Transport.Grpc;
+
+    public abstract class GrpcManagementClientBase
+    {
+        protected IStorageConnection<IGrpcStorageTransport> Connection { get; private set; }
+
+        public async Task Connect(IStorageConnection storageConnection)
+        {
+            await Connect((IStorageConnection<IGrpcStorageTransport>)storageConnection);
+        }
+
+        public async Task Disconnect(IStorageConnection storageConnection)
+        {
+            await Disconnect((IStorageConnection<IGrpcStorageTransport>)storageConnection);
+        }
+
+        public virtual async Task Connect(IStorageConnection<IGrpcStorageTransport> storageConnection)
+        {
+            await Task.Run(() => Connection = storageConnection);
+        }
+
+        public virtual async Task Disconnect(IStorageConnection<IGrpcStorageTransport> storageConnection)
+        {
+            await Task.Run(() => Connection = null);
+        }
+    }
+}
