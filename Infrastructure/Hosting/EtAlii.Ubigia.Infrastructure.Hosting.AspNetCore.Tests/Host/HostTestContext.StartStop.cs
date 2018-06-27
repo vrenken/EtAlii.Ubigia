753583d2e1e2f3a74e5d2057deaa6cdcff2a9577
Peter Vrenken <peter.vrenken@atos.net>
2018-06-27 20:57:06 +0200

    Improved free port finding algorithm

--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.AspNetCore.Tests/Host/HostTestContext.StartStop.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.AspNetCore.Tests/Host/HostTestContext.StartStop.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Net;
 	using System.Net.NetworkInformation;
 	using EtAlii.Ubigia.Infrastructure.Functional;
 	using EtAlii.Ubigia.Infrastructure.Hosting.TestHost.AspNetCore;
@@ -111,56 +112,41 @@
 
         public void Stop()
         {
-            Host.Stop();
-            Host = null;
+	        var stopAction = new Action(() =>
+	        {
+		        Host.Stop();
+		        Host = null;
 
-            Infrastructure = null;
+		        Infrastructure = null;
 
-            SystemAccountName = null;
-            SystemAccountPassword = null;
-            TestAccountName = null;
-            TestAccountPassword = null;
+		        SystemAccountName = null;
+		        SystemAccountPassword = null;
+		        TestAccountName = null;
+		        TestAccountPassword = null;
+	        });
+	        HostMutex.ExecuteExclusive(stopAction);
         }
 
         private IReadOnlyList<int> GetAvailableTcpPorts(int startingPort, int numberOfPorts)
         {
-            var result = new List<int>();
-
-            var portArray = new List<int>();
 
             var properties = IPGlobalProperties.GetIPGlobalProperties();
 
-            // Ignore active connections
-            var connections = properties.GetActiveTcpConnections();
-            portArray.AddRange(from n in connections
-                               where n.LocalEndPoint.Port >= startingPort
-                               select n.LocalEndPoint.Port);
+            
+	        var inUsePorts = Array.Empty<int>()
+		        .Concat(properties.GetActiveTcpConnections().Select(c => c.LocalEndPoint.Port)) // Ignore active connections            
+				.Concat(properties.GetActiveTcpListeners().Select(l => l.Port)) // Ignore active tcp listners
+				.Concat(properties.GetActiveUdpListeners().Select(l => l.Port)) // Ignore active udp listeners
+				.OrderBy(p => p)
+		        .Where(p => p < startingPort);
 
-            // Ignore active tcp listners
-            var endPoints = properties.GetActiveTcpListeners();
-            portArray.AddRange(from n in endPoints
-                               where n.Port >= startingPort
-                               select n.Port);
-
-            // Ignore active udp listeners
-            endPoints = properties.GetActiveUdpListeners();
-            portArray.AddRange(from n in endPoints
-                               where n.Port >= startingPort
-                               select n.Port);
-
-            portArray.Sort();
-
-            for (var i = startingPort; i < UInt16.MaxValue; i++)
-                if (!portArray.Contains(i))
-                {
-                    result.Add(i);
-                    if (--numberOfPorts == 0)
-                    {
-                        break;
-                    }
-                };
-
-            return result;
+	        return Enumerable
+		        .Range(startingPort, IPEndPoint.MaxPort)
+		        .Except(inUsePorts)
+		        .Distinct()
+		        .GroupAdjacentBy((x, y) => x + 1 == y)
+		        .Select(g => g.ToArray())
+		        .FirstOrDefault(g => g.Length >= numberOfPorts);
         }
     }
 }
