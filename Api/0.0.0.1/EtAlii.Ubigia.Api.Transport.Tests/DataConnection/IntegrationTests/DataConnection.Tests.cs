debcbc143da16c150dece2a231d432d86fbe8a68
Peter Vrenken <github.com@petervrenken.nl>
2018-06-29 21:46:11 +0200

    Bugfix: we should never change the original accounts.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Tests/DataConnection/IntegrationTests/DataConnection.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Tests/DataConnection/IntegrationTests/DataConnection.Tests.cs
@@ -62,7 +62,7 @@
         }
 
         [Fact, Trait("Category", TestAssembly.Category)]
-        public async Task DataConnection_Open_Twice()
+        public async Task DataConnection_Open_Twice_Same()
         {
             // Arrange.
             var connection = await _testContext.TransportTestContext.CreateDataConnection(
@@ -75,6 +75,26 @@
 
             // Assert.
             await Assert.ThrowsAsync<InvalidInfrastructureOperationException>(act);
+        }
+        
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public async Task DataConnection_Open_Twice_New()
+        {
+            // Arrange.
+            var connection1 = await _testContext.TransportTestContext.CreateDataConnection(
+                _testContext.TransportTestContext.Context.SystemAccountName,
+                _testContext.TransportTestContext.Context.SystemAccountPassword, SpaceName.System, false, false);
+            await connection1.Open();
+
+            // Act.
+            var connection2 = await _testContext.TransportTestContext.CreateDataConnection(
+                _testContext.TransportTestContext.Context.SystemAccountName,
+                _testContext.TransportTestContext.Context.SystemAccountPassword, SpaceName.System, false, false);
+            await connection2.Open();
+
+            // Assert.
+            Assert.True(connection1.IsConnected);
+            Assert.True(connection2.IsConnected);
         }
 
         [Fact, Trait("Category", TestAssembly.Category)]
