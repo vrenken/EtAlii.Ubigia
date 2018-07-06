55ab3a9d74174331d86d0840adc7f2d0cb9788a4
Peter Vrenken <github.com@petervrenken.nl>
2018-07-06 20:46:21 +0200

    Some minor optimization

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.ContentDefinition.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.ContentDefinition.cs
@@ -8,7 +8,7 @@
         public async Task StoreDefinition(Api.Identifier identifier, Api.ContentDefinition contentDefinition)
         {
             var request = new ContentDefinitionPostRequest {EntryId = identifier.ToWire(), ContentDefinition = contentDefinition.ToWire()};
-            await _contentDefinitionClient.PostAsync(request, _connection.Transport.AuthenticationHeaders);
+            await _contentDefinitionClient.PostAsync(request, _transport.AuthenticationHeaders);
             //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "Post", identifier, contentDefinition);
 
             MarkAsStored(contentDefinition);
@@ -17,7 +17,7 @@
         public async Task StoreDefinition(Api.Identifier identifier, Api.ContentDefinitionPart contentDefinitionPart)
         {
             var request = new ContentDefinitionPartPostRequest {EntryId = identifier.ToWire(), ContentDefinitionPart = contentDefinitionPart.ToWire(), ContentDefinitionPartId = contentDefinitionPart.Id };
-            await _contentDefinitionClient.PostPartAsync(request, _connection.Transport.AuthenticationHeaders);
+            await _contentDefinitionClient.PostPartAsync(request, _transport.AuthenticationHeaders);
             //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "PostPart", identifier, contentDefinitionPart.Id, contentDefinitionPart);
 
             MarkAsStored(contentDefinitionPart);
--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Roots/GrpcRootDataClient.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Roots/GrpcRootDataClient.cs
@@ -7,7 +7,7 @@
     internal class GrpcRootDataClient : GrpcClientBase, IRootDataClient<IGrpcSpaceTransport>
     {
         private RootGrpcService.RootGrpcServiceClient _client;
-        private IGrpcSpaceConnection _connection;
+        private IGrpcSpaceTransport _transport;
 
         public async Task<Api.Root> Add(string name)
         {
@@ -16,14 +16,14 @@
                 Name = name,
             };
             var request = new RootSingleRequest { Root = root.ToWire(), SpaceId = Connection.Space.Id.ToWire() };
-            var response = await _client.PutAsync(request,_connection.Transport.AuthenticationHeaders);
+            var response = await _client.PutAsync(request, _transport.AuthenticationHeaders);
             return response.Root.ToLocal();
         }
 
         public async Task Remove(System.Guid rootId)
         {
             var request = new RootSingleRequest { Id = rootId.ToWire(), SpaceId = Connection.Space.Id.ToWire()}; 
-            await _client.DeleteAsync(request, _connection.Transport.AuthenticationHeaders);
+            await _client.DeleteAsync(request, _transport.AuthenticationHeaders);
         }
 
         public async Task<Api.Root> Change(System.Guid rootId, string rootName)
@@ -34,42 +34,41 @@
                 Name = rootName,
             };
             var request = new RootPostSingleRequest { Root = root.ToWire(), SpaceId = Connection.Space.Id.ToWire() };
-            var response = await _client.PostAsync(request, _connection.Transport.AuthenticationHeaders);
+            var response = await _client.PostAsync(request, _transport.AuthenticationHeaders);
             return response.Root.ToLocal();
         }
 
         public async Task<Api.Root> Get(string rootName)
         {
             var request = new RootSingleRequest { Name = rootName, SpaceId = Connection.Space.Id.ToWire() }; 
-            var response = await _client.GetSingleAsync(request, _connection.Transport.AuthenticationHeaders);
+            var response = await _client.GetSingleAsync(request, _transport.AuthenticationHeaders);
             return response.Root.ToLocal();
         }
 
         public async Task<Api.Root> Get(System.Guid rootId)
         {
             var request = new RootSingleRequest { Id = rootId.ToWire(), SpaceId = Connection.Space.Id.ToWire() }; 
-            var response = await _client.GetSingleAsync(request, _connection.Transport.AuthenticationHeaders);
+            var response = await _client.GetSingleAsync(request, _transport.AuthenticationHeaders);
             return response.Root.ToLocal();
         }
 
         public async Task<IEnumerable<Api.Root>> GetAll()
         {
             var request = new RootMultipleRequest { SpaceId = Connection.Space.Id.ToWire()}; 
-            var response = await _client.GetMultipleAsync(request, _connection.Transport.AuthenticationHeaders);
+            var response = await _client.GetMultipleAsync(request, _transport.AuthenticationHeaders);
             return response.Roots.ToLocal();
         }
 
         public override Task Connect(ISpaceConnection<IGrpcSpaceTransport> spaceConnection)
         {
-            var channel = spaceConnection.Transport.Channel;
-            _connection = (IGrpcSpaceConnection)spaceConnection;
-            _client = new RootGrpcService.RootGrpcServiceClient(channel);
+            _transport = ((IGrpcSpaceConnection)spaceConnection).Transport;
+            _client = new RootGrpcService.RootGrpcServiceClient(_transport.Channel);
             return Task.CompletedTask;
         }
 
         public override Task Disconnect(ISpaceConnection<IGrpcSpaceTransport> spaceConnection)
         {
-            _connection = null;
+            _transport = null;
             _client = null;
             return Task.CompletedTask;
         }
