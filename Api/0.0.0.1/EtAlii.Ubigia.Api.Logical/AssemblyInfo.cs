db49807edd00768b974813102b527f893acd8799
Peter Vrenken <peter.vrenken@atos.net>
2018-07-10 08:23:14 +0200

    Made Grpc test compile.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/AssemblyInfo.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/AssemblyInfo.cs
@@ -4,7 +4,8 @@
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47")]
 
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Logical.Tests")]
-[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.SignalR.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.Grpc.Tests")]
 
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Windows.Diagnostics.SpaceBrowser")]
 [assembly: InternalsVisibleTo("SpaceBrowser")]
