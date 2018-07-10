fcff48e3bfd397ec5cf0efe5ddddab556d9c0400
Peter Vrenken <peter.vrenken@atos.net>
2018-07-10 08:23:14 +0200

    Made Grpc test compile.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional/AssemblyInfo.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional/AssemblyInfo.cs
@@ -3,7 +3,8 @@
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Diagnostics")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Tests")]
-[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.SignalR.Tests")]
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.NET47.Grpc.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Transport.SignalR.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Transport.WebApi.Tests")]
 
