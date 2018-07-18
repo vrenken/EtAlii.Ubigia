50de44c9b54740a1ea32471bca1df8d5b49f0df2
Peter Vrenken <github.com@petervrenken.nl>
2018-07-18 23:43:48 +0200

    Added try catch to the grpc properties data client

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Properties/GrpcPropertiesDataClient.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Properties/GrpcPropertiesDataClient.cs
@@ -2,7 +2,8 @@
 {
     using System.Threading.Tasks;
     using EtAlii.Ubigia.Api.Transport.Grpc.WireProtocol;
-    
+    using global::Grpc.Core;
+
     internal class GrpcPropertiesDataClient : GrpcClientBase, IPropertiesDataClient<IGrpcSpaceTransport>
     {
         private IGrpcSpaceTransport _transport;
@@ -10,27 +11,41 @@
 
         public async Task Store(Api.Identifier identifier, Api.PropertyDictionary properties, ExecutionScope scope)
         {
-            var request = new PropertiesPostRequest{EntryId = identifier.ToWire(), PropertyDictionary = properties.ToWire()};
-            await _client.PostAsync(request, _transport.AuthenticationHeaders);
-            //await _invoker.Invoke(_connection, GrpcHub.Property, "Post", identifier, properties);
-            PropertiesHelper.SetStored(properties, true);
+            try
+            {
+                var request = new PropertiesPostRequest{EntryId = identifier.ToWire(), PropertyDictionary = properties.ToWire()};
+                await _client.PostAsync(request, _transport.AuthenticationHeaders);
+                //await _invoker.Invoke(_connection, GrpcHub.Property, "Post", identifier, properties);
+                PropertiesHelper.SetStored(properties, true);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcPropertiesDataClient)}.Store()", e);
+            }
         }
 
         public async Task<Api.PropertyDictionary> Retrieve(Api.Identifier identifier, ExecutionScope scope)
         {
-            return await scope.Cache.GetProperties(identifier, async () =>
+            try
             {
-                var request = new PropertiesGetRequest{ EntryId = identifier.ToWire() };
-                var response = await _client.GetAsync(request, _transport.AuthenticationHeaders);
-                var result = response.PropertyDictionary.ToLocal();
-                
-                if (result != null)
+                return await scope.Cache.GetProperties(identifier, async () =>
                 {
-                    PropertiesHelper.SetStored(result, true);
-                    // properties.Stored is not serialized in the PropertyDictionaryConverter.
-                }
-                return result;
-            });
+                    var request = new PropertiesGetRequest{ EntryId = identifier.ToWire() };
+                    var response = await _client.GetAsync(request, _transport.AuthenticationHeaders);
+                    var result = response.PropertyDictionary.ToLocal();
+                    
+                    if (result != null)
+                    {
+                        PropertiesHelper.SetStored(result, true);
+                        // properties.Stored is not serialized in the PropertyDictionaryConverter.
+                    }
+                    return result;
+                });
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcPropertiesDataClient)}.Retrieve()", e);
+            }
         }
         
         public override async Task Connect(ISpaceConnection<IGrpcSpaceTransport> spaceConnection)
