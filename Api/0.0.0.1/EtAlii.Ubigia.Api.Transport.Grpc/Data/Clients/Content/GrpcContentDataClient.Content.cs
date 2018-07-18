1aaeb5e04585937bd974c4ca00e392e9ed486c90
Peter Vrenken <github.com@petervrenken.nl>
2018-07-18 15:26:39 +0200

    Introduced exceptions into Grpc Content related data clients.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.Content.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.Content.cs
@@ -2,42 +2,72 @@
 {
     using System.Threading.Tasks;
     using EtAlii.Ubigia.Api.Transport.Grpc.WireProtocol;
+    using global::Grpc.Core;
 
     internal partial class GrpcContentDataClient
     {
         public async Task Store(Api.Identifier identifier, Api.Content content)
         {
-            var request = new ContentPostRequest {EntryId = identifier.ToWire(), Content = content.ToWire()};
-            await _contentClient.PostAsync(request, _transport.AuthenticationHeaders);
-            //await _invoker.Invoke(_contentConnection, GrpcHub.Content, "Post", identifier, content);
+            try
+            {
+                var request = new ContentPostRequest {EntryId = identifier.ToWire(), Content = content.ToWire()};
+                await _contentClient.PostAsync(request, _transport.AuthenticationHeaders);
+                //await _invoker.Invoke(_contentConnection, GrpcHub.Content, "Post", identifier, content);
+    
+                // TODO: Should this call be replaced by get instead? 
+                BlobHelper.SetStored(content, true);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
 
-            // TODO: Should this call be replaced by get instead? 
-            BlobHelper.SetStored(content, true);
         }
 
         public async Task Store(Api.Identifier identifier, Api.ContentPart contentPart)
         {
-            var request = new ContentPartPostRequest {EntryId = identifier.ToWire(), ContentPart = contentPart.ToWire(), ContentPartId = contentPart.Id };
-            await _contentClient.PostPartAsync(request, _transport.AuthenticationHeaders);
-            //await _invoker.Invoke(_contentConnection, GrpcHub.Content, "PostPart", identifier, contentPart.Id, contentPart);
-
-            BlobPartHelper.SetStored(contentPart, true);
+            try
+            {
+                var request = new ContentPartPostRequest {EntryId = identifier.ToWire(), ContentPart = contentPart.ToWire(), ContentPartId = contentPart.Id };
+                await _contentClient.PostPartAsync(request, _transport.AuthenticationHeaders);
+                //await _invoker.Invoke(_contentConnection, GrpcHub.Content, "PostPart", identifier, contentPart.Id, contentPart);
+    
+                BlobPartHelper.SetStored(contentPart, true);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
         }
 
         public async Task<IReadOnlyContent> Retrieve(Api.Identifier identifier)
         {
-            var request = new ContentGetRequest { EntryId = identifier.ToWire() };
-            var response =  await _contentClient.GetAsync(request);
-            return response.Content.ToLocal();
-            //return await _invoker.Invoke<Content>(_contentConnection, GrpcHub.Content, "Get", identifier);
+            try
+            {
+                var request = new ContentGetRequest { EntryId = identifier.ToWire() };
+                var response =  await _contentClient.GetAsync(request);
+                return response.Content.ToLocal();
+                //return await _invoker.Invoke<Content>(_contentConnection, GrpcHub.Content, "Get", identifier);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
         }
 
         public async Task<IReadOnlyContentPart> Retrieve(Api.Identifier identifier, ulong contentPartId)
         {
-            var request = new ContentPartGetRequest { EntryId = identifier.ToWire(), ContentPartId = contentPartId};
-            var response =  await _contentClient.GetPartAsync(request);
-            return response.ContentPart.ToLocal();
-            //return await _invoker.Invoke<ContentPart>(_contentConnection, GrpcHub.Content, "GetPart", identifier, contentPartId);
+            try
+            {
+                var request = new ContentPartGetRequest { EntryId = identifier.ToWire(), ContentPartId = contentPartId};
+                var response =  await _contentClient.GetPartAsync(request);
+                return response.ContentPart.ToLocal();
+                //return await _invoker.Invoke<ContentPart>(_contentConnection, GrpcHub.Content, "GetPart", identifier, contentPartId);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
         }
     }
 }
--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.ContentDefinition.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Grpc/Data/Clients/Content/GrpcContentDataClient.ContentDefinition.cs
@@ -2,33 +2,55 @@
 {
     using System.Threading.Tasks;
     using EtAlii.Ubigia.Api.Transport.Grpc.WireProtocol;
+    using global::Grpc.Core;
 
     internal partial class GrpcContentDataClient
     {
         public async Task StoreDefinition(Api.Identifier identifier, Api.ContentDefinition contentDefinition)
         {
-            var request = new ContentDefinitionPostRequest {EntryId = identifier.ToWire(), ContentDefinition = contentDefinition.ToWire()};
-            await _contentDefinitionClient.PostAsync(request, _transport.AuthenticationHeaders);
-            //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "Post", identifier, contentDefinition);
-
-            MarkAsStored(contentDefinition);
+            try
+            {
+                var request = new ContentDefinitionPostRequest {EntryId = identifier.ToWire(), ContentDefinition = contentDefinition.ToWire()};
+                await _contentDefinitionClient.PostAsync(request, _transport.AuthenticationHeaders);
+                //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "Post", identifier, contentDefinition);
+    
+                MarkAsStored(contentDefinition);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
         }
 
         public async Task StoreDefinition(Api.Identifier identifier, Api.ContentDefinitionPart contentDefinitionPart)
         {
-            var request = new ContentDefinitionPartPostRequest {EntryId = identifier.ToWire(), ContentDefinitionPart = contentDefinitionPart.ToWire(), ContentDefinitionPartId = contentDefinitionPart.Id };
-            await _contentDefinitionClient.PostPartAsync(request, _transport.AuthenticationHeaders);
-            //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "PostPart", identifier, contentDefinitionPart.Id, contentDefinitionPart);
-
-            MarkAsStored(contentDefinitionPart);
+            try
+            {
+                var request = new ContentDefinitionPartPostRequest {EntryId = identifier.ToWire(), ContentDefinitionPart = contentDefinitionPart.ToWire(), ContentDefinitionPartId = contentDefinitionPart.Id };
+                await _contentDefinitionClient.PostPartAsync(request, _transport.AuthenticationHeaders);
+                //await _invoker.Invoke(_contentDefinitionConnection, GrpcHub.ContentDefinition, "PostPart", identifier, contentDefinitionPart.Id, contentDefinitionPart);
+    
+                MarkAsStored(contentDefinitionPart);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.StoreDefinition()", e);
+            }
         }
 
         public async Task<IReadOnlyContentDefinition> RetrieveDefinition(Api.Identifier identifier)
         {
-            var request = new ContentDefinitionGetRequest { EntryId = identifier.ToWire() };
-            var response =  await _contentDefinitionClient.GetAsync(request);
-            return response.ContentDefinition.ToLocal();
-            //return await _invoker.Invoke<ContentDefinition>(_contentDefinitionConnection, GrpcHub.ContentDefinition, "Get", identifier);
+            try
+            {
+                var request = new ContentDefinitionGetRequest { EntryId = identifier.ToWire() };
+                var response =  await _contentDefinitionClient.GetAsync(request);
+                return response.ContentDefinition.ToLocal();
+                //return await _invoker.Invoke<ContentDefinition>(_contentDefinitionConnection, GrpcHub.ContentDefinition, "Get", identifier);
+            }
+            catch (RpcException e)
+            {
+                throw new InvalidInfrastructureOperationException($"{nameof(GrpcContentDataClient)}.RetrieveDefinition()", e);
+            }
         }
 
         private void MarkAsStored(Api.ContentDefinition contentDefinition)
