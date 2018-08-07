6117f318c57e1416d03dc5a92f40d4c595541b12
Peter Vrenken <github.com@petervrenken.nl>
2018-08-07 21:21:37 +0200

    Cleanup

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphQL/GraphQL/Dynamic/DynamicSchema.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphQL/GraphQL/Dynamic/DynamicSchema.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using global::GraphQL;
     using global::GraphQL.Execution;
     using global::GraphQL.Language.AST;
     using global::GraphQL.Types;
