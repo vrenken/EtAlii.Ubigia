2e85e6025f5fb3d0b1856198cb9b25fbf10f50ad
Peter Vrenken <vrenken@live.nl>
2018-08-20 23:10:20 +0200

    Added definition files

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphQL/GraphQL/Dynamic/_Model/Relation.cs
@@ -0,0 +1,12 @@
+﻿namespace EtAlii.Ubigia.Api.Functional.Querying.GraphQL
+{
+
+    using System.Collections.Generic;
+    using System;
+
+    public class Relation
+    {
+        public string Path { get; set; }
+        public TypeDefinition Type { get; set; }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphQL/GraphQL/Dynamic/_Model/TypeDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EtAlii.Ubigia.Api.Functional.Querying.GraphQL
+{
+    using System;
+    using global::GraphQL.Language.AST;
+    using global::GraphQL.Types;
+
+    public class TypeDefinition
+    {
+        public string Name { get; set; }
+        public Relation[] Children { get; set; } = Array.Empty<Relation>();
+        public FieldDefinition[] Fields { get; set; } = Array.Empty<FieldDefinition>();
+    }
+}
