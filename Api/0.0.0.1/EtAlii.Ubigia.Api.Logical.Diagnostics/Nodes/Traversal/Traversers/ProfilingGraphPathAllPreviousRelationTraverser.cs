eed417052cfe1e060e00ac8cb196ebd38ea5924c
Peter Vrenken <vrenken@live.nl>
2018-08-27 21:01:06 +0200

    Changes to logical layer.
    all updates/downdates/previous/next etc.

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical.Diagnostics/Nodes/Traversal/Traversers/ProfilingGraphPathAllPreviousRelationTraverser.cs
@@ -0,0 +1,44 @@
+namespace EtAlii.Ubigia.Api.Logical.Diagnostics
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Diagnostics.Profiling;
+    using EtAlii.Ubigia.Api.Logical;
+
+    public class ProfilingGraphPathAllPreviousRelationTraverser : IGraphPathAllPreviousRelationTraverser
+    {
+        private readonly IGraphPathAllPreviousRelationTraverser _decoree;
+        private readonly IProfiler _profiler;
+
+        public ProfilingGraphPathAllPreviousRelationTraverser(
+            IGraphPathAllPreviousRelationTraverser decoree, 
+            IProfiler profiler)
+        {
+            _decoree = decoree;
+            _profiler = profiler.Create(ProfilingAspects.Logical.Traversers);
+        }
+
+        public void Configure(TraversalParameters parameters)
+        {
+            dynamic profile = _profiler.Begin("Configuring relation traversing: ALL PREVIOUS");
+            profile.Part = parameters.Part;
+
+            _decoree.Configure(parameters);
+
+            _profiler.End(profile);
+        }
+
+        public async Task<IEnumerable<Identifier>> Traverse(GraphPathPart part, Identifier start, ITraversalContext context, ExecutionScope scope)
+        {
+            dynamic profile = _profiler.Begin("Traversing relation: ALL PREVIOUS");
+            profile.Part = part;
+            profile.Start = start;
+
+            var result = await _decoree.Traverse(part, start, context, scope);
+
+            _profiler.End(profile);
+
+            return result;
+        }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical.Diagnostics/Nodes/Traversal/Traversers/ProfilingGraphPathChildrenRelationTraverser.cs
@@ -0,0 +1,42 @@
+namespace EtAlii.Ubigia.Api.Logical.Diagnostics
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Diagnostics.Profiling;
+    using EtAlii.Ubigia.Api.Logical;
+
+    public class ProfilingGraphPathChildrenRelationTraverser : IGraphPathChildrenRelationTraverser
+    {
+        private readonly IGraphPathChildrenRelationTraverser _decoree;
+        private readonly IProfiler _profiler;
+
+        public ProfilingGraphPathChildrenRelationTraverser(IGraphPathChildrenRelationTraverser decoree, IProfiler profiler)
+        {
+            _decoree = decoree;
+            _profiler = profiler.Create(ProfilingAspects.Logical.Traversers);
+        }
+
+        public void Configure(TraversalParameters parameters)
+        {
+            dynamic profile = _profiler.Begin("Configuring relation traversing: CHILDREN");
+            profile.Part = parameters.Part;
+
+            _decoree.Configure(parameters);
+
+            _profiler.End(profile);
+        }
+
+        public async Task<IEnumerable<Identifier>> Traverse(GraphPathPart part, Identifier start, ITraversalContext context, ExecutionScope scope)
+        {
+            dynamic profile = _profiler.Begin("Traversing relation: CHILDREN");
+            profile.Part = part;
+            profile.Start = start;
+
+            var result = await _decoree.Traverse(part, start, context, scope);
+
+            _profiler.End(profile);
+
+            return result;
+        }
+    }
+}
