22e56cd8cd9efa4d601d6493f073ac4439ba75b6
Peter Vrenken <vrenken@live.nl>
2018-11-17 09:59:33 +0100

    Small extraction of assert logic into dedicated class.

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphQL/Integration tests/AssertQuery.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Http;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace EtAlii.Ubigia.Api.Functional.Tests
+{
+    public static class AssertQuery
+    {
+        public static async Task ResultsAreSame(IDocumentWriter documentWriter, string expected, ExecutionResult actual)
+        {
+            var expectedResult = new ExecutionResult { Data = JObject.Parse(expected) };
+            await ResultsAreSame(documentWriter, expectedResult, actual);
+        }
+        
+        public static async Task ResultsAreSame(IDocumentWriter documentWriter, ExecutionResult expected, ExecutionResult actual)
+        {
+//            var expectedDocument = JObject.Parse(expected);
+            var expectedString = await documentWriter.WriteToStringAsync(expected);
+            var actualString = await documentWriter.WriteToStringAsync(actual);
+            Assert.Equal(expectedString, actualString);
+        }
+    }
+}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphQL/Integration tests/GraphQLQueryContext.Full.Tests.cs
@@ -0,0 +1,82 @@
+﻿namespace EtAlii.Ubigia.Api.Functional.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using GraphQL;
+    using GraphQL.Http;
+    using Newtonsoft.Json.Linq;
+    using Xunit;
+
+
+    public class GraphQLQueryContextFullTests : IClassFixture<QueryingUnitTestContext>, IDisposable
+    {
+        private IDataContext _dataContext;
+        private IGraphQLQueryContext _context;
+        private readonly QueryingUnitTestContext _testContext;
+        private readonly IDocumentWriter _documentWriter;
+
+        public GraphQLQueryContextFullTests(QueryingUnitTestContext testContext)
+        {
+            _testContext = testContext;
+            _documentWriter = new DocumentWriter(indent: false);
+                
+            TestInitialize();
+        }
+
+        public void Dispose()
+        {
+            TestCleanup();
+        }
+
+        private void TestInitialize()
+        {
+            var task = Task.Run(async () =>
+            {
+                var start = Environment.TickCount;
+
+                _dataContext = await _testContext.FunctionalTestContext.CreateFunctionalContext(true);
+                _context = new GraphQLQueryContextFactory().Create(_dataContext);
+                
+                await _testContext.FunctionalTestContext.AddJohnDoe(_dataContext);
+                await _testContext.FunctionalTestContext.AddTonyStark(_dataContext);
+
+                Console.WriteLine("DataContext_Nodes.Initialize: {0}ms", TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds);
+            });
+            task.Wait();
+        }
+
+        private void TestCleanup()
+        {
+            var task = Task.Run(() =>
+            {
+                var start = Environment.TickCount;
+
+                _context = null;
+
+                Console.WriteLine("DataContext_Nodes.Cleanup: {0}ms", TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds);
+            });
+            task.Wait();
+        }
+
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public async Task GraphQL_Query_Traverse_Person()
+        {
+            // Arrange.
+            var query = @"
+                @traverse(path:""person:Stark/Tony"") 
+                query data 
+                { 
+                    person 
+                    { 
+                        nickname 
+                    } 
+                }";
+            
+            // Act.
+            var result = await _context.Execute("Query", query, new Inputs());
+            
+            // Assert.
+            await AssertQueryResultsAreSame(@"{ ""person"": { ""nickname"": ""Iron Man"" }}", result);
+        }
+    }
+}
