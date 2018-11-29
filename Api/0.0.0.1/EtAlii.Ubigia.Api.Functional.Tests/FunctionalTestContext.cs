edb09b9aac2270bbbf71035f5521e8a57db47ab1
Peter Vrenken <vrenken@live.nl>
2018-11-29 18:25:38 +0100

    Added Friends testdata

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Tests/FunctionalTestContext.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Tests/FunctionalTestContext.cs
@@ -34,6 +34,12 @@ namespace EtAlii.Ubigia.Api.Functional.Tests
             await AddJohnDoe(context);
             await AddJaneDoe(context);
             await AddTonyStark(context);
+            await AddPeterVrenken(context);
+            await AddTanjaVrenken(context);
+            await AddArjanVrenken(context);
+            await AddIdaVrenken(context);
+
+            await AddFriends(context);
         }
 
         public async Task AddAddresses(IDataContext context)
@@ -51,6 +57,56 @@ namespace EtAlii.Ubigia.Api.Functional.Tests
             await context.Scripts.Process(addQuery);
         }
 
+        public async Task AddPeterVrenken(IDataContext context)
+        {
+            var addQueries = new[]
+            {
+                "Person:+=Vrenken/Peter",
+                "Person:Vrenken/Peter <= { Birthdate: 1978-07-28, Nickname: \'Pete\', Lives: 1 }"
+            };
+            var addQuery = String.Join("\r\n", addQueries);
+
+
+            await context.Scripts.Process(addQuery);
+        }
+
+        public async Task AddTanjaVrenken(IDataContext context)
+        {
+            var addQueries = new[]
+            {
+                "Person:+=Vrenken/Tanja",
+                "Person:Vrenken/Tanja <= { Birthdate: 1980-03-04, Nickname: \'LadyL\', Lives: 1 }"
+            };
+            var addQuery = String.Join("\r\n", addQueries);
+
+            await context.Scripts.Process(addQuery);
+        }
+
+        public async Task AddArjanVrenken(IDataContext context)
+        {
+            var addQueries = new[]
+            {
+                "Person:+=Vrenken/Arjan",
+                "Person:Vrenken/Arjan <= { Birthdate: 2015-08-13, Nickname: \'Bengel\', Lives: 1 }"
+            };
+            var addQuery = String.Join("\r\n", addQueries);
+
+            await context.Scripts.Process(addQuery);
+        }
+        
+        
+        public async Task AddIdaVrenken(IDataContext context)
+        {
+            var addQueries = new[]
+            {
+                "Person:+=Vrenken/Ida",
+                "Person:Vrenken/Ida <= { Birthdate: 2018-11-07, Nickname: \'Scheetje\', Lives: 1 }"
+            };
+            var addQuery = String.Join("\r\n", addQueries);
+
+            await context.Scripts.Process(addQuery);
+        }
+        
         public async Task AddJohnDoe(IDataContext context)
         {
             var addQueries = new[]
@@ -77,20 +133,72 @@ namespace EtAlii.Ubigia.Api.Functional.Tests
             await context.Scripts.Process(addQuery);
         }
 
-        
         public async Task AddTonyStark(IDataContext context)
         {
             var addQueries = new[]
             {
                 "Person:+=Stark/Tony",
-                "Person:Stark/Tony <= { Birthdate: 1976-05-12, Nickname: \'Iron Man\', Lives: 9 }"
+                "Person:Stark/Tony <= { Birthdate: 1976-05-12, Nickname: \'Iron Man\', Lives: 9 }",
             };
             var addQuery = String.Join("\r\n", addQueries);
 
 
             await context.Scripts.Process(addQuery);
         }
-        
+
+        public async Task AddFriends(IDataContext context)
+        {
+            var addQueries = new[]
+            {
+                "$john <= Person:Doe/John",
+                "$jane <= Person:Doe/Jane",
+                "$tony <= Person:Stark/Tony",
+                "$peter <= Person:Vrenken/Peter",
+                "$tanja <= Person:Vrenken/Tanja",
+                "$arjan <= Person:Vrenken/Arjan",
+                "$ida <= Person:Vrenken/Ida",
+                
+                "Person:Stark/Tony/ += Friends",
+                "Person:Stark/Tony/Friends += $john",
+                "Person:Stark/Tony/Friends += $jane",
+                "Person:Stark/Tony/Friends += $peter",
+                
+                "Person:Doe/John/ += Friends",
+                "Person:Doe/John/Friends += $tony",
+                "Person:Doe/John/Friends += $jane",
+                
+                "Person:Doe/Jane/ += Friends",
+                "Person:Doe/Jane/Friends += $john",
+                "Person:Doe/Jane/Friends += $tony",
+
+                "Person:Vrenken/Peter/ += Friends",
+                "Person:Vrenken/Peter/Friends += $tanja",
+                "Person:Vrenken/Peter/Friends += $arjan",
+                "Person:Vrenken/Peter/Friends += $ida",
+                "Person:Vrenken/Peter/Friends += $tony",
+
+                "Person:Vrenken/Tanja/ += Friends",
+                "Person:Vrenken/Tanja/Friends += $peter",
+                "Person:Vrenken/Tanja/Friends += $arjan",
+                "Person:Vrenken/Tanja/Friends += $ida",
+
+                "Person:Vrenken/Arjan/ += Friends",
+                "Person:Vrenken/Arjan/Friends += $tanja",
+                "Person:Vrenken/Arjan/Friends += $peter",
+                "Person:Vrenken/Arjan/Friends += $ida",
+
+                "Person:Vrenken/Ida/ += Friends",
+                "Person:Vrenken/Ida/Friends += $tanja",
+                "Person:Vrenken/Ida/Friends += $arjan",
+                "Person:Vrenken/Ida/Friends += $peter",
+            };
+            var addQuery = String.Join("\r\n", addQueries);
+
+
+            await context.Scripts.Process(addQuery);
+            
+        }
+
         #region start/stop
         
         public async Task Start()
