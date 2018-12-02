c255efa7b86fe6b5f12191822c7a8c327b22f58f
Peter Vrenken <vrenken@live.nl>
2018-12-02 23:06:47 +0100

    Small restructuring

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional/Scripting/Roots/Handlers/Default/Person/PersonByLastNameFirstNameHandler.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional/Scripting/Roots/Handlers/Default/Person/PersonByLastNameFirstNameHandler.cs
@@ -12,14 +12,20 @@ namespace EtAlii.Ubigia.Api.Functional
         {
             Template = new PathSubjectPart[]
             {
-                new TypedPathSubjectPart(TypedPathFormatter.Name.LastNameFormatter), new ParentPathSubjectPart(),
+                new TypedPathSubjectPart(TypedPathFormatter.Name.LastNameFormatter), 
+                new ParentPathSubjectPart(),
                 new TypedPathSubjectPart(TypedPathFormatter.Name.FirstNameFormatter)
             };
         }
 
         public void Process(IRootContext context, PathSubjectPart[] match, PathSubjectPart[] rest, ExecutionScope scope, IObserver<object> output)
         {
-            var parts = new PathSubjectPart[] { new ParentPathSubjectPart(), new ConstantPathSubjectPart("Person"), new ParentPathSubjectPart() }
+            var parts = new PathSubjectPart[]
+                {
+                    new ParentPathSubjectPart(), 
+                    new ConstantPathSubjectPart("Person"), 
+                    new ParentPathSubjectPart()
+                }
                .Concat(match)
                .Concat(rest)
                .ToArray();
