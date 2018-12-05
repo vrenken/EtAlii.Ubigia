0896ceaec1084374df5d4883a2d5210801fbc485
Peter Vrenken <vrenken@live.nl>
2018-12-05 11:02:32 +0100

    Moved code from Shared to _Shared

---
+++ Client/Windows/Diagnostics/SpaceBrowser/_Shared/Converters/ItemsToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+namespace EtAlii.Ubigia.Windows.Diagnostics.SpaceBrowser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class ItemsToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var items = value as IEnumerable<object>;
+            bool isVisible = items != null && items.Any();
+            if ((string)parameter == "negate") isVisible = !isVisible;
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
