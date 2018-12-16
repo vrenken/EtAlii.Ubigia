a7d5032cb66080f3db7bff6a756a921a2adf5d40
Peter Vrenken <vrenken@live.nl>
2018-12-16 18:26:08 +0100

    Small tab rename

--- Client/Windows/Diagnostics/SpaceBrowser/Documents/Commands/NewGraphScriptLanguageDocumentCommand.cs
+++ Client/Windows/Diagnostics/SpaceBrowser/Documents/Commands/NewGraphScriptLanguageDocumentCommand.cs
@@ -15,8 +15,8 @@
             DocumentFactory = factory;
             Header = "Script";
             Icon = @"pack://siteoforigin:,,,/Images/File-Format-GraphQuery.png";
-            TitleFormat = "Script view {0}";
-            InfoLine = "Create a document to invoke scripts on a space";
+            TitleFormat = "GraphSL view {0}";
+            InfoLine = "Create a document to invoke GSL scripts on a space";
             InfoTip1 = "Allows execution scripts written in the GSL language";
             InfoTip2 = "Useful for advanced space operations";
         }
