cbd9047a09be3c93d2fd8326cc23ddc7bc9c2d37
Peter Vrenken <vrenken@live.nl>
2019-01-16 00:33:04 +0100

    Small fix to make sonarqube operational

--- Infrastructure/Hosting/EtAlii.Ubigia.Hosting.Local.Tests/Properties/AssemblyInfo.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Hosting.Local.Tests/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EtAlii.Ubigia.Hosting.Local.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EtAlii.Ubigia.Hosting.Local.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("EtAlii.Ubigia.Hosting.Local.Tests")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("EtAlii.Ubigia.Hosting.Local.Tests")]
+//[assembly: AssemblyCopyright("Copyright ©  2014")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
