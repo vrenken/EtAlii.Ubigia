ab5f21d64b9612e84526146259ec160c5799f265
Peter Vrenken <github.com@petervrenken.nl>
2019-01-20 02:35:32 +0100

    Cleaned up unused namespaces

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/4. Processing/Operators/Assign/ToOutput/AssignCombinedToOutputOperatorSubProcessor.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/4. Processing/Operators/Assign/ToOutput/AssignCombinedToOutputOperatorSubProcessor.cs
@@ -1,6 +1,5 @@
 namespace EtAlii.Ubigia.Api.Functional
 {
-    using System;
     using System.Threading.Tasks;
 
     internal class AssignCombinedToOutputOperatorSubProcessor : IAssignCombinedToOutputOperatorSubProcessor
