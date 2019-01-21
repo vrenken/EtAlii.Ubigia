d0aa41c09778276087967c335ec03d6f27cc04f2
Peter Vrenken <github.com@petervrenken.nl>
2019-01-21 23:14:32 +0100

    Code cleanup

--- Client/Windows/Console/EtAlii.Ubigia.PowerShell/Entries/Select-Entries.cs
+++ Client/Windows/Console/EtAlii.Ubigia.PowerShell/Entries/Select-Entries.cs
@@ -7,10 +7,12 @@
     {
         protected override void BeginProcessing()
         {
+            // Do stuff before processing has started.
         }
 
         protected override void EndProcessing()
         {
+            // Do stuff after processing has started.
         }
     }
 }
