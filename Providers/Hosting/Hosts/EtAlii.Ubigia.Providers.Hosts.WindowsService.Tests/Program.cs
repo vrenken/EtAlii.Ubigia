dbaa6bc3daf7e9af7ae414c4ec72d09d081d8aa4
Peter Vrenken <github.com@petervrenken.nl>
2019-01-22 23:50:12 +0100

    Cleaned up code

--- Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.Tests/Program.cs
+++ Providers/Hosting/Hosts/EtAlii.Ubigia.Providers.Hosts.WindowsService.Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace EtAlii.Ubigia.Provisioning.Hosting.ConsoleHost.Net47
 {
+    using System;
     using EtAlii.xTechnology.Hosting;
     using global::Microsoft.Extensions.Configuration;
 
@@ -8,6 +9,7 @@
         /// <summary>
         /// The main entry point for the application. 
         /// </summary>
+        [STAThread]
         public static void Main()
         {
 	        var applicationConfiguration = new ConfigurationBuilder()
