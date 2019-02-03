819efd721fadd2f36b34ca64e5fb50a0c54947dc
Peter Vrenken <github.com@petervrenken.nl>
2019-02-03 13:29:50 +0100

    Code cleanup

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.Dynamic.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.Dynamic.Tests.cs
@@ -15,9 +15,26 @@
             _parser = new ScriptParserFactory().Create(scriptParserConfiguration);
         }
 
+
         public void Dispose()
         {
-            _parser = null;
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+            if (disposing)
+            {
+                _parser = null;
+            }
+        }
+
+        ~ScriptProcessorAssignDynamicUnitTests()
+        {
+            Dispose(false);
+        }
+
     }
 }
--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.String.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.String.Tests.cs
@@ -22,9 +22,25 @@
             _parser = new ScriptParserFactory().Create(scriptParserConfiguration);
         }
 
+
         public void Dispose()
         {
-            _parser = null;
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+            if (disposing)
+            {
+                _parser = null;
+            }
+        }
+
+        ~ScriptProcessorAssignStringUnitTests()
+        {
+            Dispose(false);
         }
 
         [Fact, Trait("Category", TestAssembly.Category)]
