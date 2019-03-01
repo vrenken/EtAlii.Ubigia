c5f37bdaf41d2d3de497271602891edbda8175c4
Peter Vrenken <vrenken@live.nl>
2019-03-01 18:39:14 +0100

    Updated NuGet packages

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.Grpc/Clients/_Grpc/Account.Grpc.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Management.Grpc/Clients/_Grpc/Account.Grpc.cs
@@ -218,17 +218,17 @@ namespace EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol {
           .AddMethod(__Method_Delete, serviceImpl.Delete).Build();
     }
 
-    /// <summary>Register service method implementations with a service binder. Useful when customizing the service binding logic.
+    /// <summary>Register service method with a service binder with or without implementation. Useful when customizing the  service binding logic.
     /// Note: this method is part of an experimental API that can change or be removed without any prior notice.</summary>
     /// <param name="serviceBinder">Service methods will be bound by calling <c>AddMethod</c> on this object.</param>
     /// <param name="serviceImpl">An object implementing the server-side handling logic.</param>
     public static void BindService(grpc::ServiceBinderBase serviceBinder, AccountGrpcServiceBase serviceImpl)
     {
-      serviceBinder.AddMethod(__Method_GetSingle, serviceImpl.GetSingle);
-      serviceBinder.AddMethod(__Method_GetMultiple, serviceImpl.GetMultiple);
-      serviceBinder.AddMethod(__Method_Post, serviceImpl.Post);
-      serviceBinder.AddMethod(__Method_Put, serviceImpl.Put);
-      serviceBinder.AddMethod(__Method_Delete, serviceImpl.Delete);
+      serviceBinder.AddMethod(__Method_GetSingle, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleRequest, global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleResponse>(serviceImpl.GetSingle));
+      serviceBinder.AddMethod(__Method_GetMultiple, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountMultipleRequest, global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountMultipleResponse>(serviceImpl.GetMultiple));
+      serviceBinder.AddMethod(__Method_Post, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountPostSingleRequest, global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleResponse>(serviceImpl.Post));
+      serviceBinder.AddMethod(__Method_Put, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleRequest, global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleResponse>(serviceImpl.Put));
+      serviceBinder.AddMethod(__Method_Delete, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleRequest, global::EtAlii.Ubigia.Api.Transport.Management.Grpc.WireProtocol.AccountSingleResponse>(serviceImpl.Delete));
     }
 
   }
