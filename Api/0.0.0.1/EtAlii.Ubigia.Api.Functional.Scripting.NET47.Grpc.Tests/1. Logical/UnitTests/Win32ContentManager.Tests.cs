86131ac65707c0ed9f8623852d69024781b68012
Peter Vrenken <github.com@petervrenken.nl>
2019-04-02 22:02:46 +0200

    Removing Task.Run

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.NET47.Grpc.Tests/1. Logical/UnitTests/Win32ContentManager.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.NET47.Grpc.Tests/1. Logical/UnitTests/Win32ContentManager.Tests.cs
@@ -1,10 +1,10 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.NET47.Tests
 {
-    using System;
     using System.IO;
+    using System.Threading.Tasks;
+    using Xunit;
 
-    
-    public class NET47ContentManager2Tests : IDisposable
+    public class NET47ContentManager2Tests : IAsyncLifetime
     {
         private readonly string _testImageFileName;
 
@@ -12,16 +12,19 @@
         {
             // Getting Temp file name to use
             _testImageFileName = NET47TestHelper.CreateTemporaryFileName();
-
-            NET47TestHelper.SaveResourceTestImage(_testImageFileName);
+        }
+        public async Task InitializeAsync()
+        {
+            await NET47TestHelper.SaveResourceTestImage(_testImageFileName);
         }
 
-        public void Dispose()
+        public Task DisposeAsync()
         {
             if (File.Exists(_testImageFileName))
             {
                 File.Delete(_testImageFileName);
             }
+            return Task.CompletedTask;
         }
 
         //    [Fact]
