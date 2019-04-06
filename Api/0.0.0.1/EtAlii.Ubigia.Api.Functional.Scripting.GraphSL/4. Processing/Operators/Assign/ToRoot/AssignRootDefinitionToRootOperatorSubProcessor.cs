73204731abb1bc2f8470cfe40af1dab4406480a8
Peter Vrenken <github.com@petervrenken.nl>
2019-04-06 18:23:29 +0200

    Removed comments

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/4. Processing/Operators/Assign/ToRoot/AssignRootDefinitionToRootOperatorSubProcessor.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/4. Processing/Operators/Assign/ToRoot/AssignRootDefinitionToRootOperatorSubProcessor.cs
@@ -27,12 +27,8 @@ namespace EtAlii.Ubigia.Api.Functional
                 onCompleted: () => parameters.Output.OnCompleted(),
                 onNext: async (root) =>
                 {
-//                    var task = Task.Run(async () =>
-//                    {
-                        var createdRoot = await _context.Logical.Roots.Add(root.Name);
-                        parameters.Output.OnNext(createdRoot.Identifier);
-//                    });
-//                    task.Wait();
+                    var createdRoot = await _context.Logical.Roots.Add(root.Name);
+                    parameters.Output.OnNext(createdRoot.Identifier);
                 });
         }
     }
