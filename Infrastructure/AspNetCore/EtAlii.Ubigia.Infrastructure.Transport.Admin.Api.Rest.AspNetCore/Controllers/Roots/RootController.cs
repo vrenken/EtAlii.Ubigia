9d037ba1b6824005bc1699ef959b9ec2fbd1ac18
Peter Vrenken <github.com@petervrenken.nl>
2019-04-20 23:41:48 +0200

    Code cleanup: Remove empty 'case' clause.

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Rest.AspNetCore/Controllers/Roots/RootController.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Rest.AspNetCore/Controllers/Roots/RootController.cs
@@ -41,6 +41,9 @@
                             root = _items.Get(spaceId, rootId);
                             break;
                         case "rootName":
+                            var rootName = id;
+                            root = _items.Get(spaceId, rootName);
+                            break;
                         default:
                             root = _items.Get(spaceId, id);
                             break;
