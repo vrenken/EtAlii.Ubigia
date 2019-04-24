36c233daac05a094dbe299f12ded49a2b4bca54f
Peter Vrenken <github.com@petervrenken.nl>
2019-04-24 23:13:28 +0200

    Removed commented code.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Tests/TransportTestContextFactory.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Transport.Tests/TransportTestContextFactory.cs
@@ -7,13 +7,7 @@
         public TTransportTestContext Create<TTransportTestContext>()
             where TTransportTestContext : TransportTestContextBase<InProcessInfrastructureHostTestContext>, new()
         {
-            return new TTransportTestContext();
-            //var container = new Container()
-
-            //container.Register<ITransportTestContext, TTransportTestContext>()
-            //container.Register<IHostTestContextFactory, TransportHostTestContextFactory>()
-
-            //return container.GetInstance<ITransportTestContext>()
+            return new TTransportTestContext(); 
         }
     }
 }
