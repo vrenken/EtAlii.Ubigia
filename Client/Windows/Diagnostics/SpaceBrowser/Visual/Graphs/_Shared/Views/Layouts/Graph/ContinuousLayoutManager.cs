4b24e8744a2974e7d27497f5d0d4fc59b654e98d
Peter Vrenken <github.com@petervrenken.nl>
2019-04-28 21:20:22 +0200

    Code cleanup: Reduced Cognitive Complexity.

--- Client/Windows/Diagnostics/SpaceBrowser/Visual/Graphs/_Shared/Views/Layouts/Graph/ContinuousLayoutManager.cs
+++ Client/Windows/Diagnostics/SpaceBrowser/Visual/Graphs/_Shared/Views/Layouts/Graph/ContinuousLayoutManager.cs
@@ -1,8 +1,8 @@
 ﻿namespace EtAlii.Ubigia.Windows.Diagnostics.SpaceBrowser
 {
+    using System.Linq;
     using Northwoods.GoXam;
     using Northwoods.GoXam.Layout;
-    using System.Linq;
 
     // implement various optimizations
     public class ContinuousLayoutManager : LayoutManager
@@ -13,41 +13,39 @@
         {
             var diagram = Diagram;
             if (diagram == null) return;
-            var layout = diagram.Layout as ContinuousForceDirectedLayout;
-            if (layout != null)
+            if (diagram.Layout is ContinuousForceDirectedLayout layout)
             {
                 // assume there are no Groups with their own Layout
-                if (!layout.ValidLayout)
-                {
-                    // make sure there's no animation interfering with ContinuousForceDirectedLayout
-                    Animated = false;
-                    // don't set Wait cursor during drag
-                    diagram.Cursor = null;
-                    // the ForceDirectedNetwork needs to be cleared whenever a Node or Link is added or removed
-                    var net = layout.Network;
-                    if (net == null)
-                    {  // create a new one
-                        net = new ForceDirectedNetwork();
-                        net.AddNodesAndLinks(diagram.Nodes.Where(n => CanLayoutPart(n, layout)),
-                                             diagram.Links.Where(l => CanLayoutPart(l, layout)));
-                        layout.Network = net;
-                    }
-                    else
-                    {  // update all vertex.Bounds
-                        foreach (ForceDirectedVertex v in net.Vertexes)
-                        {
-                            Node n = v.Node;
-                            if (n != null) v.Bounds = n.Bounds;
-                        }
-                    }
-                    // don't need to re-collect the appropriate Nodes and Links to pass to DoLayout
-                    IsLayingOut = true;
-                    layout.DoLayout(null, null);
-                    IsLayingOut = false;
-                    layout.ValidLayout = true;
-                    // DoLayout normally discards the Network; keep it for next time
+                if (layout.ValidLayout) return;
+                
+                // make sure there's no animation interfering with ContinuousForceDirectedLayout
+                Animated = false;
+                // don't set Wait cursor during drag
+                diagram.Cursor = null;
+                // the ForceDirectedNetwork needs to be cleared whenever a Node or Link is added or removed
+                var net = layout.Network;
+                if (net == null)
+                {  // create a new one
+                    net = new ForceDirectedNetwork();
+                    net.AddNodesAndLinks(diagram.Nodes.Where(n => CanLayoutPart(n, layout)),
+                        diagram.Links.Where(l => CanLayoutPart(l, layout)));
                     layout.Network = net;
                 }
+                else
+                {  // update all vertex.Bounds
+                    foreach (var v in net.Vertexes)
+                    {
+                        var n = v.Node;
+                        if (n != null) v.Bounds = n.Bounds;
+                    }
+                }
+                // don't need to re-collect the appropriate Nodes and Links to pass to DoLayout
+                IsLayingOut = true;
+                layout.DoLayout(null, null);
+                IsLayingOut = false;
+                layout.ValidLayout = true;
+                // DoLayout normally discards the Network; keep it for next time
+                layout.Network = net;
             }
             else
             {
@@ -65,9 +63,8 @@
             if (IsLayingOut) return;
             if (SkipsInvalidate) return;
             if (Diagram == null) return;
-            if (p == null || p.Layer == null || p.Layer.IsTemporary) return;
-            var layout = Diagram.Layout as ContinuousForceDirectedLayout;
-            if (layout != null &&
+            if (p?.Layer == null || p.Layer.IsTemporary) return;
+            if (Diagram.Layout is ContinuousForceDirectedLayout layout &&
                 (change == LayoutChange.NodeAdded || change == LayoutChange.NodeRemoved ||
                  change == LayoutChange.LinkAdded || change == LayoutChange.LinkRemoved))
             {
