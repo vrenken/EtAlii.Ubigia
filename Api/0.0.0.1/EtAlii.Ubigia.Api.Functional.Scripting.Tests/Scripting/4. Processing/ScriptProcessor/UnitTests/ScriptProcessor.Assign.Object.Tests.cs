bdd3e430f211c9f1a33defbb475a11acae795ada
Peter Vrenken <github.com@petervrenken.nl>
2019-04-29 07:45:40 +0200

    Code cleanups

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.Object.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/4. Processing/ScriptProcessor/UnitTests/ScriptProcessor.Assign.Object.Tests.cs
@@ -1,23 +1,22 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Tests
 {
     using System;
-    using EtAlii.Ubigia.Api.Functional.Diagnostics.Scripting;
-    
+
     public class ScriptProcessorAssignObjectUnitTests : IDisposable
     {
-        private IScriptParser _parser;
+        //private IScriptParser _parser
 
-        public ScriptProcessorAssignObjectUnitTests()
-        {
-            var diagnostics = TestDiagnostics.Create();
-            var scriptParserConfiguration = new ScriptParserConfiguration()
-                .Use(diagnostics);
-            _parser = new ScriptParserFactory().Create(scriptParserConfiguration);
-        }
+        //public ScriptProcessorAssignObjectUnitTests()
+        //[
+            //var diagnostics = TestDiagnostics.Create()
+            //var scriptParserConfiguration = new ScriptParserConfiguration()
+            //    .Use(diagnostics)
+            //_parser = new ScriptParserFactory().Create(scriptParserConfiguration)
+        //]
 
         public void Dispose()
         {
-            _parser = null;
+            //_parser = null;
         }
     }
 }
