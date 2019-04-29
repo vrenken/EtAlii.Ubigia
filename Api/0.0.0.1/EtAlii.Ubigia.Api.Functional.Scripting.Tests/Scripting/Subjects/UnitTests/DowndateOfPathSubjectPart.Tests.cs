e9268e8a5a02d32d0adeadda6530ba0e7e7156c3
Peter Vrenken <github.com@petervrenken.nl>
2019-04-29 07:45:40 +0200

    Code cleanups

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/Subjects/UnitTests/DowndateOfPathSubjectPart.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/Scripting/Subjects/UnitTests/DowndateOfPathSubjectPart.Tests.cs
@@ -1,25 +1,23 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Tests
 {
     using System;
-    using EtAlii.Ubigia.Api.Functional.Diagnostics.Scripting;
     using Xunit;
-
 
     public class DowndateOfPathSubjectPartTests : IDisposable
     {
-        private IScriptParser _parser;
+        //private IScriptParser _parser
 
-        public DowndateOfPathSubjectPartTests()
-        {
-            var diagnostics = TestDiagnostics.Create();
-            var scriptParserConfiguration = new ScriptParserConfiguration()
-                .Use(diagnostics);
-            _parser = new ScriptParserFactory().Create(scriptParserConfiguration);
-        }
+        //public DowndateOfPathSubjectPartTests()
+        //[
+            //var diagnostics = TestDiagnostics.Create()
+            //var scriptParserConfiguration = new ScriptParserConfiguration()
+            //    .Use(diagnostics)
+            //_parser = new ScriptParserFactory().Create(scriptParserConfiguration)
+        //]
 
         public void Dispose()
         {
-            _parser = null;
+            //_parser = null
         }
 
 
