40f1aea633ef42f2cee43786dabe96cdceded04a
Peter Vrenken <github.com@petervrenken.nl>
2019-04-29 23:11:38 +0200

    Code cleanups: Removed static variable assignment

--- Client/Windows/Shared/Client/Shared/Commands/CommandBase.cs
+++ Client/Windows/Shared/Client/Shared/Commands/CommandBase.cs
@@ -1,163 +1,173 @@
 ﻿namespace EtAlii.Ubigia.Windows.Client
 {
-  using System;
-  using System.ComponentModel;
-  using System.Windows;
-  using System.Windows.Input;
-  using System.Windows.Markup;
-  using System.Windows.Media;
-  using Container = EtAlii.xTechnology.MicroContainer.Container;
-
-  /// <summary>
-  /// Basic implementation of the <see cref="ICommand"/>
-  /// interface, which is also accessible as a markup
-  /// extension.
-  /// </summary>
-  public abstract class CommandBase<T> : MarkupExtension, ICommand
-    where T : class, ICommand, new()
-  {
-    /// <summary>
-    /// A singleton instance.
-    /// </summary>
-    private static T _command;
-
-    protected Container Container { get; }
-
-    protected CommandBase()
-    {
-      Container = App.Current.Container;
-    }
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Markup;
+    using System.Windows.Media;
+    using Container = EtAlii.xTechnology.MicroContainer.Container;
 
     /// <summary>
-    /// Gets a shared command instance.
+    /// Basic implementation of the <see cref="ICommand"/>
+    /// interface, which is also accessible as a markup
+    /// extension.
     /// </summary>
-    public override object ProvideValue(IServiceProvider serviceProvider)
+    public abstract class CommandBase<T> : CommandBase, ICommand
+        where T : class, ICommand, new()
     {
-      if (_command == null) SetCommand(new T()); 
-      return _command;
-    }
+        protected Container Container { get; }
 
-    private static void SetCommand(T command)
-    {
-      _command = command;
-    }
+        private T _command;
 
-    /// <summary>
-    /// Fires when changes occur that affect whether
-    /// or not the command should execute.
-    /// </summary>
-    public event EventHandler CanExecuteChanged
-    {
-      add => CommandManager.RequerySuggested += value;
-      remove => CommandManager.RequerySuggested -= value;
-    }
-
-    /// <summary>
-    /// Defines the method to be called when the command is invoked.
-    /// </summary>
-    /// <param name="parameter">Data used by the command.
-    /// If the command does not require data to be passed,
-    /// this object can be set to null.
-    /// </param>
-    public abstract void Execute(object parameter);
-
-    /// <summary>
-    /// Defines the method that determines whether the command
-    /// can execute in its current state.
-    /// </summary>
-    /// <returns>
-    /// This default implementation always returns true.
-    /// </returns>
-    /// <param name="parameter">Data used by the command.  
-    /// If the command does not require data to be passed,
-    /// this object can be set to null.
-    /// </param>
-    public virtual bool CanExecute(object parameter)
-    {
-      return !IsDesignMode;
-    }
-
-
-    public static bool IsDesignMode => (bool) DependencyPropertyDescriptor
-      .FromProperty(DesignerProperties.IsInDesignModeProperty, typeof(FrameworkElement))
-      .Metadata.DefaultValue;
-
-
-    /// <summary>
-    /// Resolves the window that owns the TaskbarIcon class.
-    /// </summary>
-    /// <param name="commandParameter"></param>
-    /// <returns></returns>
-    protected Window GetTaskbarWindow(object commandParameter)
-    {
-      if (IsDesignMode) return null;
-
-      //get the showcase window off the taskbaricon
-      var tb = commandParameter as Hardcodet.Wpf.TaskbarNotification.TaskbarIcon;
-      return tb == null ? null : TryFindParent<Window>(tb);
-    }
-
-
-
-    #region TryFindParent helper
-
-    /// <summary>
-    /// Finds a parent of a given item on the visual tree.
-    /// </summary>
-    /// <typeparam name="D">The type of the queried item.</typeparam>
-    /// <param name="child">A direct or indirect child of the
-    /// queried item.</param>
-    /// <returns>The first parent item that matches the submitted
-    /// type parameter. If not matching item can be found, a null
-    /// reference is being returned.</returns>
-    public static D TryFindParent<D>(DependencyObject child)
-      where D : DependencyObject
-    {
-      //get parent item
-      DependencyObject parentObject = GetParentObject(child);
-
-      //we've reached the end of the tree
-      if (parentObject == null) return null;
-
-      //check if the parent matches the type we're looking for
-      if (parentObject is D parent)
-      {
-        return parent;
-      }
-      else
-      {
-        //use recursion to proceed with next level
-        return TryFindParent<D>(parentObject);
-      }
-    }
-
-    /// <summary>
-    /// This method is an alternative to WPF's
-    /// <see cref="VisualTreeHelper.GetParent"/> method, which also
-    /// supports content elements. Keep in mind that for content element,
-    /// this method falls back to the logical tree of the element!
-    /// </summary>
-    /// <param name="child">The item to be processed.</param>
-    /// <returns>The submitted item's parent, if available. Otherwise
-    /// null.</returns>
-    public static DependencyObject GetParentObject(DependencyObject child)
-    {
-      switch (child)
-      {
-        case null: return null;
-        case ContentElement contentElement:
+        protected CommandBase()
         {
-          DependencyObject parent = ContentOperations.GetParent(contentElement);
-          if (parent != null) return parent;
-
-          return contentElement is FrameworkContentElement fce ? fce.Parent : null;
+            Container = App.Current.Container;
         }
-        default:
-          //if it's not a ContentElement, rely on VisualTreeHelper
-          return VisualTreeHelper.GetParent(child);
-      }
+
+        private static T GetInstance()
+        {
+            if (Instances.TryGetValue(typeof(T), out var instance)) return (T) instance;
+            instance = new T();
+            Instances.Add(typeof(T), instance);
+            return (T)instance;
+        }
+
+        /// <summary>
+        /// Gets a shared command instance.
+        /// </summary>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _command ?? (_command = GetInstance());
+        }
+
+        /// <summary>
+        /// Fires when changes occur that affect whether
+        /// or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.
+        /// If the command does not require data to be passed,
+        /// this object can be set to null.
+        /// </param>
+        public abstract void Execute(object parameter);
+
+        /// <summary>
+        /// Defines the method that determines whether the command
+        /// can execute in its current state.
+        /// </summary>
+        /// <returns>
+        /// This default implementation always returns true.
+        /// </returns>
+        /// <param name="parameter">Data used by the command.  
+        /// If the command does not require data to be passed,
+        /// this object can be set to null.
+        /// </param>
+        public virtual bool CanExecute(object parameter)
+        {
+            return !IsDesignMode;
+        }
+
+
+        public static bool IsDesignMode => (bool) DependencyPropertyDescriptor
+            .FromProperty(DesignerProperties.IsInDesignModeProperty, typeof(FrameworkElement))
+            .Metadata.DefaultValue;
+
+
+        /// <summary>
+        /// Resolves the window that owns the TaskbarIcon class.
+        /// </summary>
+        /// <param name="commandParameter"></param>
+        /// <returns></returns>
+        protected Window GetTaskbarWindow(object commandParameter)
+        {
+            if (IsDesignMode) return null;
+
+            //get the showcase window off the taskbaricon
+            var tb = commandParameter as Hardcodet.Wpf.TaskbarNotification.TaskbarIcon;
+            return tb == null ? null : TryFindParent<Window>(tb);
+        }
+
+
+
+        #region TryFindParent helper
+
+        /// <summary>
+        /// Finds a parent of a given item on the visual tree.
+        /// </summary>
+        /// <typeparam name="TDependencyObject">The type of the queried item.</typeparam>
+        /// <param name="child">A direct or indirect child of the
+        /// queried item.</param>
+        /// <returns>The first parent item that matches the submitted
+        /// type parameter. If not matching item can be found, a null
+        /// reference is being returned.</returns>
+        private static TDependencyObject TryFindParent<TDependencyObject>(DependencyObject child) 
+            where TDependencyObject : DependencyObject
+        {
+            while (true)
+            {
+                //get parent item
+                var parentObject = GetParentObject(child);
+
+                switch (parentObject)
+                {
+                    //we've reached the end of the tree
+                    case null:
+                        return null;
+                    //check if the parent matches the type we're looking for
+                    case TDependencyObject parent:
+                        return parent;
+                    default:
+                        //use recursion to proceed with next level
+                        child = parentObject;
+                        continue;
+                }
+                break;
+            }
+        }
+
+        /// <summary>
+        /// This method is an alternative to WPF's
+        /// <see cref="VisualTreeHelper.GetParent"/> method, which also
+        /// supports content elements. Keep in mind that for content element,
+        /// this method falls back to the logical tree of the element!
+        /// </summary>
+        /// <param name="child">The item to be processed.</param>
+        /// <returns>The submitted item's parent, if available. Otherwise
+        /// null.</returns>
+        private static DependencyObject GetParentObject(DependencyObject child)
+        {
+            switch (child)
+            {
+                case null: return null;
+                case ContentElement contentElement:
+                {
+                    DependencyObject parent = ContentOperations.GetParent(contentElement);
+                    if (parent != null) return parent;
+
+                    return contentElement is FrameworkContentElement fce ? fce.Parent : null;
+                }
+                default:
+                    //if it's not a ContentElement, rely on VisualTreeHelper
+                    return VisualTreeHelper.GetParent(child);
+            }
+        }
+
+        #endregion
     }
 
-    #endregion
-  }
+    public abstract class CommandBase : MarkupExtension
+    {
+        protected static readonly Dictionary<Type, object> Instances = new Dictionary<Type, object>();
+    }
+
 }
