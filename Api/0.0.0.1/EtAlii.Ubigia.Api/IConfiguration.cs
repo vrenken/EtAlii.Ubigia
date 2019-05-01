1fff8111941aba97cac075c44436e513de7fcab2
Peter Vrenken <github.com@petervrenken.nl>
2019-05-01 20:19:59 +0200

    Added Configuration base.

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api/IConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace EtAlii.Ubigia.Api
+{
+    public interface IConfiguration<TExtension, out TConfiguration> 
+        where TExtension : class 
+        where TConfiguration : class
+    {
+        TExtension[] Extensions { get; }
+
+        TConfiguration Use(TExtension[] extensions);
+    }
+}
