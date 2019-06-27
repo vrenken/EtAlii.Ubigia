ecdfa3167622247ce20b6114982adbc217b93083
Peter Vrenken <github.com@petervrenken.nl>
2019-06-27 21:56:27 +0200

    Restructuring in preparation of GTL processing.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/RootHandlerFactory.Extensions.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.Tests/RootHandlerFactory.Extensions.cs
@@ -1,13 +1,13 @@
-﻿namespace EtAlii.Ubigia.Api.Functional.Tests
-{
-    internal static class TestRootHandlerFactoryExtension
-    {
-        public static IRootHandlerMapper[] CreateForTesting(this RootHandlerMapperFactory factory)
-        {
-            return new IRootHandlerMapper[]
-            {
-                new TestRootHandlerMapper(), 
-            };
-        }
-    }
-}
+﻿//namespace EtAlii.Ubigia.Api.Functional.Tests
+//{
+//    internal static class TestRootHandlerFactoryExtension
+//    {
+//        public static IRootHandlerMapper[] CreateForTesting(this RootHandlerMapperFactory factory)
+//        {
+//            return new IRootHandlerMapper[]
+//            {
+//                new TestRootHandlerMapper(), 
+//            };
+//        }
+//    }
+//}
