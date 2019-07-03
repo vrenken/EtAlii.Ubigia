e1fb010fbc15f13c5f43597c4f9bac8d09ec47b7
Peter Vrenken <vrenken@live.nl>
2019-07-03 20:33:08 +0200

    Old files. Let's remove them later.
    Currently getting focus is more important.

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/2. Planning/Structure/Queries/__IQueryFragmentExecutionPlanner.cs
@@ -0,0 +1,7 @@
+﻿//namespace EtAlii.Ubigia.Api.Functional 
+//{
+//    internal interface IQueryFragmentExecutionPlanner : IFragmentExecutionPlanner
+//    {
+//        QueryFragmentExecutionPlan Plan(QueryFragment fragment);
+//    }
+//}
