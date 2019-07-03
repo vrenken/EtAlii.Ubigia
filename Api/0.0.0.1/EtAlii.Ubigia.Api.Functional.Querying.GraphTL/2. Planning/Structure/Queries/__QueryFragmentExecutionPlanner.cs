fd946cea948d947010c4dcd6f6818587e1c2783c
Peter Vrenken <vrenken@live.nl>
2019-07-03 20:33:08 +0200

    Old files. Let's remove them later.
    Currently getting focus is more important.

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/2. Planning/Structure/Queries/__QueryFragmentExecutionPlanner.cs
@@ -0,0 +1,21 @@
+//namespace EtAlii.Ubigia.Api.Functional 
+//{
+//    internal class QueryFragmentExecutionPlanner : IStructureQueryExecutionPlanner
+//    {
+//        //private readonly IConstantSubjectProcessorSelector _processorSelector;
+//
+//        public QueryFragmentExecutionPlanner()//IConstantSubjectProcessorSelector processorSelector)
+//        {
+//            //_processorSelector = processorSelector;
+//        }
+//
+//        public FragmentExecutionPlan Plan(Fragment fragment)
+//        {
+//            var queryFragment = (QueryFragment)fragment;
+//
+//            return null;
+//            //var processor = _processorSelector.Select(constantSubject);
+//            //return new ConstantSubjectExecutionPlan(constantSubject, processor);
+//        }
+//    }
+//}
---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/__QueryProcessParameters.cs
@@ -0,0 +1,18 @@
+﻿//namespace EtAlii.Ubigia.Api.Functional 
+//{
+//    public class QueryProcessParameters<TTarget, TPart>
+//    {
+//        public TPart FuturePart { get; set; }
+//        public TPart RightPart { get; set; }
+//        public TPart LeftPart { get; set; }
+//        public object RightResult { get; set; }
+//        public object LeftResult { get; set; }
+
+//        public TTarget Target { get; }
+
+//        public QueryProcessParameters(TTarget target)
+//        {
+//            Target = target;
+//        }
+//    }
+//}
