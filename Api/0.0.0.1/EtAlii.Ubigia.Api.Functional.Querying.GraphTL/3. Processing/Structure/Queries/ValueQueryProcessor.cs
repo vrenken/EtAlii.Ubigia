62e8f95cc19a1c51dd3da3235265d16ae4e192d8
Peter Vrenken <github.com@petervrenken.nl>
2019-07-10 20:39:09 +0200

    Basic implementation of ValueQueryProcessor

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/Structure/Queries/ValueQueryProcessor.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/Structure/Queries/ValueQueryProcessor.cs
@@ -1,8 +1,10 @@
 namespace EtAlii.Ubigia.Api.Functional 
 {
     using System;
+    using System.Linq;
     using System.Reactive.Linq;
     using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Logical;
 
     internal class ValueQueryProcessor : IValueQueryProcessor
     {
@@ -13,17 +15,74 @@ namespace EtAlii.Ubigia.Api.Functional
             _scriptContext = scriptContext;
         }
 
-        public async Task Process(QueryFragment fragment, QueryExecutionScope executionScope, FragmentContext fragmentContext, IObserver<Structure> output)
+        private Identifier FindRelatedIdentifier(Structure structure)
+        {
+            var node = structure.Node;
+            if (node != null)
+            {
+                return node.Id;
+            }
+            var parent = structure.Parent;
+            return parent != null 
+                ? FindRelatedIdentifier(parent) 
+                : Identifier.Empty;
+        }
+        
+        private async Task ProcessFromAnnotation(
+            ValueQuery valueQuery, 
+            QueryExecutionScope executionScope, 
+            Structure structure)
+        {
+                if (valueQuery.Annotation?.Path is PathSubject path) // @value(\#LastName)
+                {
+                    var id = FindRelatedIdentifier(structure);
+                    if (id != Identifier.Empty)
+                    {
+                        var parts = new PathSubjectPart[] { new ParentPathSubjectPart(), new IdentifierPathSubjectPart(id) }.Concat(path.Parts).ToArray(); 
+                        path = new AbsolutePathSubject(parts); 
+                        var script = new Script(new Sequence(new SequencePart[] { path }));
+
+                        var processResult = await _scriptContext.Process(script, executionScope.ScriptScope);
+                        var result = await processResult.Output.SingleOrDefaultAsync(); 
+                        if (result is IInternalNode valueNode)
+                        {
+                            structure.EditableValues.Add(new Value(valueQuery.Name, valueNode.Type));                         
+                        }
+                    }
+                }
+                else // @value()
+                {
+                    structure.EditableValues.Add(new Value(valueQuery.Name, structure.Node.Type));                         
+                }
+        }
+
+        private void ProcessFromValue(ValueQuery valueQuery, Structure structure)
+        {
+            var properties = (IPropertyDictionary)structure.Node.GetProperties();
+            if (properties.TryGetValue(valueQuery.Name, out var value))
+            {
+                structure.EditableValues.Add(new Value(valueQuery.Name, value));                        
+            }
+        }
+
+        public async Task Process(QueryFragment fragment, QueryExecutionScope executionScope, FragmentMetadata fragmentMetadata, IObserver<Structure> output)
         {
             var valueQuery = (ValueQuery) fragment;
-            
-            var script = new Script(new Sequence(new SequencePart[] {valueQuery.Annotation.Path})); 
-            var processResult = await _scriptContext.Process(script, executionScope.ScriptScope);
-            var lastOutput = await processResult.Output.LastOrDefaultAsync();
 
-            var result = new Value(valueQuery.Name, lastOutput);
-            fragmentContext.Values.Add(result);
-            //output.OnNext(result);
+            if (valueQuery.Annotation != null)
+            {
+                foreach (var structure in fragmentMetadata.Parent.Items)
+                {
+                    await ProcessFromAnnotation(valueQuery, executionScope, structure);
+                }
+            }
+            else
+            {
+                foreach (var structure in fragmentMetadata.Parent.Items)
+                {
+                    ProcessFromValue(valueQuery, structure);
+                }
+            }
         }
     }
 }
