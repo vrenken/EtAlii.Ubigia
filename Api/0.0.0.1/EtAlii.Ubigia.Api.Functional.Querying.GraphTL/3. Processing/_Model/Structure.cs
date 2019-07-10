e827750fe8381277cf845506e9a651a1ccac7557
Peter Vrenken <github.com@petervrenken.nl>
2019-07-10 20:38:06 +0200

    Added processing model

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/_Model/Structure.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/_Model/Structure.cs
@@ -1,34 +1,40 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional 
 {
     using System.Collections.ObjectModel;
+    using EtAlii.Ubigia.Api.Logical;
 
     public sealed class Structure
     {
         //public Identifier Id { get; }
+        public string Type { get; }
         public string Name { get; }
 
         public ReadOnlyObservableCollection<Structure> Children { get; }
+        internal ObservableCollection<Structure> EditableChildren { get; }
 
         public ReadOnlyObservableCollection<Value> Values { get; }
+        internal ObservableCollection<Value> EditableValues { get; }
 
-        public Structure(string name)
+        internal IInternalNode Node { get; } 
+        internal Structure Parent { get; } 
+        
+        internal Structure(string type, string name, Structure parent)
         {
+            Type = type;
             Name = name;
             
-            var children = new ObservableCollection<Structure>();
-            var values = new ObservableCollection<Value>();
-
-            Children = new ReadOnlyObservableCollection<Structure>(children);
-            Values  = new ReadOnlyObservableCollection<Value>(values);
-
+            EditableChildren = new ObservableCollection<Structure>();
+            Children = new ReadOnlyObservableCollection<Structure>(EditableChildren);
+            EditableValues = new ObservableCollection<Value>();
+            Values  = new ReadOnlyObservableCollection<Value>(EditableValues);
+            
+            Parent = parent;
         }
-        
-        public Structure(string name, ReadOnlyObservableCollection<Structure> children, ReadOnlyObservableCollection<Value> values)
+
+        internal Structure(string type, string name, Structure parent, IInternalNode node)
+            : this(type, name, parent)
         {
-            Name = name;
-            Children = children;
-            Values  = values;
+            Node = node;
         }
-
     }
 }
