71fa79d1cf216976d2ba7f538e110a49b9301688
Peter Vrenken <github.com@petervrenken.nl>
2019-07-10 20:32:54 +0200

    Removed unused Empty/None constructs.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/_Model/Structure/Mutations/MutationFragment.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/_Model/Structure/Mutations/MutationFragment.cs
@@ -2,18 +2,8 @@
 {
     public class MutationFragment : Fragment
     {
-        public static readonly MutationFragment None = new EmptyMutationFragment();
-
         public MutationFragment(string name, Annotation annotation) : base(name, annotation)
         {
         }
-        
-        private class EmptyMutationFragment : MutationFragment
-        {
-            public EmptyMutationFragment() : base(nameof(None), Annotation.None)
-            {
-            }
-        }
-
     }
 }
