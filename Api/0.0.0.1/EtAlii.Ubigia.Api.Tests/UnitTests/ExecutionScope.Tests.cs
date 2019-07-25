676f46e6448190613cdbce3febee15c734f735d5
Peter Vrenken <github.com@petervrenken.nl>
2019-07-25 22:08:31 +0200

    Moved Cache and ExecutionScope to primary solution

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Tests/UnitTests/ExecutionScope.Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace EtAlii.Ubigia.Api.Tests
+{
+    using Xunit;
+
+    public class ExecutionScope_Tests
+    {
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public void ExecutionScope_Create_Cache_Enabled()
+        {
+            // Arrange.
+
+            // Act.
+            var executionScope = new ExecutionScope(true);
+
+            // Assert.
+            Assert.NotNull(executionScope);
+        }
+
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public void ExecutionScope_Create_Cache_Disabled()
+        {
+            // Arrange.
+
+            // Act.
+            var executionScope = new ExecutionScope(false);
+
+            // Assert.
+            Assert.NotNull(executionScope);
+        }
+
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public void ExecutionScope_GetWildCardRegex()
+        {
+            // Arrange.
+            var executionScope = new ExecutionScope(true);
+
+            // Act.
+            var regex = executionScope.GetWildCardRegex("Vre*");
+
+            // Assert.
+            Assert.NotNull(regex);
+        }
+
+    }
+}
