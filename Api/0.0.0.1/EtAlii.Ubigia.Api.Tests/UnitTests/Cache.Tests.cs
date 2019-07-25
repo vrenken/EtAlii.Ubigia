89d03caf79aaa7d68f35d4bf52e5558d91108bae
Peter Vrenken <github.com@petervrenken.nl>
2019-07-25 22:08:31 +0200

    Moved Cache and ExecutionScope to primary solution

---
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Tests/UnitTests/Cache.Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace EtAlii.Ubigia.Api.Tests
+{
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Tests;
+    using Xunit;
+
+    public class Cache_Tests
+    {
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public void Cache_Create()
+        {
+            // Arrange.
+
+            // Act.
+            var cache = new Cache(true);
+
+            // Assert.
+            Assert.NotNull(cache);
+        }
+
+        [Fact, Trait("Category", TestAssembly.Category)]
+        public async Task Cache_Create_Null()
+        {
+            // Arrange.
+            var identifier = new TestIdentifierFactory().Create();
+            var cache = new Cache(true);
+
+            // Act.
+            var entry = await cache.GetEntry(identifier, () => Task.FromResult<IReadOnlyEntry>(null));
+
+            // Assert.
+            Assert.Null(entry);
+        }
+
+    }
+}
