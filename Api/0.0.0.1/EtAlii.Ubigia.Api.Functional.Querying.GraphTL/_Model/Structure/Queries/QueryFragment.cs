ee59d43d7851cec21c33ad581d231ea123b1c500
Peter Vrenken <github.com@petervrenken.nl>
2019-07-27 16:09:53 +0200

    Code cleanup

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/_Model/Structure/Queries/QueryFragment.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/_Model/Structure/Queries/QueryFragment.cs
@@ -4,18 +4,9 @@
     {
         public Requirement Requirement { get; }
         
-//        public static readonly QueryFragment None = new EmptyQueryFragment();
-
         public QueryFragment(string name, Annotation annotation, Requirement requirement) : base(name, annotation)
         {
             Requirement = requirement;
         }
-//        
-//        private class EmptyQueryFragment : QueryFragment
-//        {
-//            public EmptyQueryFragment() : base(nameof(None), null, Requirement.None)
-//            {
-//            }
-//        }
     }
 }
