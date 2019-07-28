549d6bfc684f0690d54af53d458f4e10fb35b6b0
Peter Vrenken <github.com@petervrenken.nl>
2019-07-29 00:07:06 +0200

    Further implementation of GTL structure parsing / processing.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphTL/1. Parsing/Unit tests/AnnotationParser.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphTL/1. Parsing/Unit tests/AnnotationParser.Tests.cs
@@ -28,7 +28,7 @@
         {
             // Arrange.
             var parser = CreateAnnotationParser();
-            var text = @"@node(Person:Stark/Tony)";
+            var text = "@node(Person:Stark/Tony)";
             
             
             // Act.
@@ -39,8 +39,134 @@
             Assert.NotNull(node);
             Assert.NotNull(annotation);
             Assert.Equal(AnnotationType.Node,annotation.Type);
-            Assert.Equal("Person:Stark/Tony",annotation.Path.ToString());
+            Assert.Equal("Person:Stark/Tony",annotation.Path?.ToString());
         }
+        
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Add_Whitespaces()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts += Pepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" += ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+        
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Add_Tabs()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts\t+=\tPepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" += ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Add_Compact()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts+=Pepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" += ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Remove_Whitespaces()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts -= Pepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" -= ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+        
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Remove_Tabs()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts\t-=\tPepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" -= ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+
+        [Fact]
+        public void AnnotationParser_Parse_Node_Person_Remove_Compact()
+        {
+            // Arrange.
+            var parser = CreateAnnotationParser();
+            var text = "@node(Person:Potts-=Pepper)";
+            
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var annotation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Node,annotation.Type);
+            Assert.Equal("Person:Potts",annotation.Path?.ToString());
+            Assert.Equal(" -= ",annotation.Operator?.ToString());
+            Assert.Equal("\"Pepper\"",annotation.Subject?.ToString());
+        }
+
         [Fact]
         public void AnnotationParser_Parse_Nodes_Persons()
         {
