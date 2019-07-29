75b6844276db3c27f76756c958d05a1e596f8208
Peter Vrenken <github.com@petervrenken.nl>
2019-07-29 10:50:50 +0200

    Small order restructuring

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/_Scaffolding/QueryProcessingScaffolding.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/3. Processing/_Scaffolding/QueryProcessingScaffolding.cs
@@ -16,20 +16,21 @@
             container.Register<IQueryProcessor, QueryProcessor>();
             
             container.Register<IValueQueryProcessor, ValueQueryProcessor>();
-            container.Register<IStructureQueryProcessor, StructureQueryProcessor>();
-            
             container.Register<IValueMutationProcessor, ValueMutationProcessor>();
-            container.Register<IStructureMutationProcessor, StructureMutationProcessor>();
-            
-            container.Register<IStructureBuilder, StructureBuilder >();
-            
-            container.Register<IRelatedIdentityFinder, RelatedIdentityFinder>();
             container.Register<IValueGetter, ValueGetter>();
             container.Register<IValueSetter, ValueSetter>();
             container.Register<IPropertiesValueGetter, PropertiesValueGetter>();
             container.Register<IPropertiesValueSetter, PropertiesValueSetter>();
             container.Register<IPathValueGetter, PathValueGetter>();
             container.Register<IPathValueSetter, PathValueSetter>();
+
+            container.Register<IStructureQueryProcessor, StructureQueryProcessor>();
+            container.Register<IStructureMutationProcessor, StructureMutationProcessor>();
+            container.Register<IStructureQueryBuilder, StructureQueryBuilder >();
+            container.Register<IStructureMutationBuilder, StructureMutationBuilder>();
+
+
+            container.Register<IRelatedIdentityFinder, RelatedIdentityFinder>();
 
             container.Register<IScriptProcessingContext, ScriptProcessingContext>();
             container.Register(() => _configuration.ScriptContext);
