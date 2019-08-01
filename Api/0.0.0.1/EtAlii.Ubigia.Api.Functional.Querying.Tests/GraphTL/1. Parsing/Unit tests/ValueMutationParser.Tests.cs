09f683771fa8942d970a184935b95c845c944abb
Peter Vrenken <github.com@petervrenken.nl>
2019-08-01 12:22:55 +0200

    Added some GTL ValueMutationParser unit tests.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphTL/1. Parsing/Unit tests/ValueMutationParser.Tests.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Querying.Tests/GraphTL/1. Parsing/Unit tests/ValueMutationParser.Tests.cs
@@ -19,11 +19,11 @@
         private IValueMutationParser CreateValueMutationParser() => new QueryParserTestContainerFactory().Create().GetInstance<IValueMutationParser>();
         
         [Fact]
-        public void ValueMutationParser_Parse_Value()
+        public void ValueMutationParser_Parse_Value_Space()
         {
             // Arrange.
             var parser = CreateValueMutationParser();
-            var text = @"firstname <= ""John""";
+            var text = "firstname <= \"John\"";
             
             // Act.
             var node = parser.Parser.Do(text);
@@ -38,11 +38,11 @@
         }
         
         [Fact]
-        public void ValueMutationParser_Parse_Value_Correct_Assignment_Operator()
+        public void ValueMutationParser_Parse_Value_Tab()
         {
             // Arrange.
             var parser = CreateValueMutationParser();
-            var text = @"firstname <= ""John""";
+            var text = "firstname\t<=\t\"John\"";
             
             // Act.
             var node = parser.Parser.Do(text);
@@ -57,11 +57,30 @@
         }
         
         [Fact]
-        public void ValueMutationParser_Parse_Value_Relative()
+        public void ValueMutationParser_Parse_Value_Compact()
         {
             // Arrange.
             var parser = CreateValueMutationParser();
-            var text = @"lastname <= @value(\\)";
+            var text = "firstname<=\"John\"";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.Null(annotation);
+            Assert.NotNull(valueMutation);
+            Assert.Equal("John",valueMutation.Value);
+        }
+
+        [Fact]
+        public void ValueMutationParser_Parse_Value_From_Annotation_Space()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "Location <= @value(Location:NL/Overijssel/Enschede/Oldebokhoek/52)";
             
             // Act.
             var node = parser.Parser.Do(text);
@@ -72,7 +91,111 @@
             var annotation = valueMutation.Annotation;
             Assert.NotNull(annotation);
             Assert.Equal(AnnotationType.Value, annotation.Type);
-            Assert.Equal(@"\\", annotation.Path.ToString());
+            Assert.Equal("Location",valueMutation.Name);
+            Assert.Equal("Location:NL/Overijssel/Enschede/Oldebokhoek/52", annotation.Path.ToString());
+        }
+        
+        [Fact]
+        public void ValueMutationParser_Parse_Value_From_Annotation_Compact()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "Location<=@value(Location:NL/Overijssel/Enschede/Oldebokhoek/52)";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Value, annotation.Type);
+            Assert.Equal("Location",valueMutation.Name);
+            Assert.Equal("Location:NL/Overijssel/Enschede/Oldebokhoek/52", annotation.Path.ToString());
+        }
+
+                
+        [Fact]
+        public void ValueMutationParser_Parse_Value_From_Annotation_Tab()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "Location\t<=\t@value(Location:NL/Overijssel/Enschede/Oldebokhoek/52)";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Value, annotation.Type);
+            Assert.Equal("Location",valueMutation.Name);
+            Assert.Equal("Location:NL/Overijssel/Enschede/Oldebokhoek/52", annotation.Path.ToString());
+        }
+
+        [Fact]
+        public void ValueMutationParser_Parse_Value_Assignment_From_Constant_Space()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "FirstName @value() <= \"John\"";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Value, annotation.Type);
+            Assert.Equal("FirstName",valueMutation.Name);
+            Assert.Null(annotation.Path);
+            Assert.Equal("John", valueMutation.Value.ToString());
+        }
+                
+        [Fact]
+        public void ValueMutationParser_Parse_Value_Assignment_From_Constant_Tab()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "FirstName @value()\t<=\t\"John\"";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Value, annotation.Type);
+            Assert.Equal("FirstName",valueMutation.Name);
+            Assert.Null(annotation.Path);
+            Assert.Equal("John", valueMutation.Value.ToString());
+        }
+        [Fact]
+        public void ValueMutationParser_Parse_Value_Assignment_From_Constant_Compact()
+        {
+            // Arrange.
+            var parser = CreateValueMutationParser();
+            var text = "FirstName @value()<=\"John\"";
+            
+            // Act.
+            var node = parser.Parser.Do(text);
+            var valueMutation = parser.Parse(node);
+            
+            // Assert.
+            Assert.NotNull(node);
+            var annotation = valueMutation.Annotation;
+            Assert.NotNull(annotation);
+            Assert.Equal(AnnotationType.Value, annotation.Type);
+            Assert.Equal("FirstName",valueMutation.Name);
+            Assert.Null(annotation.Path);
+            Assert.Equal("John", valueMutation.Value.ToString());
         }
     }
 }
