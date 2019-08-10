5c96867abdbf81019d75535612252ea8304e79f5
Peter Vrenken <github.com@petervrenken.nl>
2019-08-10 10:53:38 +0200

    Added some comments

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/IGraphPathBuilder.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/IGraphPathBuilder.cs
@@ -1,10 +1,35 @@
 ﻿namespace EtAlii.Ubigia.Api.Logical
 {
+    /// <summary>
+    /// Use this class to build GraphPath instances of identifiers, nodes and relations. 
+    /// </summary>
     public interface IGraphPathBuilder
     {
+        /// <summary>
+        /// Add a start identifier to the GraphPath to be build.
+        /// </summary>
+        /// <param name="startIdentifier"></param>
+        /// <returns></returns>
         IGraphPathBuilder Add(Identifier startIdentifier);
+        
+        /// <summary>
+        /// Add a node to the GraphPath to be build.
+        /// </summary>
+        /// <param name="nodeName"></param>
+        /// <returns></returns>
         IGraphPathBuilder Add(string nodeName);
+        
+        /// <summary>
+        /// Add a relation to the GraphPath to be build.
+        /// </summary>
+        /// <param name="relation"></param>
+        /// <returns></returns>
         IGraphPathBuilder Add(GraphRelation relation);
+        
+        /// <summary>
+        /// Build the GraphPath and return it.
+        /// </summary>
+        /// <returns></returns>
         GraphPath ToPath();
     }
 }
