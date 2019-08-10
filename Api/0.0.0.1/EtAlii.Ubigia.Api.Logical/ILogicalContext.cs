9a22601dd5d7b2c4b9e239798fe9b22e9b022f6f
Peter Vrenken <github.com@petervrenken.nl>
2019-08-10 10:56:18 +0200

    Added some comments

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/ILogicalContext.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Logical/ILogicalContext.cs
@@ -13,8 +13,22 @@ namespace EtAlii.Ubigia.Api.Logical
         /// The Nodes property provides logical access to graph nodes.
         /// </summary>
         ILogicalNodeSet Nodes { get; }
+        
+        /// <summary>
+        /// The Roots property provides logical access to the roots of the graph.
+        /// Use it to start a traversal.
+        /// </summary>
         ILogicalRootSet Roots { get; }
+
+        /// <summary>
+        /// The Content property provides access non-value content stored in graph nodes.
+        /// For example media or other more-binary oriented materials. 
+        /// </summary>
         IContentManager Content { get; }
+        
+        /// <summary>
+        /// The Properties property provides access to properties stored in graph nodes.
+        /// </summary>
         IPropertiesManager Properties { get; }
     }
 }
