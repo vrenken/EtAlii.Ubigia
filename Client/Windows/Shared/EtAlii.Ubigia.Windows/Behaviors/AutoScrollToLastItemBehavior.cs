7d4b9f4f6b237f860cb1f866e5351ce4f9f729dc
Peter Vrenken <github.com@petervrenken.nl>
2019-08-14 20:33:12 +0200

    Made AutoScrollToLastItemBehavior inherit from Microsoft.Xaml.Behaviors.Behavior

--- Client/Windows/Shared/EtAlii.Ubigia.Windows/Behaviors/AutoScrollToLastItemBehavior.cs
+++ Client/Windows/Shared/EtAlii.Ubigia.Windows/Behaviors/AutoScrollToLastItemBehavior.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Specialized;
     using System.Windows.Controls;
-    using System.Windows.Interactivity;
+    using Microsoft.Xaml.Behaviors;
 
     public sealed class AutoScrollToLastItemBehavior : Behavior<DataGrid>
     {
@@ -10,8 +10,7 @@
         {
             base.OnAttached();
 
-            var collection = AssociatedObject.Items.SourceCollection as INotifyCollectionChanged;
-            if (collection != null)
+            if (AssociatedObject.Items.SourceCollection is INotifyCollectionChanged collection)
             {
                 collection.CollectionChanged += OnCollectionChanged;
             }
@@ -19,8 +18,7 @@
 
         protected override void OnDetaching()
         {
-            var collection = AssociatedObject.Items.SourceCollection as INotifyCollectionChanged;
-            if (collection != null)
+            if (AssociatedObject.Items.SourceCollection is INotifyCollectionChanged collection)
             {
                 collection.CollectionChanged -= OnCollectionChanged;
             }
