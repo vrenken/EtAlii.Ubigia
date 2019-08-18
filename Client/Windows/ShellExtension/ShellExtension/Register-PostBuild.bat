b7ee3dadadacd5cf63daf31ebdd264ee69b04c2d
Peter Vrenken <github.com@petervrenken.nl>
2019-08-18 18:52:26 +0200

    Replaced ShellExtension by simple stub.
    The code was that old and most probably not used for a future implementation.

--- Client/Windows/ShellExtension/ShellExtension/Register-PostBuild.bat
+++
@@ -1,5 +0,0 @@
-﻿REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\sn.exe" -d UbigiaCryptoContainer
-REM if exist "$(TargetDir)UbigiaCryptoContainer.snk" del "$(TargetDir)ShellRegistration-public.snk"
-REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\sn.exe" -Ra "$(TargetPath)" "$(ProjectDir)ShellRegistration.snk"
-REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\sn.exe" -e "$(ProjectDir)ShellRegistration.snk" "$(TargetDir)ShellRegistration-public.snk"
-REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\sn.exe" -i "$(ProjectDir)ShellRegistration.snk" UbigiaCryptoContainer
--- Client/Windows/ShellExtension/ShellExtension/Register-PreBuild.bat
+++
@@ -1,2 +0,0 @@
-﻿REM if exist "$(TargetPath)" "$(ProjectDir)\RestartExplorer.exe"
-REM if exist "$(TargetPath)" "$(ProjectDir)\RegisterExtensionDotNet40.exe" -u "$(TargetPath)"
