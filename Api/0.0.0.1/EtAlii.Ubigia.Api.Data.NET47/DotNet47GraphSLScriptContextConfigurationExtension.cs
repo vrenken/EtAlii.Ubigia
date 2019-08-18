042e054f1c32345e87aa758a3437c4969cd301c6
Peter Vrenken <github.com@petervrenken.nl>
2019-08-18 14:44:25 +0200

    Cleaning up Net47

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Data.NET47/DotNet47GraphSLScriptContextConfigurationExtension.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Data.NET47/DotNet47GraphSLScriptContextConfigurationExtension.cs
@@ -1,20 +1,20 @@
-﻿namespace EtAlii.Ubigia.Api.Functional.NET47
-{
-    public static class DotNet47GraphSLScriptContextConfigurationExtension
-    {
-        public static TGraphSLScriptContextConfiguration UseDotNet47<TGraphSLScriptContextConfiguration>(this TGraphSLScriptContextConfiguration configuration)
-            where TGraphSLScriptContextConfiguration : GraphSLScriptContextConfiguration
-        {
-            var functionHandlers = new[] 
-            {
-                new FileFunctionHandlerFactory().Create(),
-                new FormatFunctionHandlerFactory().Create(),
-            };
-            var functionHandlersProvider = new FunctionHandlersProvider(functionHandlers);
-
-            configuration.Use(functionHandlersProvider);
-
-            return configuration;
-        }
-    }
-}
+﻿//namespace EtAlii.Ubigia.Api.Functional.NET47
+//{
+//    public static class DotNet47GraphSLScriptContextConfigurationExtension
+//    {
+//        public static TGraphSLScriptContextConfiguration UseDotNet47<TGraphSLScriptContextConfiguration>(this TGraphSLScriptContextConfiguration configuration)
+//            where TGraphSLScriptContextConfiguration : GraphSLScriptContextConfiguration
+//        {
+//            var functionHandlers = new[] 
+//            {
+//                new FileFunctionHandlerFactory().Create(),
+//                new FormatFunctionHandlerFactory().Create(),
+//            };
+//            var functionHandlersProvider = new FunctionHandlersProvider(functionHandlers);
+//
+//            configuration.Use(functionHandlersProvider);
+//
+//            return configuration;
+//        }
+//    }
+//}
