813aa979ef37b46edd8bbcfd244b7eb4303ae8fe
Peter Vrenken <github.com@petervrenken.nl>
2019-08-19 18:20:39 +0200

    Corrected project folder names of hosting providers.

---
+++ Providers/Hosting/Hosts/EtAlii.Ubigia.Provisioning.Hosting.ConsoleHost.NetCore/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EtAlii.Ubigia.Provisioning.Hosting.ConsoleHost
+{
+	using System;
+	using EtAlii.xTechnology.Hosting;
+	using global::Microsoft.Extensions.Configuration;
+
+	public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application. 
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+	        var applicationConfiguration = new ConfigurationBuilder()
+		        .AddJsonFile("settings.json")
+		        .Build();
+
+	        var hostConfiguration = new HostConfigurationBuilder()
+		        .Build(applicationConfiguration)
+		        .UseConsoleHost();
+
+	        ConsoleHost.Start(hostConfiguration);
+        }
+    }
+}
