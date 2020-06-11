d8c72637f55eccf6ecbb40768bb298621f8bd0c4
Peter Vrenken <github.com@petervrenken.nl>
2020-06-11 21:47:15 +0200

    New hosting compatible rest/signalr services.

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR/UserSignalRService.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR/UserSignalRService.cs
@@ -9,71 +9,130 @@
 	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.DependencyInjection;
 
-	public class UserSignalRService : NetCoreServiceBase
+	public class UserSignalRService : ServiceBase
     {
         public UserSignalRService(IConfigurationSection configuration) 
             : base(configuration)
         {
         }
 
-        protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
+        protected override void ConfigureServices(IServiceCollection services)
         {
-            var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
+	        var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
+	        services
+		        .AddSingleton(infrastructure.Spaces)
+		        .AddSingleton(infrastructure.Accounts)
+		        .AddSingleton(infrastructure.Roots)
+		        .AddSingleton(infrastructure.Entries)
+		        .AddSingleton(infrastructure.Properties)
+		        .AddSingleton(infrastructure.Content)
+		        .AddSingleton(infrastructure.ContentDefinition)
 
-            applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.User.Api.SignalR.BaseUrl,
-                services =>
-                {
-	                services
-		                .AddSingleton(infrastructure.Spaces)
-		                .AddSingleton(infrastructure.Accounts)
-		                .AddSingleton(infrastructure.Roots)
-		                .AddSingleton(infrastructure.Entries)
-		                .AddSingleton(infrastructure.Properties)
-		                .AddSingleton(infrastructure.Content)
-		                .AddSingleton(infrastructure.ContentDefinition)
+		        .AddInfrastructureSimpleAuthentication(infrastructure)
+		        .AddInfrastructureSerialization()
 
-		                .AddInfrastructureSimpleAuthentication(infrastructure)
-		                .AddInfrastructureSerialization()
-
-		                .AddCors()
-		                .AddSignalR(options =>
-		                {
-			                if (Debugger.IsAttached)
-			                {
-				                options.EnableDetailedErrors = Debugger.IsAttached;
-			                }
-		                })
-		                .AddHubOptions<ContentHub>(options =>
-		                {
-							const long maximumReceiveMessageSizeInMegaByte = 1024 * 1024 * 2;
-							options.MaximumReceiveMessageSize = maximumReceiveMessageSizeInMegaByte;
-		                })
-		                .AddNewtonsoftJsonProtocol(options => SerializerFactory.Configure(options.PayloadSerializerSettings));
-                },
-                appBuilder =>
-                {
-                    appBuilder
-                        .UseCors(configuration =>
-	                    {
-		                    configuration.AllowAnyMethod();
-		                    configuration.AllowAnyHeader();
-                            configuration.AllowAnyOrigin(); 
-                        })
-                        .UseRouting()
-                        .UseEndpoints(endPoints =>
-                        {
-	                        endPoints.MapHub<AuthenticationHub>(SignalRHub.Authentication);
-
-	                        endPoints.MapHub<AccountHub>(SignalRHub.Account);
-	                        endPoints.MapHub<SpaceHub>(SignalRHub.Space);
-
-	                        endPoints.MapHub<RootHub>(SignalRHub.Root);
-	                        endPoints.MapHub<EntryHub>(SignalRHub.Entry);
-	                        endPoints.MapHub<PropertiesHub>(SignalRHub.Property);
-	                        endPoints.MapHub<ContentHub>(SignalRHub.Content);
-	                        endPoints.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
-                        });
-                });
+		        .AddRouting()
+		        .AddCors()
+		        .AddSignalR(options => 
+		        {
+			        if (Debugger.IsAttached)
+			        {
+				        options.EnableDetailedErrors = Debugger.IsAttached;
+			        }
+		        })
+		        .AddHubOptions<ContentHub>(options =>
+		        {
+			        const long maximumReceiveMessageSizeInMegaByte = 1024 * 1024 * 2;
+			        options.MaximumReceiveMessageSize = maximumReceiveMessageSizeInMegaByte;
+		        })
+		        .AddNewtonsoftJsonProtocol(options => SerializerFactory.Configure(options.PayloadSerializerSettings));
         }
+
+        protected override void ConfigureApplication(IApplicationBuilder applicationBuilder)
+        {
+	        applicationBuilder
+		        .UseCors(builder =>
+		        {
+			        builder
+				        .AllowAnyOrigin() 
+				        .AllowAnyHeader()
+				        .AllowAnyMethod()
+				        .WithOrigins($"http://{HostString}");
+		        })
+		        .UseRouting()
+		        .UseEndpoints(endPoints =>
+		        {
+			        endPoints.MapHub<AuthenticationHub>(SignalRHub.Authentication);
+
+			        endPoints.MapHub<AccountHub>(SignalRHub.Account);
+			        endPoints.MapHub<SpaceHub>(SignalRHub.Space);
+
+			        endPoints.MapHub<RootHub>(SignalRHub.Root);
+			        endPoints.MapHub<EntryHub>(SignalRHub.Entry);
+			        endPoints.MapHub<PropertiesHub>(SignalRHub.Property);
+			        endPoints.MapHub<ContentHub>(SignalRHub.Content);
+			        endPoints.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
+		        });
+        }
+
+       //  protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
+       //  {
+       //      var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
+       //
+       //      applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.User.Api.SignalR.BaseUrl,
+       //          services =>
+       //          {
+	      //           services
+		     //            .AddSingleton(infrastructure.Spaces)
+		     //            .AddSingleton(infrastructure.Accounts)
+		     //            .AddSingleton(infrastructure.Roots)
+		     //            .AddSingleton(infrastructure.Entries)
+		     //            .AddSingleton(infrastructure.Properties)
+		     //            .AddSingleton(infrastructure.Content)
+		     //            .AddSingleton(infrastructure.ContentDefinition)
+       //
+		     //            .AddInfrastructureSimpleAuthentication(infrastructure)
+		     //            .AddInfrastructureSerialization()
+       //
+		     //            .AddCors()
+		     //            .AddSignalR(options =>
+		     //            {
+			    //             if (Debugger.IsAttached)
+			    //             {
+				   //              options.EnableDetailedErrors = Debugger.IsAttached;
+			    //             }
+		     //            })
+		     //            .AddHubOptions<ContentHub>(options =>
+		     //            {
+							// const long maximumReceiveMessageSizeInMegaByte = 1024 * 1024 * 2;
+							// options.MaximumReceiveMessageSize = maximumReceiveMessageSizeInMegaByte;
+		     //            })
+		     //            .AddNewtonsoftJsonProtocol(options => SerializerFactory.Configure(options.PayloadSerializerSettings));
+       //          },
+       //          appBuilder =>
+       //          {
+       //              appBuilder
+       //                  .UseCors(configuration =>
+	      //               {
+		     //                configuration.AllowAnyMethod();
+		     //                configuration.AllowAnyHeader();
+       //                      configuration.AllowAnyOrigin(); 
+       //                  })
+       //                  .UseRouting()
+       //                  .UseEndpoints(endPoints =>
+       //                  {
+	      //                   endPoints.MapHub<AuthenticationHub>(SignalRHub.Authentication);
+       //
+	      //                   endPoints.MapHub<AccountHub>(SignalRHub.Account);
+	      //                   endPoints.MapHub<SpaceHub>(SignalRHub.Space);
+       //
+	      //                   endPoints.MapHub<RootHub>(SignalRHub.Root);
+	      //                   endPoints.MapHub<EntryHub>(SignalRHub.Entry);
+	      //                   endPoints.MapHub<PropertiesHub>(SignalRHub.Property);
+	      //                   endPoints.MapHub<ContentHub>(SignalRHub.Content);
+	      //                   endPoints.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
+       //                  });
+       //          });
+       //  }
     }
 }
