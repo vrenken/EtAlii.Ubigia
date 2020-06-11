f938b3f956c8c47b5d72353941901927640802d8
Peter Vrenken <github.com@petervrenken.nl>
2020-06-12 01:42:49 +0200

    Disabled TrayIcon host

--- Providers/Hosting/Hosts/EtAlii.Ubigia.Provisioning.Hosting.TrayIconHost.NetCore/App.xaml.cs
+++ Providers/Hosting/Hosts/EtAlii.Ubigia.Provisioning.Hosting.TrayIconHost.NetCore/App.xaml.cs
@@ -1,25 +1,31 @@
 ﻿namespace EtAlii.Ubigia.Provisioning.Hosting.TrayIconHost
 {
+	using System;
 	using System.Windows;
 	using EtAlii.xTechnology.Hosting;
 	using global::Microsoft.Extensions.Configuration;
 
 	partial class App
 	{
-        private void OnApplicationStartup(object sender, StartupEventArgs e)
+        private async void OnApplicationStartup(object sender, StartupEventArgs e)
         {
+	        var details = await new ConfigurationDetailsParser()
+		        .Parse("settings.json");
+
 	        var applicationConfiguration = new ConfigurationBuilder()
-		        .AddJsonFile("settings.json")
+		        .AddConfigurationDetails(details)
 		        .Build();
 
 	        var hostConfiguration = new HostConfigurationBuilder()
-		        .Build(applicationConfiguration)
-                .UseTrayIconHost(
-                    this,
-                    "Icon-Logo-White-Shaded.ico",
-                    "Icon-Logo-Black.ico",
-                    "Icon-Logo-Red.ico");
-            TrayIconHost.Start(hostConfiguration);
+		        .Build(applicationConfiguration);
+            //     .UseTrayIconHost(
+            //         this,
+            //         "Icon-Logo-White-Shaded.ico",
+            //         "Icon-Logo-Black.ico",
+            //         "Icon-Logo-Red.ico");
+            // TrayIconHost.Start(hostConfiguration);
+            
+            throw new InvalidOperationException("Tray icon host not implemented currently.");
         }
 	}
 }
