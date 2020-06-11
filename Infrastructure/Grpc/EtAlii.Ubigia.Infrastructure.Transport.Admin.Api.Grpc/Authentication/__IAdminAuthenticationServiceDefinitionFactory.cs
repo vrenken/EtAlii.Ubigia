18323c9332bd4bc5dc5af8a646ecaca5ddfe10d4
Peter Vrenken <github.com@petervrenken.nl>
2020-06-11 21:54:51 +0200

    Disabled GrpcServiceDefinition factories.

---
+++ Infrastructure/Grpc/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Grpc/Authentication/__IAdminAuthenticationServiceDefinitionFactory.cs
@@ -0,0 +1,10 @@
+﻿// namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Grpc
+// {
+//     using EtAlii.Ubigia.Infrastructure.Functional;
+//     using global::Grpc.Core;
+//
+//     public interface IAdminAuthenticationServiceDefinitionFactory
+//     {
+//         ServerServiceDefinition Create(IInfrastructure infrastructure);
+//     }
+// }
