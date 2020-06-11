9d517775110cc84aae5ec61ce3f70e06e00c5a1b
Peter Vrenken <github.com@petervrenken.nl>
2020-06-11 21:54:51 +0200

    Disabled GrpcServiceDefinition factories.

---
+++ Infrastructure/Grpc/EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Grpc/Storages/__IAdminSpaceServiceDefinitionFactory.cs
@@ -0,0 +1,10 @@
+﻿// namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Api.Grpc
+// {
+//     using EtAlii.Ubigia.Infrastructure.Functional;
+//     using global::Grpc.Core;
+//
+//     public interface IAdminStorageServiceDefinitionFactory
+//     {
+//         ServerServiceDefinition Create(IInfrastructure infrastructure, IAccountAuthenticationInterceptor accountAuthenticationInterceptor);
+//     }
+// }
---
+++ Infrastructure/Grpc/EtAlii.Ubigia.Infrastructure.Transport.User.Api.Grpc/ContentDefinition/__IUserContentDefinitionServiceDefinitionFactory.cs
@@ -0,0 +1,10 @@
+﻿// namespace EtAlii.Ubigia.Infrastructure.Transport.User.Api.Grpc
+// {
+//     using EtAlii.Ubigia.Infrastructure.Functional;
+//     using global::Grpc.Core;
+//
+//     public interface IUserContentDefinitionServiceDefinitionFactory
+//     {
+//         ServerServiceDefinition Create(IInfrastructure infrastructure, ISpaceAuthenticationInterceptor spaceAuthenticationInterceptor);
+//     }
+// }
