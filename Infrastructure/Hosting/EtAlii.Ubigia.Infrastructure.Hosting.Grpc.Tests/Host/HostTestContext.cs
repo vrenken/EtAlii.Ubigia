7486aed633ee2133290fef29a2f2a54b87595900
Peter Vrenken <github.com@petervrenken.nl>
2020-06-14 13:34:06 +0200

    Correction of HostTestContext paths.

--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Grpc.Tests/Host/HostTestContext.cs
+++ Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Grpc.Tests/Host/HostTestContext.cs
@@ -1,19 +1,27 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
 {
+	using System;
 	using System.Threading.Tasks;
 	using EtAlii.Ubigia.Api.Transport;
 	using EtAlii.Ubigia.Infrastructure.Functional;
-	using EtAlii.Ubigia.Infrastructure.Hosting.TestHost.Grpc;
 	using EtAlii.Ubigia.Infrastructure.Transport;
 
 	public partial class HostTestContext<TInfrastructureTestHost> : HostTestContextBase<TInfrastructureTestHost>, IHostTestContext<TInfrastructureTestHost>
-        where TInfrastructureTestHost : class, IInfrastructureTestHost
+        where TInfrastructureTestHost : class, IInfrastructureTestHostBase
     {
 	    protected HostTestContext()
 		    : base("Host/settings.json")
 	    {
 	    }
-	    
+
+	    public override async Task Start()
+	    {
+		    await base.Start();
+
+		    ManagementServiceAddress = new Uri($"{HostSchemaAndIp}:{Ports["AdminPort"]}{Paths["AdminApi"]}");
+		    DataServiceAddress = new Uri($"{HostSchemaAndIp}:{Ports["UserPort"]}{Paths["UserApi"]}");
+	    }
+
 	    public Task<ISystemConnection> CreateSystemConnection()
 	    {
 		    var connectionConfiguration = new SystemConnectionConfiguration()
