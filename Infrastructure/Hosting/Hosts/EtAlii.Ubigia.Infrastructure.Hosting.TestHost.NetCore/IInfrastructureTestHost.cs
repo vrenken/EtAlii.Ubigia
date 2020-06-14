4f5b0e7ec0317ca46cdd4a5a37ea8e6d6e49361e
Peter Vrenken <github.com@petervrenken.nl>
2020-06-14 13:26:44 +0200

    Minor cleanup

--- Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.TestHost.NetCore/IInfrastructureTestHost.cs
+++ Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.TestHost.NetCore/IInfrastructureTestHost.cs
@@ -1,15 +1,8 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.TestHost.NetCore
 {
     using EtAlii.Ubigia.Infrastructure.Hosting.Tests;
-    using EtAlii.Ubigia.Infrastructure.Transport.Admin.NetCore;
-    using EtAlii.Ubigia.Infrastructure.Transport.User.NetCore;
-    using EtAlii.Ubigia.Storage;
 
     public interface IInfrastructureTestHost : IInfrastructureTestHostBase
     {
-        IStorage Storage { get; }
-
-        AdminModule AdminModule { get; }
-        UserModule UserModule { get; }
     }
 }
