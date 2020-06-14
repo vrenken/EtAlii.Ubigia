4b779e255d1b656d6e518a3bc62d6a1574a81116
Peter Vrenken <github.com@petervrenken.nl>
2020-06-14 13:27:14 +0200

    Minor cleanup

--- Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.TestHost.Grpc/IInfrastructureTestHost.cs
+++ Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.TestHost.Grpc/IInfrastructureTestHost.cs
@@ -1,15 +1,12 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Hosting.TestHost.Grpc
 {
     using EtAlii.Ubigia.Infrastructure.Hosting.Tests;
-    using EtAlii.Ubigia.Infrastructure.Transport.Admin.Grpc;
-    using EtAlii.Ubigia.Infrastructure.Transport.User.Grpc;
     using EtAlii.Ubigia.Storage;
+    // using EtAlii.Ubigia.Infrastructure.Transport.Admin.Grpc;
+    // using EtAlii.Ubigia.Infrastructure.Transport.User.Grpc;
 
     public interface IInfrastructureTestHost : IInfrastructureTestHostBase
     {
         IStorage Storage { get; }
-
-        AdminModule AdminModule { get; }
-        UserModule UserModule { get; }
     }
 }
