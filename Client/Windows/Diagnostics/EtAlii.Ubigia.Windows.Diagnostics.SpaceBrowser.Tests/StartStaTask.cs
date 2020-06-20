470adfd2540cea69b1f27d36749d2e724dab4df2
Peter Vrenken <github.com@petervrenken.nl>
2020-06-21 01:18:56 +0200

    Added client unit tests.

---
+++ Client/Windows/Diagnostics/EtAlii.Ubigia.Windows.Diagnostics.SpaceBrowser.Tests/StartStaTask.cs
@@ -0,0 +1,29 @@
+namespace EtAlii.Ubigia.Windows.Diagnostics.SpaceBrowser.Tests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class StaHelper
+    {
+        public static Task StartStaTask(Action action)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    action();
+                    tcs.SetResult(new object());
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            return tcs.Task;
+        }
+    }
+}
