de0b74a7c81ad35cd7edb50ff2272ad444eba5a0
Peter Vrenken <github.com@petervrenken.nl>
2020-06-23 23:15:22 +0200

    Cleanup of SignalR code

--- Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR/UserSignalRService.cs
+++ Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.User.Api.SignalR/UserSignalRService.cs
@@ -28,7 +28,7 @@
 		        .AddSingleton(infrastructure.Content)
 		        .AddSingleton(infrastructure.ContentDefinition)
 
-		        .AddInfrastructureSimpleAuthentication(infrastructure)
+		        .AddInfrastructureAuthentication(infrastructure)
 		        .AddInfrastructureSerialization()
 
 		        .AddRouting()
@@ -74,65 +74,5 @@
 			        endPoints.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
 		        });
         }
-
-       //  protected override void OnConfigureApplication(IApplicationBuilder applicationBuilder)
-       //  {
-       //      var infrastructure = System.Services.OfType<IInfrastructureService>().Single().Infrastructure;
-       //
-       //      applicationBuilder.UseBranchWithServices(Port, AbsoluteUri.User.Api.SignalR.BaseUrl,
-       //          services =>
-       //          {
-	      //           services
-		     //            .AddSingleton(infrastructure.Spaces)
-		     //            .AddSingleton(infrastructure.Accounts)
-		     //            .AddSingleton(infrastructure.Roots)
-		     //            .AddSingleton(infrastructure.Entries)
-		     //            .AddSingleton(infrastructure.Properties)
-		     //            .AddSingleton(infrastructure.Content)
-		     //            .AddSingleton(infrastructure.ContentDefinition)
-       //
-		     //            .AddInfrastructureSimpleAuthentication(infrastructure)
-		     //            .AddInfrastructureSerialization()
-       //
-		     //            .AddCors()
-		     //            .AddSignalR(options =>
-		     //            {
-			    //             if (Debugger.IsAttached)
-			    //             {
-				   //              options.EnableDetailedErrors = Debugger.IsAttached;
-			    //             }
-		     //            })
-		     //            .AddHubOptions<ContentHub>(options =>
-		     //            {
-							// const long maximumReceiveMessageSizeInMegaByte = 1024 * 1024 * 2;
-							// options.MaximumReceiveMessageSize = maximumReceiveMessageSizeInMegaByte;
-		     //            })
-		     //            .AddNewtonsoftJsonProtocol(options => SerializerFactory.Configure(options.PayloadSerializerSettings));
-       //          },
-       //          appBuilder =>
-       //          {
-       //              appBuilder
-       //                  .UseCors(configuration =>
-	      //               {
-		     //                configuration.AllowAnyMethod();
-		     //                configuration.AllowAnyHeader();
-       //                      configuration.AllowAnyOrigin(); 
-       //                  })
-       //                  .UseRouting()
-       //                  .UseEndpoints(endPoints =>
-       //                  {
-	      //                   endPoints.MapHub<AuthenticationHub>(SignalRHub.Authentication);
-       //
-	      //                   endPoints.MapHub<AccountHub>(SignalRHub.Account);
-	      //                   endPoints.MapHub<SpaceHub>(SignalRHub.Space);
-       //
-	      //                   endPoints.MapHub<RootHub>(SignalRHub.Root);
-	      //                   endPoints.MapHub<EntryHub>(SignalRHub.Entry);
-	      //                   endPoints.MapHub<PropertiesHub>(SignalRHub.Property);
-	      //                   endPoints.MapHub<ContentHub>(SignalRHub.Content);
-	      //                   endPoints.MapHub<ContentDefinitionHub>(SignalRHub.ContentDefinition);
-       //                  });
-       //          });
-       //  }
     }
 }
