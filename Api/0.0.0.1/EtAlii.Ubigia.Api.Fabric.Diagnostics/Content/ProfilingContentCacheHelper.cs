428f8791afbc4bdee815f2ba844b0ec092f136e8
Peter Vrenken <vrenken@live.nl>
2020-06-25 23:50:01 +0200

    Removed obsolete usings.

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Fabric.Diagnostics/Content/ProfilingContentCacheHelper.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Fabric.Diagnostics/Content/ProfilingContentCacheHelper.cs
@@ -1,7 +1,6 @@
 namespace EtAlii.Ubigia.Api.Fabric.Diagnostics
 {
     using EtAlii.Ubigia.Api.Diagnostics.Profiling;
-    using EtAlii.Ubigia.Api.Fabric;
 
     internal class ProfilingContentCacheHelper : IContentCacheHelper
     {
