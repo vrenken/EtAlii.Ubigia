502c274eb50812d0aa8ba4629054c1cda4f1a06e
Peter Vrenken <vrenken@live.nl>
2020-09-03 17:45:39 +0200

    Cleaned up AssemblyInfo usage in test projects.

---
+++ Client/Windows/Console/EtAlii.Ubigia.PowerShell.Tests/UnitTestSettings.cs
@@ -0,0 +1,4 @@
+﻿using Xunit;
+
+// For now the PowerShell API requires a bit more delicate testing. 
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
--- Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.TestHost/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿//[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Tests")]
-//[assembly: InternalsVisibleTo("EtAlii.Ubigia.Infrastructure.Tests")]
-//[assembly: InternalsVisibleTo("EtAlii.Ubigia.Hosting.Tests")]
