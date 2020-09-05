8c94d2e52a6dffe99fa4c27df56649e22031bc4c
Peter Vrenken <vrenken@live.nl>
2020-09-05 17:48:16 +0200

    Code cleanup

--- Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL.Tests/0. Base/Content/IntegrationTests/Win32LogicalUnitTestContext.cs
+++ Api/0.0.0.1/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL.Tests/0. Base/Content/IntegrationTests/Win32LogicalUnitTestContext.cs
@@ -4,12 +4,14 @@
     using System.IO;
     using EtAlii.Ubigia.Api.Logical.Tests;
     using EtAlii.Ubigia.Tests;
+    using EtAlii.xTechnology.Networking;
 
     public class Win32LogicalUnitTestContext : IDisposable
     {
-        public string TestFile2MImage;
-        public string TestFile10MRaw;
-        public string TestFile100MRaw;
+        private readonly Guid _uniqueId = Guid.Parse("5F763915-44A5-496F-B478-BFA42F60E406"); 
+        public string TestFile2MImage { get; }
+        public string TestFile10MRaw { get; }
+        public string TestFile100MRaw { get; }
 
         public FileComparer FileComparer { get; }
         public FolderComparer FolderComparer { get; }
@@ -22,14 +24,17 @@
 
             LogicalTestContext = new LogicalTestContextFactory().Create();
 
-            // Getting Temp file names to use
-            TestFile2MImage = Win32TestHelper.CreateTemporaryFileName();
-            TestFile10MRaw = Win32TestHelper.CreateTemporaryFileName();
-            TestFile100MRaw = Win32TestHelper.CreateTemporaryFileName();
+            using (var _ = new SystemSafeExecutionScope(_uniqueId))
+            {
+                // Getting Temp file names to use
+                TestFile2MImage = Win32TestHelper.CreateTemporaryFileName();
+                TestFile10MRaw = Win32TestHelper.CreateTemporaryFileName();
+                TestFile100MRaw = Win32TestHelper.CreateTemporaryFileName();
 
-            Win32TestHelper.SaveResourceTestImage(TestFile2MImage);
-            Win32TestHelper.SaveTestFile(TestFile10MRaw, 10);
-            Win32TestHelper.SaveTestFile(TestFile100MRaw, 100);
+                Win32TestHelper.SaveResourceTestImage(TestFile2MImage);
+                Win32TestHelper.SaveTestFile(TestFile10MRaw, 10);
+                Win32TestHelper.SaveTestFile(TestFile100MRaw, 100);
+            }
         }
 
         public void Dispose()
--- UnitTestSettings.cs
+++ UnitTestSettings.cs
@@ -1,6 +1,6 @@
 ﻿using Xunit;
 
 // Settings in this file are shared across most Unit test projects. 
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = false)]
-//[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
+//[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = false)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
  
