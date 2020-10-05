7d0eb30ef25d90f3b1fd4e870fcaa7a4cdd91a15
Peter Vrenken <vrenken@live.nl>
2020-10-05 22:36:27 +0200

    Removed rider warning.

--- Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/Roots/Handlers/Matching/_Model/MatchResult.cs
+++ Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/Roots/Handlers/Matching/_Model/MatchResult.cs
@@ -6,7 +6,6 @@
         public PathSubjectPart[] Match { get; }
         public PathSubjectPart[] Rest { get; }
 
-        // ReSharper disable InconsistentNaming
         public static readonly MatchResult NoMatch = new MatchResult(null, new PathSubjectPart[0], new PathSubjectPart[0]);
 
         public MatchResult(IRootHandler rootHandler, PathSubjectPart[] match, PathSubjectPart[] rest)
