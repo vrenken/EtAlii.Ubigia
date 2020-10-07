c44d976676c0a170440c6fc4f7d21b024a32d5f2
Peter Vrenken <vrenken@live.nl>
2020-10-07 10:49:16 +0200

    Removed rider warnings.

--- Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/_Model/Subjects/Paths/Parts/Typed/TypedPathFormatter.cs
+++ Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/_Model/Subjects/Paths/Parts/Typed/TypedPathFormatter.cs
@@ -1,63 +1,76 @@
-﻿// ReSharper disable InconsistentNaming
-namespace EtAlii.Ubigia.Api.Functional.Scripting
+﻿namespace EtAlii.Ubigia.Api.Functional.Scripting
 {
-    using System;
     using System.Linq;
 
     public class TypedPathFormatter
     {
         public static class Time
         {
-            public static TypedPathFormatter YearFormatter => _yearFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _yearFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("YYYY", @"^[0123]?[0123456789]{1,3}$"));
-            public static TypedPathFormatter MonthFormatter => _monthFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _monthFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("MM", @"^[01]?[0123456789]$"));
-            public static TypedPathFormatter DayFormatter => _dayFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _dayFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("DD", @"^[0123]?[0123456789]$"));
-            public static TypedPathFormatter HourFormatter => _hourFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _hourFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("HH", @"^[012]?[0123456789]$"));
-            public static TypedPathFormatter MinuteFormatter => _minuteFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _minuteFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("mm", @"^[012345]?[0123456789]$"));
-            public static TypedPathFormatter SecondFormatter => _secondFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _secondFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("ss", @"^[012345]?[0123456789]$"));
-            public static TypedPathFormatter MillisecondFormatter => _millisecondFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _millisecondFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("MMM", @"^[0123456789]{1,3}$"));
+            public static readonly TypedPathFormatter YearFormatter;
+            public static readonly TypedPathFormatter MonthFormatter;
+            public static readonly TypedPathFormatter DayFormatter;
+            public static readonly TypedPathFormatter HourFormatter;
+            public static readonly TypedPathFormatter MinuteFormatter;
+            public static readonly TypedPathFormatter SecondFormatter;
+            public static readonly TypedPathFormatter MillisecondFormatter;
+
+            static Time()
+            {
+                YearFormatter = new TypedPathFormatter("YYYY", @"^[0123]?[0123456789]{1,3}$");
+                MonthFormatter = new TypedPathFormatter("MM", @"^[01]?[0123456789]$");
+                DayFormatter = new TypedPathFormatter("DD", @"^[0123]?[0123456789]$");
+                HourFormatter = new TypedPathFormatter("HH", @"^[012]?[0123456789]$");
+                MinuteFormatter = new TypedPathFormatter("mm", @"^[012345]?[0123456789]$");
+                SecondFormatter = new TypedPathFormatter("ss", @"^[012345]?[0123456789]$");
+                MillisecondFormatter = new TypedPathFormatter("MMM", @"^[0123456789]{1,3}$");
+            }
         }
 
         public static class Text
         {
-            public static TypedPathFormatter WordFormatter => _wordFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _wordFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("WORD", @"^\p{L}+$"));
-            public static TypedPathFormatter NumberFormatter => _numberFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _numberFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("NUMBER", @"^[0123456789]+$"));
+            public static readonly TypedPathFormatter WordFormatter;
+            public static readonly TypedPathFormatter NumberFormatter;
+
+            static Text()
+            {
+                WordFormatter = new TypedPathFormatter("WORD", @"^\p{L}+$");
+                NumberFormatter = new TypedPathFormatter("NUMBER", @"^[0123456789]+$");
+            }
         }
 
         public static class Name
         {
-            public static TypedPathFormatter FirstNameFormatter => _firstNameFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _firstNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("FIRSTNAME", @"^\p{L}+$"));
-            public static TypedPathFormatter LastNameFormatter => _lastNameFormatter.Value;
-            private static readonly Lazy<TypedPathFormatter> _lastNameFormatter = new Lazy<TypedPathFormatter>(() => new TypedPathFormatter("LASTNAME", @"^\p{L}+$"));
+            public static readonly TypedPathFormatter FirstNameFormatter;
+            public static readonly TypedPathFormatter LastNameFormatter;
+
+            static Name()
+            {
+                FirstNameFormatter = new TypedPathFormatter("FIRSTNAME", @"^\p{L}+$");
+                LastNameFormatter = new TypedPathFormatter("LASTNAME", @"^\p{L}+$");
+            }
         }
 
-        public static TypedPathFormatter[] All => _all.Value;
-        private static readonly Lazy<TypedPathFormatter[]> _all = new Lazy<TypedPathFormatter[]>(() => new[]
+        public static readonly TypedPathFormatter[] All;
+
+        static TypedPathFormatter()
         {
-            Time.YearFormatter,
-            Time.MonthFormatter,
-            Time.DayFormatter,
-            Time.HourFormatter,
-            Time.MinuteFormatter,
-            Time.SecondFormatter,
-            Time.MillisecondFormatter,
+            All = new[]
+            {
+                Time.YearFormatter,
+                Time.MonthFormatter,
+                Time.DayFormatter,
+                Time.HourFormatter,
+                Time.MinuteFormatter,
+                Time.SecondFormatter,
+                Time.MillisecondFormatter,
 
-            Text.WordFormatter,
-            Text.NumberFormatter,
+                Text.WordFormatter,
+                Text.NumberFormatter,
 
-            Name.FirstNameFormatter,
-            Name.LastNameFormatter,
-        });
-
+                Name.FirstNameFormatter,
+                Name.LastNameFormatter,
+            };
+        }
         public string Type { get; }
         public string Regex { get; }
 
