05880c948f42d298976859f86790ea1690f59dc1
Peter Vrenken <vrenken@live.nl>
2020-10-10 14:39:20 +0200

    Test for build server

--- UnitTestSettings.cs
+++ UnitTestSettings.cs
@@ -2,5 +2,10 @@
 
 // Settings in this file are shared across most Unit test projects. 
 //[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = false)]
+
+#if UBIGIA_RUNNING_ON_BUILD_AGENT
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
+#else
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
+#endif 
  
