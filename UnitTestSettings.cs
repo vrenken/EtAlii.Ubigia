345c8259a42e873bd06dcfaae3e6e123e9dabac0
Peter Vrenken <vrenken@live.nl>
2020-10-10 15:22:48 +0200

    Bugfix in build server unit test sequentialization.

--- UnitTestSettings.cs
+++ UnitTestSettings.cs
@@ -2,8 +2,8 @@
 
 // We want to run the unit tests in sequence on the build agent. 
 #if UBIGIA_IS_RUNNING_ON_BUILD_AGENT
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
 #else
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
 #endif 
  
