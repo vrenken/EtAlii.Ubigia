a6bc158193f30f0729eebd0df1517344f40da5e2
Peter Vrenken <vrenken@live.nl>
2020-10-11 15:02:56 +0200

    Added temporary SeriLog logger instance.
    We'll replace the xTechnology.ILogger with the Serilog Ilogger soon.

--- Frameworks/EtAlii.xTechnology.Diagnostics/EtAlii.xTechnology.Diagnostics.Serilog/Logger.cs
+++ Frameworks/EtAlii.xTechnology.Diagnostics/EtAlii.xTechnology.Diagnostics.Serilog/Logger.cs
@@ -1,147 +1,115 @@
-﻿//using Gibraltar.Agent;
-//using System;
+﻿namespace EtAlii.xTechnology.Diagnostics.Serilog
+{
+    using System;
+    using System.Diagnostics;
+    using global::Serilog;
 
-//namespace EtAlii.xTechnology.Diagnostics
-//{
-//    using System.Diagnostics;
+    [DebuggerStepThrough]
+    public class SerilogLogger : global::EtAlii.xTechnology.Diagnostics.ILogger
+    {
+        private readonly ILogger _logger;
 
-//    [DebuggerStepThrough]
-//    public class Logger : ILogger
-//    {
-//        private readonly string _name;
-//        private readonly string _category;
+        internal SerilogLogger(string name, string category)
+        {
+            _logger = Log
+                .ForContext("name", name)
+                .ForContext("category", category);
+        }
 
-//        private static string _productName;
-//        private static string _applicationName;
-//        private static Version _applicationVersion;
-//        private static bool _hasCustomNaming;
+        public void Warning(string message)
+        {
+            _logger.Warning(message);
+        }
 
-//        private const int _framesToSkip = 1;
+        public void Warning(string message, params object[] args)
+        {
+            _logger.Warning(message, args);
 
-//        public LogLevel Level { get; set; } = LogLevel.Info;
+            // if (Level >= LogLevel.Warning)
+            // {
+            //     Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
+            // }
+        }
 
-//        internal Logger(string name, string category)
-//        {
-//            _category = category;
-//            _name = name;
-//        }
+        public void Warning(string message, Exception e)
+        {
+            _logger.Warning(e, message);
 
-//        public static void StartSession()
-//        {
-//            Log.StartSession(_framesToSkip, null);
-//        }
+            // if (Level >= LogLevel.Warning)
+            // {
+            //     Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message);
+            // }
+        }
 
-//        public static void StartSession(string productName, string applicationName, Version applicationVersion)
-//        {
-//            _hasCustomNaming = true;
-//            _productName = productName;
-//            _applicationName = applicationName;
-//            _applicationVersion = applicationVersion;
+        public void Warning(string message, Exception e, params object[] args)
+        {
+            _logger.Warning(e, message, args);
 
-//            Log.Initializing += OnLogInitializing;
-//            Log.StartSession(_framesToSkip, null);
-//        }
+            // if (Level >= LogLevel.Warning)
+            // {
+            //     Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message, args);
+            // }
+        }
 
-//        private static void OnLogInitializing(object sender, LogInitializingEventArgs e)
-//        {
-//            e.Configuration.Publisher.ProductName = _productName;
-//            e.Configuration.Publisher.ApplicationName = _applicationName;
-//            e.Configuration.Publisher.ApplicationVersion = _applicationVersion;
-//        }
+        public void Verbose(string message)
+        {
+            _logger.Verbose(message);
 
-//        public static void EndSession()
-//        {
-//            Log.EndSession( SessionStatus.Normal, _framesToSkip, null);
-//            if (_hasCustomNaming)
-//            {
-//                Log.Initializing -= OnLogInitializing;
-//            }
-//        }
+            // if (Level >= LogLevel.Verbose)
+            // {
+            //     Log.Write(LogMessageSeverity.Verbose, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message);
+            // }
+        }
 
-//        //public static void ReportUnhandledException(Exception e)
-//        //{
-//        //    Log.ReportException(e, "Unhandled", true, false);
-//        //}
+        public void Verbose(string message, params object[] args)
+        {
+            _logger.Verbose(message, args);
+            //
+            // if (Level >= LogLevel.Verbose)
+            // {
+            //     Log.Write(LogMessageSeverity.Verbose, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
+            // }
+        }
 
-//        public void Warning(string message)
-//        {
-//            if (Level >= LogLevel.Warning)
-//            {
-//                Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message);
-//            }
-//        }
+        public void Info(string message)
+        {
+            _logger.Information(message);
 
-//        public void Warning(string message, params object[] args)
-//        {
-//            if (Level >= LogLevel.Warning)
-//            {
-//                Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
-//            }
-//        }
+            // if (Level >= LogLevel.Info)
+            // {
+            //     Log.Write(LogMessageSeverity.Information, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message);
+            // }
+        }
 
-//        public void Warning(string message, Exception e)
-//        {
-//            if (Level >= LogLevel.Warning)
-//            {
-//                Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message);
-//            }
-//        }
+        public void Info(string message, params object[] args)
+        {
+            _logger.Information(message, args);
 
-//        public void Warning(string message, Exception e, params object[] args)
-//        {
-//            if (Level >= LogLevel.Warning)
-//            {
-//                Log.Write(LogMessageSeverity.Warning, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message, args);
-//            }
-//        }
+            // if (Level >= LogLevel.Info)
+            // {
+            //     Log.Write(LogMessageSeverity.Information, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
+            // }
+        }
 
-//        public void Verbose(string message)
-//        {
-//            if (Level >= LogLevel.Verbose)
-//            {
-//                Log.Write(LogMessageSeverity.Verbose, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message);
-//            }
-//        }
+        public void Critical(string message, Exception e)
+        {
+            _logger.Error(e, message);
 
-//        public void Verbose(string message, params object[] args)
-//        {
-//            if (Level >= LogLevel.Verbose)
-//            {
-//                Log.Write(LogMessageSeverity.Verbose, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
-//            }
-//        }
+            // if (Level >= LogLevel.Critical)
+            // {
+            //     Log.Write(LogMessageSeverity.Critical, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message);
+            // }
+        }
 
-//        public void Info(string message)
-//        {
-//            if (Level >= LogLevel.Info)
-//            {
-//                Log.Write(LogMessageSeverity.Information, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message);
-//            }
-//        }
+        public void Critical(string message, Exception e, params object[] args)
+        {
+            _logger.Error(e, message, args);
 
-//        public void Info(string message, params object[] args)
-//        {
-//            if (Level >= LogLevel.Info)
-//            {
-//                Log.Write(LogMessageSeverity.Information, _name, _framesToSkip, null, LogWriteMode.Queued, null, _category, null, message, args);
-//            }
-//        }
-
-//        public void Critical(string message, Exception e)
-//        {
-//            if (Level >= LogLevel.Critical)
-//            {
-//                Log.Write(LogMessageSeverity.Critical, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message);
-//            }
-//        }
-
-//        public void Critical(string message, Exception e, params object[] args)
-//        {
-//            if (Level >= LogLevel.Critical)
-//            {
-//                Log.Write(LogMessageSeverity.Critical, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message, args);
-//            }
-//        }
-//    }
-//}
+            // if (Level >= LogLevel.Critical)
+            // {
+            //     Log.Write(LogMessageSeverity.Critical, _name, _framesToSkip, e, LogWriteMode.Queued, null, _category, null, message, args);
+            // }
+        }
+    }
+}
 
