f1867ce4b0a318c0e8d88694e02586542a8b8dff
Peter Vrenken <vrenken@live.nl>
2020-10-17 00:22:14 +0200

    Added comments.

--- Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/_Model/Script.cs
+++ Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/_Model/Script.cs
@@ -8,21 +8,15 @@
     /// </summary>
     public class Script
     {
+        /// <summary>
+        /// The executable sequences that make up the script.
+        /// </summary>
         public IEnumerable<Sequence> Sequences { get; }
 
-        internal Script(Sequence sequences)
-        {
-            Sequences = new[] { sequences };
-        }
+        internal Script(Sequence sequences) => Sequences = new[] { sequences };
 
-        internal Script(IEnumerable<Sequence> sequences)
-        {
-            Sequences = sequences;
-        }
+        internal Script(IEnumerable<Sequence> sequences) => Sequences = sequences;
 
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, Sequences);
-        }
+        public override string ToString() => string.Join(Environment.NewLine, Sequences);
     }
 }
