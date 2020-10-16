45cbfa2a5e03972ed06d3aefe55015b43fad2ca4
Peter Vrenken <vrenken@live.nl>
2020-10-16 15:25:50 +0200

    Took the powershell tests into the main unit test parallelization pattern.

--- Client/Windows/Console/EtAlii.Ubigia.PowerShell.Tests/UnitTestSettings.Local.cs
+++
@@ -1,4 +0,0 @@
-﻿using Xunit;
-
-// TODO: PowerShell doesn't like when tests are run in parallel.
-[assembly: CollectionBehavior(collectionBehavior: CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
