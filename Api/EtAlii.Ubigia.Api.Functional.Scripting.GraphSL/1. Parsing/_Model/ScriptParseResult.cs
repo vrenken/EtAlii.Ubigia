1ab00e23d7ef9b7054b080bb0e7e0e9bacf865bb
Peter Vrenken <vrenken@live.nl>
2020-10-17 00:22:14 +0200

    Added comments.

--- Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/1. Parsing/_Model/ScriptParseResult.cs
+++ Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL/1. Parsing/_Model/ScriptParseResult.cs
@@ -2,13 +2,22 @@
 {
     public class ScriptParseResult
     {
+        /// <summary>
+        /// The source that got parsed.
+        /// </summary>
         public string Source { get; }
-
+        
+        /// <summary>
+        /// The script that got created from the provided source.
+        /// </summary>
         public Script Script { get; }
-
+        
+        /// <summary>
+        /// If any, this property shows any parsing errors.
+        /// </summary>
         public ScriptParserError[] Errors { get; }
 
-        public ScriptParseResult(string source, Script script, ScriptParserError[] errors)
+        internal ScriptParseResult(string source, Script script, ScriptParserError[] errors)
         {
             Source = source;
             Script = script;
