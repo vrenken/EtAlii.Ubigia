1383ba97a23a0dad1e04ef3192ffb2c4dc035b62
Peter Vrenken <vrenken@live.nl>
2020-10-17 00:22:54 +0200

    Added correlation ids to the script parser and processor.

--- Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL.Diagnostics/Parsing/LoggingScriptParser.cs
+++ Api/EtAlii.Ubigia.Api.Functional.Scripting.GraphSL.Diagnostics/Parsing/LoggingScriptParser.cs
@@ -1,6 +1,7 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Scripting
 {
     using System;
+    using EtAlii.xTechnology.Diagnostics;
     using Serilog;
 
     internal class LoggingScriptParser : IScriptParser
@@ -15,34 +16,46 @@
 
         public ScriptParseResult Parse(string text)
         {
-            var lines = text.Replace("\r\n", "\n").Split('\n');
-            var line = lines.Length == 1 ? lines[0] : $"{lines[0]}...";
-            var message = "Parsing text (Text: {line})";
-            _logger.Information(message, line);
-            var start = Environment.TickCount;
+            // We want to be able to track method calls throughout the whole application stack.
+            // Including across network and process boundaries.
+            // For this we create a unique correlationId and pass it through all involved systems.
+            using (ContextCorrelator.BeginCorrelationScope("CorrelationId", Guid.NewGuid(), false))
+            {
+                var lines = text.Replace("\r\n", "\n").Split('\n');
+                var line = lines.Length == 1 ? lines[0] : $"{lines[0]}...";
+                var message = "Parsing text (Text: {line})";
+                _logger.Information(message, line);
+                var start = Environment.TickCount;
 
-            var scriptParseResult = _parser.Parse(text);
+                var scriptParseResult = _parser.Parse(text);
 
-            var duration = TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds;
-            message = "Parsed text (Text: \"{line}\" Duration: {duration}ms)";
-            _logger.Information(message, line, duration);
-
-            return scriptParseResult;
+                var duration = TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds;
+                message = "Parsed text (Text: \"{line}\" Duration: {duration}ms)";
+                _logger.Information(message, line, duration);
+                
+                return scriptParseResult;
+            }
         }
 
         public ScriptParseResult Parse(string[] text)
         {
-            var line = text.Length == 1 ? text[0] : $"{text[0]}...";
-            var message = $"Parsing text (Text: {line})";
-            _logger.Information(message);
-            var start = Environment.TickCount;
+            // We want to be able to track method calls throughout the whole application stack.
+            // Including across network and process boundaries.
+            // For this we create a unique correlationId and pass it through all involved systems.
+            using (ContextCorrelator.BeginCorrelationScope("CorrelationId", Guid.NewGuid(), false))
+            {
+                var line = text.Length == 1 ? text[0] : $"{text[0]}...";
+                var message = $"Parsing text (Text: {line})";
+                _logger.Information(message);
+                var start = Environment.TickCount;
 
-            var scriptParseResult = _parser.Parse(text);
+                var scriptParseResult = _parser.Parse(text);
 
-            message = $"Parsed text (Text: \"{line}\" Duration: {TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds}ms)";
-            _logger.Information(message);
+                message = $"Parsed text (Text: \"{line}\" Duration: {TimeSpan.FromTicks(Environment.TickCount - start).TotalMilliseconds}ms)";
+                _logger.Information(message);
 
-            return scriptParseResult;
+                return scriptParseResult;
+            }
         }
     }
 }
