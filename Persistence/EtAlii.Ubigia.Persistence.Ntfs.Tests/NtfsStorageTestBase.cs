83c9b888b8e0c0de8da738212b534b8ffcda28f9
Peter Vrenken <vrenken@live.nl>
2020-10-18 00:41:33 +0200

    Cleaned up temporary test files after the test.

--- Persistence/EtAlii.Ubigia.Persistence.Ntfs.Tests/NtfsStorageTestBase.cs
+++ Persistence/EtAlii.Ubigia.Persistence.Ntfs.Tests/NtfsStorageTestBase.cs
@@ -1,27 +1,23 @@
 ﻿namespace EtAlii.Ubigia.Persistence.Ntfs.Tests
 {
-    using System;
+    using EtAlii.Ubigia.Persistence.Tests;
     using EtAlii.Ubigia.Tests;
     using EtAlii.xTechnology.Diagnostics;
 
-    public abstract class NtfsStorageTestBase : IDisposable
+    public abstract class NtfsStorageTestBase : FileSystemStorageTestBase
     {
         protected TestContentFactory TestContentFactory { get; }
         protected TestContentDefinitionFactory TestContentDefinitionFactory { get; }
         protected TestPropertiesFactory TestPropertiesFactory { get; }
-
-        protected IStorage Storage { get; private set; }
-
-        private readonly string _rootFolder = @"c:\temp\" + Guid.NewGuid() + @"\";
 
         protected NtfsStorageTestBase()
         {
             TestContentFactory = new TestContentFactory();
             TestContentDefinitionFactory = new TestContentDefinitionFactory();
             TestPropertiesFactory = new TestPropertiesFactory();
-
+            
             Storage = CreateStorage();
-
+            
             var folder = Storage.PathBuilder.BaseFolder;
             if (Storage.FolderManager.Exists(folder))
             {
@@ -29,33 +25,12 @@
             }
         }
 
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            // Cleanup
-            if (disposing)
-            {
-                Storage = null;
-            }
-        }
-
-        ~NtfsStorageTestBase()
-        {
-            Dispose(false);
-        }
-
         private IStorage CreateStorage()
         {
             var configuration = new StorageConfiguration()
                 .Use(TestAssembly.StorageName)
                 .Use(DiagnosticsConfiguration.Default)
-                .UseNtfsStorage(_rootFolder);
+                .UseNtfsStorage(RootFolder);
 
             return new StorageFactory().Create(configuration);
         }
---
+++ Persistence/EtAlii.Ubigia.Persistence.Tests/FileSystemStorageTestBase.cs
@@ -0,0 +1,50 @@
+﻿namespace EtAlii.Ubigia.Persistence.Tests
+{
+    using System;
+    using System.IO;
+
+    //you have to label the class with this or it is never scanned for methods
+    public abstract class FileSystemStorageTestBase : IDisposable
+    {
+        protected IStorage Storage { get; set; }
+
+        protected readonly string RootFolder = @"c:\temp\" + Guid.NewGuid() + @"\";
+
+        protected FileSystemStorageTestBase()
+        {
+            Directory.CreateDirectory(RootFolder);
+            AppDomain.CurrentDomain.ProcessExit += (o,e) => DeleteTestData();
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+            if (disposing)
+            {
+                Storage = null;
+            }
+            
+            DeleteTestData();
+        }
+
+        private void DeleteTestData()
+        {
+            // Let's cleanup.
+            if(Directory.Exists(RootFolder))
+            {
+                Directory.Delete(RootFolder, true);
+            }
+        }
+
+        ~FileSystemStorageTestBase()
+        {
+            Dispose(false);
+        }
+    }
+}
