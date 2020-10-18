35b17900824ebe09473ec85146be9c814cc31805
Peter Vrenken <vrenken@live.nl>
2020-10-18 02:23:47 +0200

    Reverting back to old state.

--- UnitTestSettings.cs
+++ UnitTestSettings.cs
@@ -2,7 +2,7 @@
 
 // We want to run the unit tests in sequence on the build agent. 
 #if UBIGIA_IS_RUNNING_ON_BUILD_AGENT
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
 #else
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = false)] // CollectionPerClass
 #endif
