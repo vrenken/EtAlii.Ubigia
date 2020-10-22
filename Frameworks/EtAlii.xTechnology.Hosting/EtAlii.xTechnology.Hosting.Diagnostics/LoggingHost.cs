c74abcc7118eb04dee820c8faa34febe6f7051fd
Peter Vrenken <vrenken@live.nl>
2020-10-22 23:57:57 +0200

    Test with LoggingHost.

---
+++ Frameworks/EtAlii.xTechnology.Hosting/EtAlii.xTechnology.Hosting.Diagnostics/LoggingHost.cs
@@ -0,0 +1,72 @@
+namespace EtAlii.xTechnology.Hosting.Diagnostics
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Server.Kestrel.Core;
+    using Serilog;
+
+    public class LoggingHost : IConfigurableHost
+    {
+        private readonly IHost _decoree;
+        private readonly ILogger _logger = Log.ForContext<IHost>();
+
+        private readonly IConfigurableHost _configurableHost;
+
+        public LoggingHost(IHost decoree)
+        {
+            _decoree = decoree;
+            _decoree.PropertyChanged += (o, e) => PropertyChanged?.Invoke(this, e);
+
+            _configurableHost = (IConfigurableHost) decoree;
+            _configurableHost.ConfigureApplication += builder => ConfigureApplication?.Invoke(builder);
+            _configurableHost.ConfigureHost += builder => ConfigureHost?.Invoke(builder);
+            _configurableHost.ConfigureKestrel += options => ConfigureKestrel?.Invoke(options);
+        }
+
+        public State State => _decoree.State;
+        public Status[] Status => _decoree.Status;
+        public ICommand[] Commands => _decoree.Commands;
+        public ISystem[] Systems => _decoree.Systems;
+
+        public IHostManager Manager => _configurableHost.Manager;
+        public event Action<IApplicationBuilder> ConfigureApplication;
+        public event Action<IWebHostBuilder> ConfigureHost;
+        public event Action<KestrelServerOptions> ConfigureKestrel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public IHostConfiguration Configuration => _decoree.Configuration;
+
+        public async Task Start()
+        {
+            _logger.Information("Starting host {HostName}", GetType().Name);
+            await _decoree.Start();   
+        }
+
+        public async Task Stop()
+        {
+            _logger.Information("Stopping host {HostName}", GetType().Name);
+            await _decoree.Stop();   
+        }
+
+        public async Task Shutdown()
+        {
+            _logger.Information("Shutting down host {HostName}", GetType().Name);
+            await _decoree.Shutdown();
+        }
+
+        public void Setup(ICommand[] commands, Status[] status)
+        {
+            _logger.Information("Setting up host {HostName}", GetType().Name);
+            _decoree.Setup(commands, status);
+        }
+
+        public void Initialize()
+        {
+            _logger.Information("Initializing host {HostName}", GetType().Name);
+            _decoree.Initialize();
+        }
+    }
+}
