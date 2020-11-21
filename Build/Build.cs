9516413df2f475a9137257f3ad191bc083c86ba8
Peter Vrenken <peter.vrenken@atos.net>
2020-11-21 16:27:11 +0100

    First attempt to introduce the Nuke build system.

---
+++ Build/Build.cs
@@ -0,0 +1,81 @@
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+// We need to give Nuke a bit more time to do it's magic.
+// More info: https://github.com/nuke-build/nuke/issues/260
+[CheckBuildProjectConfigurations(TimeoutInMilliseconds = 2000)] 
+[ShutdownDotNetAfterServerBuild]
+[AzurePipelines(
+    AzurePipelinesImage.WindowsLatest,
+    InvokedTargets = new []
+        {nameof(Test), 
+        nameof(Pack)},
+    NonEntryTargets = new []
+    {
+        nameof(Restore), 
+        nameof(Compile), 
+        nameof(Publish),
+    },
+    TriggerPathsInclude = new []
+    {
+        "master", 
+        "feature/*", 
+        "release/*"
+    }
+)]
+public partial class Build : NukeBuild
+{
+    /// To run nuke locally install it as a global tool:
+    /// "dotnet tool install Nuke.GlobalTool --global"
+    /// 
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Pack);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    AbsolutePath SourceDirectory => RootDirectory / "source";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+}
