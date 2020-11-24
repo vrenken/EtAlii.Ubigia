1328a9c10a44bca2cb9a65d2050f13656e4c994a
Peter Vrenken <peter.vrenken@atos.net>
2020-11-24 20:59:33 +0100

    Added two new tests.

---
+++ Source/Client/Windows/Shared/EtAlii.Ubigia.Windows.Management.Tests/UnitTests/ConnectionDialogViewModel.Tests.cs
@@ -0,0 +1,23 @@
+namespace EtAlii.Ubigia.Windows.Management.Tests
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ConnectionDialogViewModelTests
+    {
+        [Fact]
+        public async Task ConnectionDialogViewModel_Create()
+        {
+            await StaHelper.StartStaTask(() =>
+            {
+                // Arrange.
+
+                // Act.
+                var viewModel = new ConnectionDialogViewModel(new ConnectionDialogWindow(), "http://nohost", "nouser", "nopassword");
+
+                // Assert.
+                Assert.NotNull(viewModel);
+            });
+        }
+    }
+}
---
+++ Source/Client/Windows/Shared/EtAlii.Ubigia.Windows.Management.Tests/UnitTests/ConnectionSettingsPersister.Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace EtAlii.Ubigia.Windows.Management.Tests
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ConnectionSettingsPersisterTests
+    {
+        [Fact]
+        public async Task ConnectionSettingsPersister_Create()
+        {
+            await StaHelper.StartStaTask(() =>
+            {
+                // Arrange.
+                var viewModel = new ConnectionDialogViewModel(new ConnectionDialogWindow(), "http://nohost", "nouser", "nopassword");
+
+                // Act.
+                var connectionSettingsPersister = new ConnectionSettingsPersister(viewModel);
+
+                // Assert.
+                Assert.NotNull(connectionSettingsPersister);
+            });
+        }
+    }
+}
