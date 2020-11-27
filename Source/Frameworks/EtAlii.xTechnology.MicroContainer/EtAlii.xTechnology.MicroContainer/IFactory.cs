c4fdf846b37011e09243589949771737df8c2662
Peter Vrenken <peter.vrenken@atos.net>
2020-11-27 10:28:50 +0100

    Added comments to the Container.

--- Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/IFactory.cs
+++ Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/IFactory.cs
@@ -1,7 +1,15 @@
 ﻿namespace EtAlii.xTechnology.MicroContainer
 {
+    /// <summary>
+    /// A generic factory definition useful when building complex DI structures. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IFactory<out T>
 	{
+        /// <summary>
+        /// Create a new instance using the internal container registrations. 
+        /// </summary>
+        /// <returns></returns>
         T Create();
     }
 }
--- Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/IScaffolding.cs
+++ Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/IScaffolding.cs
@@ -1,7 +1,16 @@
 ﻿namespace EtAlii.xTechnology.MicroContainer
 {
+    /// <summary>
+    /// This interface is useful to make consistent groups of container registrations,
+    /// which essentially are the 'scaffolding' on which an application gets instantiated.
+    /// </summary>
+
     public interface IScaffolding
     {
+        /// <summary>
+        /// Run the scaffolding and corresponding container register calls it contains.
+        /// </summary>
+        /// <param name="container"></param>
         void Register(Container container);
     }
 }
