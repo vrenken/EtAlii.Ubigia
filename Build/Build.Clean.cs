368baf0b9692ebbfeae961edcdc12239bd8d26b5
Peter Vrenken <peter.vrenken@atos.net>
2020-11-30 21:21:14 +0100

    Removed debug log entries.

--- Build/Build.Clean.cs
+++ Build/Build.Clean.cs
@@ -8,25 +8,20 @@ namespace EtAlii.Ubigia.Pipelines
     public partial class Build
     {
             
-        Target Clean => _ => _
+        public Target Clean => _ => _
             .Description("Clean output")
             .Unlisted()
             .Executes(CleanInternal);
 
         private void CleanInternal()
         {
-            Logger.Info($"==== SonarQubeProjectKey:  {SonarQubeProjectKey ?? "NULL"}");
-            Logger.Info($"==== SonarQubeServerToken: {SonarQubeServerToken ?? "NULL"}");
-            Logger.Info($"==== SonarQubeServerUrl:   {SonarQubeServerUrl ?? "NULL"}");
-            Logger.Info($"==== NuGetFeedToken:       {NuGetFeedToken ?? "NULL"}");
-            Logger.Info($"==== NuGetFeedUrl:         {NuGetFeedUrl ?? "NULL"}");
             SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             EnsureCleanDirectory(ArtifactsDirectory);
         }
         
 // ============= Test Targets 
         
-        Target RunClean => _ => _
+        public Target RunClean => _ => _
             .Description("Clean output")
             .Executes(CleanInternal);
     }
