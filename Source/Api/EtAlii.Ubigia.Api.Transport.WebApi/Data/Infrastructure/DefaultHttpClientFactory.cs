df49c6a47b256961e69d104e5784f821b7de929c
Peter Vrenken <peter.vrenken@atos.net>
2020-12-04 12:15:31 +0100

    Removed warning about instance not getting disposed adequately.
    Remark: Also has a change in the previous commit.

--- Source/Api/EtAlii.Ubigia.Api.Transport.WebApi/Data/Infrastructure/DefaultHttpClientFactory.cs
+++ Source/Api/EtAlii.Ubigia.Api.Transport.WebApi/Data/Infrastructure/DefaultHttpClientFactory.cs
@@ -8,7 +8,9 @@
     {
         public HttpClient Create(ICredentials credentials, string hostIdentifier, string authenticationToken)
         {
-            var client = new HttpClient(new ClientHttpMessageHandler(credentials, hostIdentifier, authenticationToken));
+#pragma warning disable CA2000 // The HttpClient is instructed to dispose the handler.            
+            var client = new HttpClient(new ClientHttpMessageHandler(credentials, hostIdentifier, authenticationToken), true);
+#pragma warning restore CA2000
 
 			// Set the Accept header for BSON.
 			client.DefaultRequestHeaders.Accept.Clear();
