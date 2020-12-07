533d01bfb387a186a11127ce5bc1517650d931f0
Peter Vrenken <peter.vrenken@atos.net>
2020-12-07 13:36:29 +0100

    Disabled CA1724

--- Source/Api/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/0. Base/_Model/Schema.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Querying.GraphTL/0. Base/_Model/Schema.cs
@@ -3,7 +3,9 @@
     /// <summary>
     /// The Query class contains all information needed to execute graph traversal actions on the current infrastructureClient.
     /// </summary>
+#pragma warning disable CA1724// This class really has a purpose.
     public class Schema
+#pragma warning restore CA1724
     {
         public StructureFragment Structure { get; }
 
