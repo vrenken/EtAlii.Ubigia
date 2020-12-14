bc180f81ef77e565c6169fd345ad1c89fd07e229
Peter Vrenken <peter.vrenken@atos.net>
2020-12-14 15:36:41 +0100

    Removed old files.

--- Source/Build-Solution.ps1
+++
@@ -1,1 +0,0 @@
-﻿dir "*.Tests.csproj" -Recurse | %{dotnet test $PSItem.FullName /p:CollectCoverage=true /p:SolutionDir='c:\Git\EtAlii\EtAlii.Ubigia\' --no-restore --no-build --collect:"XPlat Code Coverage" -nodeReuse:false -maxCpuCount:8 -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover} 
--- Source/Clean-Output.ps1
+++
@@ -1,4 +0,0 @@
-﻿<# 
- https://sachabarbs.wordpress.com/2014/10/24/powershell-to-clean-visual-studio-binobj-folders/
-#>
-Get-ChildItem -inc bin,obj -rec | Remove-Item -rec -force
