2c8689c87b1826187391cd296360fb6099c0a121
Peter Vrenken <peter.vrenken@atos.net>
2020-12-17 21:52:28 +0100

    Added CompositeDisposable.cs

---
+++ Source/Frameworks/EtAlii.xTechnology.Threading/EtAlii.xTechnology.Threading/CompositeDisposable.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license in https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.xTechnology.Threading
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class CompositeDisposable : IDisposable
+    {
+        private readonly IEnumerable<IDisposable> _disposables;
+        public CompositeDisposable(IEnumerable<IDisposable> disposables)
+        {
+            _disposables = disposables;
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (var disposable in _disposables)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CompositeDisposable()
+        {
+            Dispose(false);
+        }
+    }
+}
