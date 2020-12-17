26060a6e638df1354d1748ba1b616c77b8087384
Peter Vrenken <peter.vrenken@atos.net>
2020-12-17 14:21:08 +0100

    Added ShortGuid and Correlation collection.

---
+++ Source/Api/EtAlii.Ubigia/Data/ShortGuid.cs
@@ -0,0 +1,16 @@
+﻿namespace EtAlii.Ubigia
+{
+    using System;
+
+    /// <summary>
+    /// A guid can be made more compact when the full alphabet is used.
+    /// </summary>
+    public static class ShortGuid
+    {
+
+        public static string New()
+        {
+            return Base36Convert.ToString(Guid.NewGuid()).ToUpper();
+        }
+    }
+}
---
+++ Source/Api/EtAlii.Ubigia/Scoping/Correlation.cs
@@ -0,0 +1,12 @@
+﻿namespace EtAlii.Ubigia
+{
+    /// <summary>
+    /// This class provides a consistent list of all correlation ID's used all over the Ubigia code base.
+    /// </summary>
+    public static class Correlation
+    {
+        public const string ScriptId = "ScriptCorrelationId";
+
+        public static string[] AllIds = {ScriptId};
+    }
+}
