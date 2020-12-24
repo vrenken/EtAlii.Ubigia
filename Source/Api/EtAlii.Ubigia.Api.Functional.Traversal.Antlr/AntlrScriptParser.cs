846cdac67505126586dba7755283efc4c86b2e04
Peter Vrenken <peter.vrenken@atos.net>
2020-12-24 13:21:44 +0100

    Operational helloworld Antlr parser.
    Up next: Implementing a GTL parser.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/AntlrScriptParser.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/AntlrScriptParser.cs
@@ -1,5 +1,8 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
+    using System.Collections.Generic;
+    using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
+
     /// <summary>
     /// The interface that abstracts away any GTL specific parser implementation.
     /// </summary>
@@ -15,4 +18,22 @@
             return null;
         }
     }
+
+    public class GtlVisitor : GtlBaseVisitor<object>
+    {
+        public readonly List<GtlLine> Lines = new();
+
+        public override object VisitLine(GtlParser.LineContext context)
+        {
+            var name = context.name();
+            var opinion = context.opinion();
+
+            var line = new GtlLine(name.GetText(), opinion.GetText().Trim('"'));
+            Lines.Add(line);
+
+            return line;
+        }
+    }
+
+    public record GtlLine(string Person, string Text);
 }
---
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Tests/1. Parsing/UnitTests/Antlr/AntlrParser.Tests.cs
@@ -0,0 +1,66 @@
+﻿using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
+
+namespace EtAlii.Ubigia.Api.Functional.Traversal.Tests
+{
+    using Xunit;
+    using Antlr4.Runtime;
+
+    public class AntlrParserTests
+    {
+        private GtlParser Setup(string text)
+        {
+            var inputStream = new AntlrInputStream(text);
+            var gtlLexer = new GtlLexer(inputStream);
+            var commonTokenStream = new CommonTokenStream(gtlLexer);
+            var gtlParser = new GtlParser(commonTokenStream);
+            return gtlParser;
+        }
+        [Fact]
+        public void GtlParser_Chat()
+        {
+            // Arrange.
+            var parser = Setup("john says \"hello\" n michael says \"world\" \n");
+
+            // Act.
+            var context = parser.chat();
+            var visitor = new GtlVisitor();
+            visitor.Visit(context);
+
+            // Assert.
+            Assert.Equal(2, visitor.Lines.Count);
+        }
+
+        [Fact]
+        public void GtlParser_Line()
+        {
+            // Arrange.
+            var parser = Setup("john says \"hello\" \n");
+
+            // Act.
+            var context = parser.line();
+            var visitor = new GtlVisitor();
+            var line = (GtlLine) visitor.VisitLine(context);
+
+            // Assert.
+            Assert.NotNull(line);
+            Assert.Equal("john", line.Person);
+            Assert.Equal("hello", line.Text);
+        }
+
+        [Fact]
+        public void GtlParser_WrongLine()
+        {
+            // Arrange.
+            var parser = Setup("john sayan \"hello\" \n");
+
+            // Act.
+            var context = parser.line();
+
+            // Assert.
+            Assert.IsType<GtlParser.LineContext>(context);
+            Assert.Equal("john", context.name().GetText());
+            Assert.Null(context.SAYS());
+            Assert.Equal("johnsayan\"hello\"", context.GetText());
+        }
+    }
+}
