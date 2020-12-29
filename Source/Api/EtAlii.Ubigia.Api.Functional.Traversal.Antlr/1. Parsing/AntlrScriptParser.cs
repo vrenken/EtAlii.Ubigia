c203fe3846d9e84b3a4e5bc5f1a13c4b36cdecb0
Peter Vrenken <peter.vrenken@atos.net>
2020-12-29 18:06:25 +0100

    Added basic AntlrScriptParser.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/1. Parsing/AntlrScriptParser.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/1. Parsing/AntlrScriptParser.cs
@@ -1,5 +1,9 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
+    using System;
+    using Antlr4.Runtime;
+    using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
+
     /// <summary>
     /// The interface that abstracts away any GTL specific parser implementation.
     /// </summary>
@@ -7,13 +11,36 @@
     {
         public ScriptParseResult Parse(string text)
         {
-            return null;
+            text ??= string.Empty;
+
+            // Newlines and tabs are nasty. Correct them (newlines) or get rid of them (tabs).
+            //text = text.Replace("\r\n", "\n");
+            //text = text.Replace("\t", " ");
+
+            var errors = Array.Empty<ScriptParserError>();
+            Script script = null;
+
+            try
+            {
+                var inputStream = new AntlrInputStream(text);
+                var gtlLexer = new GtlLexer(inputStream);
+                var commonTokenStream = new CommonTokenStream(gtlLexer);
+                var parser = new GtlParser(commonTokenStream);
+
+                // Act.
+                var context = parser.script();
+                var visitor = new GtlVisitor();
+                script = visitor.Visit(context) as Script;
+            }
+            catch (Exception e)
+            {
+                errors = new[] { new ScriptParserError(e, e.Message, 0, 0) };
+            }
+
+            return new ScriptParseResult(string.Join(Environment.NewLine, text), script, errors);
         }
 
-        public ScriptParseResult Parse(string[] text)
-        {
-            return null;
-        }
+        public ScriptParseResult Parse(string[] text) =>  Parse(string.Join("\n", text));
     }
 
     public record GtlLine(string Person, string Text);
