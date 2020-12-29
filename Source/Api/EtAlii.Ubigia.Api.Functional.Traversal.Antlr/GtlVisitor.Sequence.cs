807cc3d6f1f54232f851240ae851895462830f9b
Peter Vrenken <peter.vrenken@atos.net>
2020-12-29 18:04:35 +0100

    Added logic for AssignOperator addition.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
@@ -3,43 +3,52 @@
 namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
     using System.Collections.Generic;
+    using System.Linq;
     using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
 
     public partial class GtlVisitor
     {
         public override object VisitSequence(GtlParser.SequenceContext context)
         {
-            var sequenceParts = new List<SequencePart>();
+            var parts = new List<SequencePart>();
 
             var leftSubjectContext = context.subject(0);
             if (leftSubjectContext != null)
             {
                 var leftSubject = (SequencePart)Visit(leftSubjectContext);
-                sequenceParts.Add(leftSubject);
+                parts.Add(leftSubject);
             }
 
             var operatorContext = context.OPERATOR();
             if (operatorContext != null)
             {
                 var @operator = (SequencePart)Visit(operatorContext);
-                sequenceParts.Add(@operator);
+                parts.Add(@operator);
             }
 
             var rightSubjectContext = context.subject(1);
             if (rightSubjectContext != null)
             {
                 var rightSubject = (SequencePart)Visit(rightSubjectContext);
-                sequenceParts.Add(rightSubject);
+                parts.Add(rightSubject);
             }
 
             var commentContext = context.comment();
             if (commentContext != null)
             {
                 var comment = (SequencePart)Visit(commentContext);
-                sequenceParts.Add(comment);
+                parts.Add(comment);
             }
 
-            return new Sequence(sequenceParts.ToArray());
+            // if the first part of the sequence is a subject we add an additional assignment operator [<=] to output the result.
+            //if [parts.First[] is Subject]
+            if (!parts.Any(p => p is Operator) &&
+                !(parts.Count == 1 && parts.First() is Comment))
+            {
+                parts.Insert(0, new AssignOperator());
+            }
+
+            return new Sequence(parts.ToArray());
         }
 
         public override object VisitComment(GtlParser.CommentContext context)
