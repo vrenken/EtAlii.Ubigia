0f2b8c7a5563001d5b0205b841fef194d4c354a8
Peter Vrenken <peter.vrenken@atos.net>
2020-12-29 02:32:14 +0100

    New wave of GTL lexing and parsing

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
@@ -2,30 +2,50 @@
 
 namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
+    using System.Collections.Generic;
     using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
 
     public partial class GtlVisitor
     {
-
-        public override object VisitOperandOnlySequence(GtlParser.OperandOnlySequenceContext context)
+        public override object VisitSequence(GtlParser.SequenceContext context)
         {
-            var operandContext = context.operand();
-            var subject = VisitOperand(operandContext) as Subject;
-            return new Sequence(new SequencePart[]{ subject });
+            var sequenceParts = new List<SequencePart>();
+
+            var leftOperandContext = context.operand(0);
+            if (leftOperandContext != null)
+            {
+                var leftOperand = (SequencePart)Visit(leftOperandContext);
+                sequenceParts.Add(leftOperand);
+            }
+
+            var operatorContext = context.OPERATOR();
+            if (operatorContext != null)
+            {
+                var @operator = (SequencePart)Visit(operatorContext);
+                sequenceParts.Add(@operator);
+            }
+
+            var rightOperandContext = context.operand(1);
+            if (rightOperandContext != null)
+            {
+                var rightOperand = (SequencePart)Visit(rightOperandContext);
+                sequenceParts.Add(rightOperand);
+            }
+
+            var commentContext = context.comment();
+            if (commentContext != null)
+            {
+                var comment = (SequencePart)Visit(commentContext);
+                sequenceParts.Add(comment);
+            }
+
+            return new Sequence(sequenceParts.ToArray());
         }
 
-        public override object VisitOperatorOperandSequence(GtlParser.OperatorOperandSequenceContext context)
+        public override object VisitComment(GtlParser.CommentContext context)
         {
             var text = context.GetText().Substring(CommentPrefixLength);
-            var part = new Comment(text);
-            return new Sequence(new SequencePart[]{ part });
-        }
-
-        public override object VisitOperandOperatorOperandSequence(GtlParser.OperandOperatorOperandSequenceContext context)
-        {
-            var text = context.GetText().Substring(2);
-            var part = new Comment(text);
-            return new Sequence(new SequencePart[]{ part });
+            return new Comment(text);
         }
     }
 }
