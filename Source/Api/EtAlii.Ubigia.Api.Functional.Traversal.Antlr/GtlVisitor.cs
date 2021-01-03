42a7121c88c0864b9948dad86d40ffa30f680a64
Peter Vrenken <peter.vrenken@atos.net>
2021-01-03 23:54:24 +0100

    Added validation helpers.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.cs
@@ -3,6 +3,7 @@
 namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
     using System.Linq;
+    using Antlr4.Runtime.Tree;
     using EtAlii.Ubigia.Api.Functional.Traversal.Antlr;
 
     public partial class GtlVisitor : GtlParserBaseVisitor<object>
@@ -17,6 +18,49 @@ namespace EtAlii.Ubigia.Api.Functional.Traversal
                 .ToArray();
 
             return new Script(sequences);
+        }
+
+        private (IParseTree before, IParseTree after) GetSiblings(IParseTree current, IParseTree parent)
+        {
+            IParseTree previous = null;
+            var childCount = parent.ChildCount;
+            for (var i = 0; i < childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (child == current)
+                {
+                    var before = previous;
+                    var after
+                        = parent.GetChild(i + 1);
+                    return (before, after);
+                }
+
+                previous = child;
+            }
+
+            return (null, null);
+        }
+
+        private (IParseTree before, IParseTree after) GetSiblings(IParseTree current) => GetSiblings(current, current.Parent);
+
+        private (IParseTree before, IParseTree after) GetSiblings<TParent>(IParseTree current)
+            where TParent: IParseTree
+        {
+            var parent = GetParent<TParent>(current, out var childOfParent);
+            return GetSiblings(childOfParent, parent);
+        }
+
+        private IParseTree GetParent<TParent>(IParseTree current, out IParseTree parentOfChild)
+        {
+            var parent = current.Parent;
+            parentOfChild = current;
+            while (parent is not TParent && parent != null)
+            {
+                parentOfChild = parent;
+                parent = parent.Parent;
+            }
+
+            return parent;
         }
     }
 }
