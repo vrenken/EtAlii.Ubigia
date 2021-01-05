355fba4345c11c7306707f19333d1c87ac1c8209
Peter Vrenken <peter.vrenken@atos.net>
2021-01-05 01:33:56 +0100

    Improved validation

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal.Antlr/GtlVisitor.Sequence.cs
@@ -8,30 +8,110 @@ namespace EtAlii.Ubigia.Api.Functional.Traversal
 
     public partial class GtlVisitor
     {
-        public override object VisitSequence(GtlParser.SequenceContext context)
+        public override object VisitSequence_pattern_1(GtlParser.Sequence_pattern_1Context context)
         {
             var parts = new List<SequencePart>();
 
-            var leftSubjectContext = context.subject(0);
-            if (leftSubjectContext != null)
+            foreach (var subjectOperatorPair in context.subject_operator_pair())
             {
-                var leftSubject = (SequencePart)Visit(leftSubjectContext);
-                parts.Add(leftSubject);
-            }
-
-            var operatorContext = context.@operator();
-            if (operatorContext != null)
-            {
-                var @operator = (SequencePart)Visit(operatorContext);
+                var subject = (SequencePart)VisitSubject(subjectOperatorPair.subject());
+                parts.Add(subject);
+                var @operator = (SequencePart)VisitOperator(subjectOperatorPair.@operator());
                 parts.Add(@operator);
             }
 
-            var rightSubjectContext = context.subject(1);
-            if (rightSubjectContext != null)
+            var optionalSubjectContext = context.subject_optional();
+            if (optionalSubjectContext != null)
             {
-                var rightSubject = (SequencePart)Visit(rightSubjectContext);
+                var rightSubject = (SequencePart)Visit(optionalSubjectContext);
                 parts.Add(rightSubject);
             }
+
+            var commentContext = context.comment();
+            if (commentContext != null)
+            {
+                var comment = (SequencePart)Visit(commentContext);
+                parts.Add(comment);
+            }
+
+            // if the first part of the sequence is a subject we add an additional assignment operator [<=] to output the result.
+            // if [parts.First[] is Subject]
+            if (!parts.Any(p => p is Operator) &&
+                !(parts.Count == 1 && parts.First() is Comment))
+            {
+                parts.Insert(0, new AssignOperator());
+            }
+
+            return new Sequence(parts.ToArray());
+        }
+        public override object VisitSequence_pattern_2(GtlParser.Sequence_pattern_2Context context)
+        {
+            var parts = new List<SequencePart>();
+
+            foreach (var operatorSubjectPair in context.operator_subject_pair())
+            {
+                var @operator = (SequencePart)VisitOperator(operatorSubjectPair.@operator());
+                parts.Add(@operator);
+                var subject = (SequencePart)VisitSubject(operatorSubjectPair.subject());
+                parts.Add(subject);
+            }
+
+            var optionalOperator = context.operator_optional();
+            if (optionalOperator != null)
+            {
+                var @operator = (SequencePart)Visit(optionalOperator);
+                parts.Add(@operator);
+            }
+
+            var commentContext = context.comment();
+            if (commentContext != null)
+            {
+                var comment = (SequencePart)Visit(commentContext);
+                parts.Add(comment);
+            }
+
+            // if the first part of the sequence is a subject we add an additional assignment operator [<=] to output the result.
+            //if [parts.First[] is Subject]
+            if (!parts.Any(p => p is Operator) &&
+                !(parts.Count == 1 && parts.First() is Comment))
+            {
+                parts.Insert(0, new AssignOperator());
+            }
+
+            return new Sequence(parts.ToArray());
+        }
+        public override object VisitSequence_pattern_3(GtlParser.Sequence_pattern_3Context context)
+        {
+            var parts = new List<SequencePart>();
+
+            var subject = context.subject();
+            if (subject != null)
+            {
+                var rightSubject = (SequencePart)Visit(subject);
+                parts.Add(rightSubject);
+            }
+
+            var commentContext = context.comment();
+            if (commentContext != null)
+            {
+                var comment = (SequencePart)Visit(commentContext);
+                parts.Add(comment);
+            }
+
+            // if the first part of the sequence is a subject we add an additional assignment operator [<=] to output the result.
+            //if [parts.First[] is Subject]
+            if (!parts.Any(p => p is Operator) &&
+                !(parts.Count == 1 && parts.First() is Comment))
+            {
+                parts.Insert(0, new AssignOperator());
+            }
+
+            return new Sequence(parts.ToArray());
+        }
+
+        public override object VisitSequence_pattern_4(GtlParser.Sequence_pattern_4Context context)
+        {
+            var parts = new List<SequencePart>();
 
             var commentContext = context.comment();
             if (commentContext != null)
