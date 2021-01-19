f8c1ba9edd7ef67a87c1a0715921ee375b8005d4
Peter Vrenken <peter.vrenken@atos.net>
2021-01-19 21:26:54 +0100

    Wowza, code generators are easy to start with.

---
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Context/4. CodeGeneration/ISchemaProcessor.ProcessTyped.cs
@@ -0,0 +1,14 @@
+﻿namespace EtAlii.Ubigia.Api.Functional.Context
+{
+    using System.Threading.Tasks;
+
+    public static class SchemaProcessorProcessTypedExtension
+    {
+        public static Task<SchemaProcessingResult<TResult>> Process<TResult>(Schema schema)
+        {
+            var result = new SchemaProcessingResult<TResult>(null, 0, null);
+
+            return Task.FromResult(result);
+        }
+    }
+}
