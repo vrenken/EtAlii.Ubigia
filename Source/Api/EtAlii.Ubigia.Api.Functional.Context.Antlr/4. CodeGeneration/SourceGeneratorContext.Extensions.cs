a1d881340e9994f152730f9a8b42ac7afa38a0ea
Peter Vrenken <peter.vrenken@atos.net>
2021-02-03 20:30:03 +0100

    Move to new Roslyn based code generation - current version is still ugly though.

---
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Context.Antlr/4. CodeGeneration/SourceGeneratorContext.Extensions.cs
@@ -0,0 +1,31 @@
+﻿namespace EtAlii.Ubigia.Api.Functional.Context
+{
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+
+    internal static class SourceGeneratorContextExtensions
+    {
+        private const string SourceItemGroupMetadata = "build_metadata.AdditionalFiles.SourceItemGroup";
+
+        public static string GetMSBuildProperty(
+            this GeneratorExecutionContext context,
+            string name,
+            string defaultValue = "")
+        {
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{name}", out var value);
+            return value ?? defaultValue;
+        }
+
+        public static string[] GetMSBuildItems(this GeneratorExecutionContext context, string name)
+        {
+            return context
+                .AdditionalFiles
+                .Where(f => context.AnalyzerConfigOptions
+                                .GetOptions(f)
+                                .TryGetValue(SourceItemGroupMetadata, out var sourceItemGroup)
+                            && sourceItemGroup == name)
+                .Select(f => f.Path)
+                .ToArray();
+        }
+    }
+}
