97b8f0678fe7f5e4570c7ac933b0556219194ae6
Peter Vrenken <peter.vrenken@atos.net>
2021-02-23 22:38:48 +0100

    Added simple placeholder for current node/value processing.

---
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Context/0. _Model/Annotations/Nodes/Select/SelectCurrentNodeAnnotation.cs
@@ -0,0 +1,14 @@
+﻿namespace EtAlii.Ubigia.Api.Functional.Context
+{
+    public class SelectCurrentNodeAnnotation : NodeAnnotation
+    {
+        public SelectCurrentNodeAnnotation() : base(null)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"@{AnnotationPrefix.Node}()";
+        }
+    }
+}
