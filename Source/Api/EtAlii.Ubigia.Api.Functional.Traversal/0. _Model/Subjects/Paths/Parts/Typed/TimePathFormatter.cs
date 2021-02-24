74f958230ea0b2fa8885f0c79bda2c37738ea514
Peter Vrenken <peter.vrenken@atos.net>
2021-02-24 20:12:35 +0100

    Fixed yyyy format.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Traversal/0. _Model/Subjects/Paths/Parts/Typed/TimePathFormatter.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Traversal/0. _Model/Subjects/Paths/Parts/Typed/TimePathFormatter.cs
@@ -2,7 +2,7 @@ namespace EtAlii.Ubigia.Api.Functional.Traversal
 {
     public static class TimePathFormatter
     {
-        public static readonly TypedPathFormatter YearFormatter = new("YYYY", @"^[0123]?[0123456789]{1,3}$");
+        public static readonly TypedPathFormatter YearFormatter = new("yyyy", @"^[0123]?[0123456789]{1,3}$");
         public static readonly TypedPathFormatter MonthFormatter = new("MM", @"^[01]?[0123456789]$");
         public static readonly TypedPathFormatter DayFormatter = new("DD", @"^[0123]?[0123456789]$");
         public static readonly TypedPathFormatter HourFormatter = new("HH", @"^[012]?[0123456789]$");
