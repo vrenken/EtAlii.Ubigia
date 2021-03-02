fcbc363aa08b4a524870aaa6f4b7ac3f420aac00
Peter Vrenken <peter.vrenken@atos.net>
2021-03-03 00:31:25 +0100

    Code cleanups.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Context/2. Planning/_Model/ExecutionPlan.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Context/2. Planning/_Model/ExecutionPlan.cs
@@ -1,12 +1,9 @@
 ﻿namespace EtAlii.Ubigia.Api.Functional.Context
 {
-    using System;
     using System.Threading.Tasks;
 
     public abstract class ExecutionPlan
     {
-        public abstract Type OutputType { get; }
-
         internal ExecutionPlanResultSink ResultSink { get; private set; }
 
         internal abstract Task Execute(SchemaExecutionScope executionScope);
