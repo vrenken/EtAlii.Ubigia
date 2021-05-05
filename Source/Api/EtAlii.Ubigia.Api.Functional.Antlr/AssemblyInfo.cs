8c161420afb4a4430f58866afb0cbef514389810
Peter Vrenken <peter.vrenken@atos.net>
2021-05-05 23:20:05 +0200

    Added dotsettings details.

--- Source/Api/EtAlii.Ubigia.Api.Functional.Antlr/AssemblyInfo.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Antlr/AssemblyInfo.cs
@@ -3,3 +3,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Context.SignalR.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Context.Grpc.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Context.WebApi.Tests")]
+
+[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Traversal.Tests")]
