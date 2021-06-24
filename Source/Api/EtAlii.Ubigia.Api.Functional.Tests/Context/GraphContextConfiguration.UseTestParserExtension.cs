8e8c13c0affcccaecf52d809371edd1ceda6ebb8
Peter Vrenken <peter.vrenken@atos.net>
2021-06-24 19:45:11 +0200

    Added file headers to the Infrastructure projects

--- Source/Api/EtAlii.Ubigia.Api.Functional.Tests/Context/GraphContextConfiguration.UseTestParserExtension.cs
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Tests/Context/GraphContextConfiguration.UseTestParserExtension.cs
@@ -1,9 +1,5 @@
 ﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
 
-github.com/vrenken/EtAlii.Ubigia
-
-github.com/vrenken/EtAlii.Ubigia
-
 // ReSharper disable once CheckNamespace
 namespace EtAlii.Ubigia.Api.Functional.Context
 {
