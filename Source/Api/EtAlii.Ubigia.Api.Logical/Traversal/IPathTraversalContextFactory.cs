17df7112d026262eb9689ac4694d351bd58b1500
Peter Vrenken <peter.vrenken@atos.net>
2021-06-26 01:49:00 +0200

    Code cleanup

--- Source/Api/EtAlii.Ubigia.Api.Logical/Traversal/IPathTraversalContextFactory.cs
+++ Source/Api/EtAlii.Ubigia.Api.Logical/Traversal/IPathTraversalContextFactory.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
 
-    namespace EtAlii.Ubigia.Api.Logical
+namespace EtAlii.Ubigia.Api.Logical
 {
     public interface IPathTraversalContextFactory
     {
