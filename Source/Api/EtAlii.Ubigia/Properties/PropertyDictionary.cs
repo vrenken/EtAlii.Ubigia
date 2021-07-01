ef2976c28e4b58204f6769c1151e92ac24b40000
Peter Vrenken <peter.vrenken@atos.net>
2021-07-01 21:44:35 +0200

    Resolved TODO: Improved usage of deterministic string hash.

--- Source/Api/EtAlii.Ubigia/Properties/PropertyDictionary.cs
+++ Source/Api/EtAlii.Ubigia/Properties/PropertyDictionary.cs
@@ -122,26 +122,30 @@ namespace EtAlii.Ubigia
         {
             if (value is string valueAsString)
             {
-                return DetermineHashForString(valueAsString);
+                return GetDeterministicHashCodeFromString(valueAsString);
             }
             return value.GetHashCode();
         }
 
-        private int DetermineHashForString(string valueAsString)
+        // We need a deterministic hash for the int comparison as else our values will jump back and forth.
+        // https://andrewlock.net/why-is-string-gethashcode-different-each-time-i-run-my-program-in-net-core/
+        private int GetDeterministicHashCodeFromString(string str)
         {
-            // TODO: Investigate if this is sound.
-            var hashCode = int.MinValue;
-            var bytes = Encoding.Default.GetBytes(valueAsString);
-            foreach (var b in bytes)
+            unchecked
             {
-                var delta = (int.MaxValue - b) - hashCode;
-                if (delta <= 0)
+                var hash1 = (5381 << 16) + 5381;
+                var hash2 = hash1;
+
+                for (var i = 0; i < str.Length; i += 2)
                 {
-                    hashCode = int.MinValue + -delta;
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
                 }
-                hashCode += b;
+
+                return hash1 + (hash2 * 1566083941);
             }
-            return hashCode;
         }
 
         public bool Equals(IPropertyDictionary other)
