ddf3e07cb9a077c9100065cf51b584b58b99f5d1
Peter Vrenken <peter.vrenken@atos.net>
2021-07-07 23:24:20 +0200

    Corrected folder name.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Functional/Properties/IPropertiesRepository.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Functional
+{
+    public interface IPropertiesRepository
+    {
+        void Store(in Identifier identifier, PropertyDictionary properties);
+        PropertyDictionary Get(in Identifier identifier);
+    }
+}
