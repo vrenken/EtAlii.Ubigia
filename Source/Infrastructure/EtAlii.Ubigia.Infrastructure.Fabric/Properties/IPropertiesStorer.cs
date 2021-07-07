48f9e094d1581a869d9cf7b4b84754ab8e3b4fe8
Peter Vrenken <peter.vrenken@atos.net>
2021-07-07 23:26:02 +0200

    Corrected folder name.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Fabric/Properties/IPropertiesStorer.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Fabric
+{
+    public interface IPropertiesStorer
+    {
+        void Store(in Identifier identifier, PropertyDictionary properties);
+    }
+}
---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Fabric/Properties/PropertiesGetter.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Fabric
+{
+    using EtAlii.Ubigia.Persistence;
+
+    internal class PropertiesGetter : IPropertiesGetter
+    {
+        private readonly IStorage _storage;
+
+        public PropertiesGetter(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public PropertyDictionary Get(in Identifier identifier)
+        {
+            var containerId = _storage.ContainerProvider.FromIdentifier(identifier);
+            var properties = _storage.Properties.Retrieve(containerId);
+            return properties;
+        }
+    }
+}
