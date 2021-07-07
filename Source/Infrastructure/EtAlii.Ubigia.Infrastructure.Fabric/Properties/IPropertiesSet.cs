81fd227ebd0fe2cc73f2c57efe645c2b3fba0c56
Peter Vrenken <peter.vrenken@atos.net>
2021-07-07 23:26:02 +0200

    Corrected folder name.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Fabric/Properties/IPropertiesSet.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Fabric
+{
+    public interface IPropertiesSet
+    {
+        PropertyDictionary Get(in Identifier identifier);
+        void Store(in Identifier identifier, PropertyDictionary properties);
+    }
+}
