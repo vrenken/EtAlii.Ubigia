6d578e437b58bfd5c1eddbb4b2658036b5f833db
Peter Vrenken <peter.vrenken@atos.net>
2021-07-18 18:21:26 +0200

    Small nudge to the 1.4.0 branch.

--- Source/Api/EtAlii.Ubigia.Diagnostics/AssemblyInfo.cs
+++ Source/Api/EtAlii.Ubigia.Diagnostics/AssemblyInfo.cs
@@ -2,12 +2,10 @@
 
 using System.Runtime.CompilerServices;
 
-//[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Management")]
 
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Diagnostics.Tests")]
-//[assembly: InternalsVisibleTo("EtAlii.Ubigia.Api.Functional.Win32.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Persistence.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Persistence.InMemory.Tests")]
 [assembly: InternalsVisibleTo("EtAlii.Ubigia.Persistence.Azure.Tests")]
