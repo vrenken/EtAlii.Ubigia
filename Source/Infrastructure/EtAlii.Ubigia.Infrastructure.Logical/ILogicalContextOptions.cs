c538f84112f85178070879fda9e044f7fd29802e
Peter Vrenken <peter.vrenken@atos.net>
2021-09-11 17:27:15 +0200

    Refactored Fabric layer even further.

--- Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Logical/ILogicalContextOptions.cs
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Logical/ILogicalContextOptions.cs
@@ -6,26 +6,15 @@ namespace EtAlii.Ubigia.Infrastructure.Logical
 	using EtAlii.Ubigia.Infrastructure.Fabric;
     using Microsoft.Extensions.Configuration;
 
+    // TODO: Remove this interface.
     public interface ILogicalContextOptions
     {
-        /// <summary>
-        /// The host configuration root that will be used to configure the logical context.
-        /// </summary>
         IConfigurationRoot ConfigurationRoot { get; }
 
-	    /// <summary>
-	    /// The name of the Ubigia storage.
-	    /// </summary>
         string Name { get; }
 
-        /// <summary>
-        /// The address (schema+host) at which the storage can be found.
-        /// </summary>
 	    Uri StorageAddress { get; }
 
-        /// <summary>
-        /// The fabric that should be used by the logical context.
-        /// </summary>
         IFabricContext Fabric { get; }
 
         LogicalContextOptions Use(string name, Uri storageAddress);
