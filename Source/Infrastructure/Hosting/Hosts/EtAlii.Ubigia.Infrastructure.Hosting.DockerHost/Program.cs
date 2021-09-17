2885e234839fda8de84bdc65e312c354b10ce3aa
Peter Vrenken <peter.vrenken@atos.net>
2021-09-17 22:01:14 +0200

    New attempt to make docker image creation operational.

---
+++ Source/Infrastructure/Hosting/Hosts/EtAlii.Ubigia.Infrastructure.Hosting.DockerHost/Program.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Hosting.DockerHost
+{
+    using System.Threading.Tasks;
+    using EtAlii.xTechnology.Hosting;
+    using EtAlii.xTechnology.Hosting.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static async Task Main()
+        {
+            var details = await new ConfigurationDetailsParser()
+                .Parse("settings.json")
+                .ConfigureAwait(false);
+
+            var configurationRoot = new ConfigurationBuilder()
+                .AddConfigurationDetails(details)
+                .Build();
+
+            var hostOptions = new HostOptionsBuilder()
+                .Build(configurationRoot, details)
+                .UseConsoleHost()
+                .UseHostDiagnostics();
+
+            ConsoleHost.Start(hostOptions);
+        }
+    }
+}
