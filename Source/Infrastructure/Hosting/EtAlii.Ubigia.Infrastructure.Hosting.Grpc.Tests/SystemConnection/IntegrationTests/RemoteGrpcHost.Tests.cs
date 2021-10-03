619b756d011dd22d5b7814d1a0995ad5d542b7de
Peter Vrenken <peter.vrenken@atos.net>
2021-10-03 16:38:11 +0200

    Added simple test to see if the Grpc services are still accessible.

---
+++ Source/Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting.Grpc.Tests/SystemConnection/IntegrationTests/RemoteGrpcHost.Tests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Hosting.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Transport.Management;
+    using EtAlii.Ubigia.Api.Transport.Management.Grpc;
+    using EtAlii.xTechnology.Threading;
+    using Microsoft.Extensions.Configuration;
+    using Xunit;
+
+    public class RemoteGrpcHostTests
+    {
+        [Fact(Skip = "This test should not be run on the server, but can be used locally to see if the console/docker applications facilitate Grpc requests.")]
+        public async Task RemoteGrpcHost_Connect_Using_Admin_Transport()
+        {
+            // Arrange.
+            var correlator = new ContextCorrelator();
+            using var configurationRoot = new ConfigurationRoot(new List<IConfigurationProvider>());
+            var connectionOptions = new ManagementConnectionOptions(configurationRoot)
+                .Use(GrpcStorageTransportProvider.Create(correlator))
+                .Use("Administrator", "administrator123")
+                .Use(new Uri("https://localhost:64001"));
+            using var managementConnection = Factory.Create<IManagementConnection>(connectionOptions);
+            await managementConnection
+                .Open()
+                .ConfigureAwait(false);
+
+            // Act.
+            var accounts = await managementConnection.Accounts
+                .GetAll()
+                .ToArrayAsync()
+                .ConfigureAwait(false);
+
+            // Assert.
+            Assert.Equal(2, accounts.Length);
+        }
+    }
+}
