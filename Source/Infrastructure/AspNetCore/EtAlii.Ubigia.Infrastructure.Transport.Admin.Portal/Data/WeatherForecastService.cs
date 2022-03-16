6e910a8f503a57298b0ead2701303d4e7455dc20
Peter Vrenken <peter.vrenken@atos.net>
2022-03-17 00:29:06 +0100

    Removed Sonarqube security warning.

--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Data/WeatherForecastService.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Data/WeatherForecastService.cs
@@ -6,19 +6,18 @@ namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            var rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = 21,
+                Summary = Summaries[3]
             }).ToArray());
         }
     }
