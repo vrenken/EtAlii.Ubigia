8ddd8510ac21de53ed3e3d80941961298179a8ca
Peter Vrenken <peter.vrenken@atos.net>
2022-03-20 09:20:56 +0100

    Improvements on portal.

--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/DefaultLayout.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/DefaultLayout.cs
@@ -9,7 +9,7 @@ namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 
         public static IFluentColumnWithSize ItemColumnWidth => new FluentColumn().IsFull.OnMobile.IsHalf.OnTablet;
 
-        public static IFluentColumnWithSize ItemDashboardColumnWidth => new FluentColumn().IsFull.OnMobile.IsHalf.OnTablet.Is4.OnDesktop;
+        public static IFluentColumnWithSize ItemDashboardColumnWidth => new FluentColumn().IsFull.OnMobile.IsHalf.OnTablet.Is5.OnDesktop;
         public static IFluentColumnWithSize ItemOneOffColumnWidth => new FluentColumn().IsFull.OnMobile.Is4.OnTablet.Is3.OnDesktop;
     }
 }
--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/MainLayout.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/MainLayout.cs
@@ -2,6 +2,5 @@
 {
     public partial class MainLayout
     {
-        private bool _topbarVisible;
     }
 }
