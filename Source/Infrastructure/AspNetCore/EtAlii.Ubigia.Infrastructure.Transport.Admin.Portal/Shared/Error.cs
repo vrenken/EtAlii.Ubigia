4a032d47ddb8291468cc07765132d08713cd485a
Peter Vrenken <peter.vrenken@atos.net>
2022-03-20 09:53:32 +0100

    Disable Sonarqube

--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/Error.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/Error.cs
@@ -1,9 +1,14 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 {
+    using System.Diagnostics.CodeAnalysis;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [SuppressMessage(
+        category: "Sonar Code Smell",
+        checkId: "S4502:Disabling CSRF protections is security-sensitive",
+        Justification = "Default pattern as specified in the Asp.NET Core Razor/Blazor approach. So probably safe to do so here.")]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
