b163db95496b53c5a8cb55ae7abace61a7968ec4
Peter Vrenken <peter.vrenken@atos.net>
2022-03-21 11:29:02 +0100

    Added KestelConfigurator.Tests.

---
+++ Source/Frameworks/EtAlii.xTechnology.Hosting/Tests/EtAlii.xTechnology.Hosting.Tests.Local/KestrelConfigurator.Tests.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.xTechnology.Hosting.Tests.Local
+{
+    using System.Threading.Tasks;
+    using EtAlii.xTechnology.Diagnostics;
+    using EtAlii.xTechnology.Hosting.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Xunit;
+
+    public class KestrelConfiguratorTests
+    {
+        [Fact]
+        public async Task KestrelConfigurator_Configure()
+        {
+            // Arrange.
+            var details = await new ConfigurationDetailsParser()
+                .ParseForTesting(ConfigurationFiles.HostSettingsSystems2VariantGrpc, UnitTestSettings.NetworkPortRange)
+                .ConfigureAwait(false);
+
+            var configurationRoot = new ConfigurationBuilder()
+                .AddConfigurationDetails(details)
+                .AddConfiguration(DiagnosticsOptions.ConfigurationRoot) // For testing we'll override the configured logging et.
+                .Build();
+
+            // Arrange.
+            var host = Host
+                .CreateDefaultBuilder()
+                .UseHostLogging(configurationRoot, typeof(KestrelConfiguratorTests).Assembly)
+                .UseHostServices<System1HostServicesFactory>(configurationRoot)
+                .Build();
+
+            // Assert.
+            Assert.NotNull(host);
+        }
+    }
+}
