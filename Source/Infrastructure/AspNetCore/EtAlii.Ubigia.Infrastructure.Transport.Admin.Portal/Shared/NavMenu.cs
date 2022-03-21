91cafde9ee0d247879d29a23956119e7d1d58b5f
Peter Vrenken <peter.vrenken@atos.net>
2022-03-21 11:28:30 +0100

    Small cleanup

--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/NavMenu.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/Shared/NavMenu.cs
@@ -1,12 +1,5 @@
 ﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 {
-    using System;
-    using System.Collections.ObjectModel;
-    using System.Collections.Specialized;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Components;
-
     public partial class NavMenu
     {
         protected override void OnInitialized()
