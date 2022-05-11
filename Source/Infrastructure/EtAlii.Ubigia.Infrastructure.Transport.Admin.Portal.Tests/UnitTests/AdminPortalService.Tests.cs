3b4ac99ef027b1344b2caa4bbd3968157fd76fd4
Peter Vrenken <peter.vrenken@atos.net>
2022-05-12 00:27:56 +0200

    Added new admin portal unit tests.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.Tests/UnitTests/AdminPortalService.Tests.cs
@@ -0,0 +1,58 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal.Tests
+{
+    using System.Linq;
+    using EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal;
+    using EtAlii.xTechnology.Hosting;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.TestHost;
+    using Microsoft.Extensions.Hosting;
+    using Xunit;
+    using IServiceCollection = Microsoft.Extensions.DependencyInjection.IServiceCollection;
+
+    public class AdminPortalServiceTests
+    {
+
+        [Fact]
+        public void AdminPortalService_Create()
+        {
+            // Arrange.
+            var configuration = new ServiceConfiguration();
+
+            // Act.
+            var service = new AdminPortalService(configuration);
+
+            // Assert.
+            Assert.NotNull(service);
+        }
+
+        [Fact]
+        public void AdminPortalService_ConfigureServices()
+        {
+            // Arrange.
+            var configuration = new ServiceConfiguration();
+            var service = new AdminPortalService(configuration);
+
+            // Act.
+            var host = Host
+                .CreateDefaultBuilder()
+                .ConfigureWebHost(webHostBuilder =>
+                {
+                    webHostBuilder
+                        .UseTestServer()
+                        .Configure(builder => { builder.Isolate(_ => { }, services => { service.ConfigureServices(services, builder.ApplicationServices); }); });
+                })
+                .Build();
+
+            // Assert.
+            Assert.NotNull(host);
+        }
+
+        public static void ConfigureBackgroundServices(IService[] services, IServiceCollection serviceCollection)
+        {
+            foreach (var service in services.OfType<IBackgroundService>())
+            {
+                service.ConfigureServices(serviceCollection, services);
+            }
+        }
+    }
+}
