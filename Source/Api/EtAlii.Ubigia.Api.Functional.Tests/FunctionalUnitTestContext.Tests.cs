8de92e1a052f4c2b3d78b00580c4af0abf88e64f
Peter Vrenken <peter.vrenken@atos.net>
2022-05-12 22:26:47 +0200

    Added missing files.

---
+++ Source/Api/EtAlii.Ubigia.Api.Functional.Tests/FunctionalUnitTestContext.Tests.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Api.Functional.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Functional.Context;
+    using EtAlii.Ubigia.Api.Functional.Traversal;
+    using EtAlii.Ubigia.Tests;
+    using Xunit;
+
+    [CorrelateUnitTests]
+    public class FunctionalUnitTestContextTests
+    {
+        private async Task<FunctionalOptions> InitializeAsync(FunctionalUnitTestContext context)
+        {
+            var options = await new FunctionalOptions(context.ClientConfiguration)
+                .UseTestParsing()
+                .UseDiagnostics()
+                .UseLogicalContext(context, true)
+                .ConfigureAwait(false);
+
+            var (_, traversalContext) = context.CreateComponent<IGraphContext, ITraversalContext>(options);
+
+            var scope = new ExecutionScope();
+            await context.Functional
+                .AddPeople(traversalContext, scope)
+                .ConfigureAwait(false);
+            await context.Functional
+                .AddAddresses(traversalContext, scope)
+                .ConfigureAwait(false);
+
+            return options;
+        }
+
+        private async Task DisposeAsync(FunctionalOptions options)
+        {
+            await options.LogicalContext
+                .DisposeAsync()
+                .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task FunctionalUnitTestContext_Create_Empty()
+        {
+            // Arrange.
+            var context = new FunctionalUnitTestContext();
+            var start = DateTime.Now;
+
+            // Act.
+            try
+            {
+                await context.InitializeAsync().ConfigureAwait(false);
+
+            }
+            finally
+            {
+                await context.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var end = DateTime.Now;
+            var duration = end - start;
+
+            // Assert.
+            Assert.NotNull(context);
+            Assert.True(duration.TotalMinutes < 3);
+        }
+
+        [Fact(Skip = "Temporary disabled")]
+        public async Task FunctionalUnitTestContext_Create_Complete()
+        {
+            // Arrange.
+            var context = new FunctionalUnitTestContext();
+            var start = DateTime.Now;
+
+            // Act.
+            try
+            {
+                await context.InitializeAsync().ConfigureAwait(false);
+                var options = await InitializeAsync(context).ConfigureAwait(false);
+
+                await DisposeAsync(options).ConfigureAwait(false);
+            }
+            finally
+            {
+                await context.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var end = DateTime.Now;
+            var duration = end - start;
+
+            // Assert.
+            Assert.NotNull(context);
+            Assert.True(duration.TotalMinutes < 3);
+        }
+    }
+}
---
+++ Source/Api/EtAlii.Ubigia.Api.Logical.Tests/LogicalUnitTestContext.Tests.cs
@@ -0,0 +1,85 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Api.Logical.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Api.Logical.Diagnostics;
+    using Xunit;
+
+    public class LogicalUnitTestContextTests
+    {
+        private async Task<LogicalOptions> InitializeAsync(LogicalUnitTestContext testContext)
+        {
+            // Arrange.
+            var options = await testContext.Fabric
+                .CreateFabricOptions(true)
+                .UseLogicalContext()
+                .UseDiagnostics()
+                .ConfigureAwait(false);
+
+            return options;
+        }
+
+        private async Task DisposeAsync(LogicalOptions options)
+        {
+            await options.FabricContext.Options.Connection
+                .Close()
+                .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task LogicalUnitTestContext_Create_Empty()
+        {
+            // Arrange.
+            var context = new LogicalUnitTestContext();
+            var start = DateTime.Now;
+
+            // Act.
+            try
+            {
+                await context.InitializeAsync().ConfigureAwait(false);
+
+            }
+            finally
+            {
+                await context.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var end = DateTime.Now;
+            var duration = end - start;
+
+            // Assert.
+            Assert.NotNull(context);
+            Assert.True(duration.TotalMinutes < 3);
+        }
+
+        [Fact]
+        public async Task LogicalUnitTestContext_Create_Complete()
+        {
+            // Arrange.
+            var context = new LogicalUnitTestContext();
+            var start = DateTime.Now;
+
+            // Act.
+            try
+            {
+                await context.InitializeAsync().ConfigureAwait(false);
+                var options = await InitializeAsync(context).ConfigureAwait(false);
+
+                await DisposeAsync(options).ConfigureAwait(false);
+            }
+            finally
+            {
+                await context.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var end = DateTime.Now;
+            var duration = end - start;
+
+            // Assert.
+            Assert.NotNull(context);
+            Assert.True(duration.TotalMinutes < 3);
+        }
+    }
+}
