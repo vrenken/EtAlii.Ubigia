ec77a7d3a11967e7a01cf2f1e65e12a8b1411d41
Peter Vrenken <peter.vrenken@atos.net>
2022-05-19 12:23:10 +0200

    Removed unused code.

--- Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/WebHostEnvironment.UseStaticWebAssets.cs
+++ Source/Infrastructure/AspNetCore/EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal/WebHostEnvironment.UseStaticWebAssets.cs
@@ -3,15 +3,10 @@
 namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 {
     using System;
-    using System.IO;
-    using System.Reflection;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Hosting.StaticWebAssets;
-    using Microsoft.AspNetCore.StaticFiles;
     using Microsoft.Extensions.FileProviders;
     using Microsoft.Extensions.Options;
-    using Serilog;
 
     public class UIConfigureOptions : IPostConfigureOptions<StaticFileOptions>
     {
@@ -24,11 +19,14 @@ namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 
         public void PostConfigure(string name, StaticFileOptions options)
         {
-            name = name ?? throw new ArgumentNullException(nameof(name));
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
             options = options ?? throw new ArgumentNullException(nameof(options));
 
             // Basic initialization in case the options weren't initialized by any other component
-            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
+            //options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
             if (options.FileProvider == null && _environment.WebRootFileProvider == null)
             {
                 throw new InvalidOperationException("Missing FileProvider");
@@ -40,42 +38,6 @@ namespace EtAlii.Ubigia.Infrastructure.Transport.Admin.Portal
 
             var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, basePath);
             options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
-        }
-    }
-
-    internal static class WebHostEnvironmentUseStaticWebAssetsExtension
-    {
-        /// <summary>
-        ///     Serve static web assets provided by the module.
-        /// </summary>
-        public static IWebHostEnvironment UseStaticWebAssets<TModule>(this IWebHostEnvironment environment)
-        {
-            var assembly = typeof(TModule).Assembly;
-            var assemblyDirectory = Path.GetDirectoryName(assembly.Location)!;
-
-            var manifestPath = Path.Combine(assemblyDirectory!, $"{assembly.GetName().Name}.StaticWebAssets.runtime.json");
-
-            var log = Log.ForContext("SourceContext", nameof(WebHostEnvironmentUseStaticWebAssetsExtension));
-
-            var files = Directory.GetFiles(assemblyDirectory);
-            foreach (var file in files)
-            {
-                log.Information("Found file: {FileName}", file);
-            }
-
-            if (!File.Exists(manifestPath))
-            {
-                throw new InvalidOperationException($"Unable to find StaticWebAssets.json at: {manifestPath}");
-            }
-
-            using var manifest = File.OpenRead(manifestPath);
-
-            #pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
-            var useStaticWebAssetsCoreMethod = typeof(StaticWebAssetsLoader).GetMethod("UseStaticWebAssetsCore", BindingFlags.NonPublic | BindingFlags.Static);
-            #pragma warning restore S3011
-
-            useStaticWebAssetsCoreMethod!.Invoke(null, new object[] { environment, manifest });
-            return environment;
         }
     }
 }
