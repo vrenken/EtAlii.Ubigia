0fe422d0892f79285b01e1531cd4b6ea419b4a7c
Peter Vrenken <peter.vrenken@atos.net>
2022-05-20 14:54:23 +0200

    Disabled unused code.

--- Source/Frameworks/EtAlii.xTechnology.Hosting/EtAlii.xTechnology.Hosting/HostStringBuilder.cs
+++ Source/Frameworks/EtAlii.xTechnology.Hosting/EtAlii.xTechnology.Hosting/HostStringBuilder.cs
@@ -1,43 +1,43 @@
-﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
-
-namespace EtAlii.xTechnology.Hosting
-{
-    using System;
-    using System.Net;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.Extensions.Configuration;
-
-    public class HostStringBuilder
-    {
-        public HostString Build(IConfigurationSection configuration, IPAddress fallbackAddress)
-        {
-            // Port magic.
-            var port = configuration.GetValue("Port", -1);
-
-            // Host magic.
-            var ipAddressString = configuration.GetValue<string>("IpAddress", null);
-            string host = null;
-            if (!string.IsNullOrWhiteSpace(ipAddressString))
-            {
-                if (IPAddress.TryParse(ipAddressString, out var ipAddress))
-                {
-                    host = ipAddress.ToString();
-                }
-                else
-                {
-                    throw new InvalidOperationException($"Unable to parse {GetType().Name} IP address: {ipAddressString}");
-                }
-            }
-
-            host = port != -1 && host != null
-                ? host
-                : fallbackAddress.ToString();
-
-            var hostString = port != -1
-                ? new HostString(host, port)
-                : new HostString(host);
-            return hostString;
-
-        }
-    }
-}
+﻿// // Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+//
+// namespace EtAlii.xTechnology.Hosting
+// [
+//     using System
+//     using System.Net
+//     using Microsoft.AspNetCore.Http
+//     using Microsoft.Extensions.Configuration
+//
+//     public class HostStringBuilder
+//     [
+//         public HostString Build(IConfigurationSection configuration, IPAddress fallbackAddress)
+//         [
+//             // Port magic.
+//             var port = configuration.GetValue("Port", -1)
+//
+//             // Host magic.
+//             var ipAddressString = configuration.GetValue<string>("IpAddress", null)
+//             string host = null
+//             if (!string.IsNullOrWhiteSpace(ipAddressString))
+//             [
+//                 if (IPAddress.TryParse(ipAddressString, out var ipAddress))
+//                 [
+//                     host = ipAddress.ToString()
+//                 ]
+//                 else
+//                 [
+//                     throw new InvalidOperationException($"Unable to parse [GetType().Name] IP address: [ipAddressString]")
+//                 ]
+//             ]
+//
+//             host = port != -1 && host != null
+//                 ? host
+//                 : fallbackAddress.ToString()
+//
+//             var hostString = port != -1
+//                 ? new HostString(host, port)
+//                 : new HostString(host)
+//             return hostString
+//
+//         ]
+//     ]
+// ]
--- Source/Frameworks/EtAlii.xTechnology.Hosting/EtAlii.xTechnology.Hosting/PathStringBuilder.cs
+++ Source/Frameworks/EtAlii.xTechnology.Hosting/EtAlii.xTechnology.Hosting/PathStringBuilder.cs
@@ -1,17 +1,17 @@
-// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
-
-namespace EtAlii.xTechnology.Hosting
-{
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.Extensions.Configuration;
-
-    public class PathStringBuilder
-    {
-        public PathString Build(IConfigurationSection configuration)
-        {
-            var path = configuration.GetValue("Path", string.Empty);
-            var pathString = new PathString(path);
-            return pathString;
-        }
-    }
-}
+// // Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+//
+// namespace EtAlii.xTechnology.Hosting
+// [
+//     using Microsoft.AspNetCore.Http
+//     using Microsoft.Extensions.Configuration
+//
+//     public class PathStringBuilder
+//     [
+//         public PathString Build(IConfigurationSection configuration)
+//         [
+//             var path = configuration.GetValue("Path", string.Empty)
+//             var pathString = new PathString(path)
+//             return pathString
+//         ]
+//     ]
+// ]
