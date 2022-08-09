5e413cfdb4d567d4e49a1264c039dad1d60385ba
Peter Vrenken <peter.vrenken@atos.net>
2022-08-09 04:02:38 +0200

    Added two new root transport API tests.

---
+++ Source/Api/EtAlii.Ubigia.Api.Transport.Tests/Roots/IntegrationTests/Roots.Tests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Api.Transport.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using EtAlii.Ubigia.Tests;
+    using Xunit;
+
+    [CorrelateUnitTests]
+    public class RootsTests : IClassFixture<TransportUnitTestContext>
+    {
+        private readonly TransportUnitTestContext _testContext;
+
+        public RootsTests(TransportUnitTestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [Fact]
+        public async Task Root_Add_By_Name()
+        {
+            // Arrange.
+            var (connection, _) = await _testContext.TransportTestContext
+                .CreateDataConnectionToExistingSpace(_testContext.TransportTestContext.Host.SystemAccountName, _testContext.TransportTestContext.Host.SystemAccountPassword, SpaceName.System, false)
+                .ConfigureAwait(false);
+            await connection.Open().ConfigureAwait(false);
+            var name = "TestRoot";
+            // Act.
+            var root = await connection.Roots.Data.Add(name).ConfigureAwait(false);
+
+            // Assert.
+            Assert.NotNull(root);
+            Assert.NotEqual(Guid.Empty,root.Id);
+            Assert.Equal(name,root.Name);
+        }
+
+        [Fact]
+        public async Task Root_Add_By_Name_Empty()
+        {
+            // Arrange.
+            var (connection, _) = await _testContext.TransportTestContext
+                .CreateDataConnectionToExistingSpace(_testContext.TransportTestContext.Host.SystemAccountName, _testContext.TransportTestContext.Host.SystemAccountPassword, SpaceName.System, false)
+                .ConfigureAwait(false);
+            await connection.Open().ConfigureAwait(false);
+
+            // Act.
+            var act = new Func<Task>(async () => await connection.Roots.Data.Add(null).ConfigureAwait(false));
+
+            // Assert.
+            await Assert.ThrowsAsync<InvalidInfrastructureOperationException>(act).ConfigureAwait(false);
+        }
+    }
+}
