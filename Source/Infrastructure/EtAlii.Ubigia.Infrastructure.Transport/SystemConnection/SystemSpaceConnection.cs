e285467c102fcd1715799a83e739866d531eb8a9
Peter Vrenken <peter.vrenken@atos.net>
2022-08-17 22:41:49 +0200

    Moved SystemSpaceConnection and SystemStorageConnection in the right folder.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Transport/SystemConnection/SystemSpaceConnection.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
+
+namespace EtAlii.Ubigia.Infrastructure.Transport
+{
+    using EtAlii.Ubigia.Api.Transport;
+
+    internal class SystemSpaceConnection : SpaceConnection<SystemSpaceTransport>, ISystemSpaceConnection
+    {
+        public SystemSpaceConnection(
+            ISpaceTransport transport,
+            SpaceConnectionOptions options,
+            IRootContext roots,
+            IEntryContext entries,
+            IContentContext content,
+            IPropertiesContext properties,
+            IAuthenticationContext authentication)
+            : base(transport, options, roots, entries, content, properties, authentication)
+        {
+        }
+    }
+}
