1fdb4b1700e238d9025820d39a47b1e6b9fbf2ec
Peter Vrenken <peter.vrenken@atos.net>
2022-08-22 05:50:02 +0200

    Added Setup-Portal

--- Source/Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting/InfrastructureHostServicesFactory.cs
+++ Source/Infrastructure/Hosting/EtAlii.Ubigia.Infrastructure.Hosting/InfrastructureHostServicesFactory.cs
@@ -12,17 +12,26 @@ namespace EtAlii.Ubigia.Infrastructure.Hosting
         {
             var services = new List<IService>();
 
+            // Core internal services.
             TryAddService(services, configurationRoot, "Storage");
             TryAddService(services, configurationRoot, "Infrastructure");
+
+            // Core user network services.
             TryAddService(services, configurationRoot, "User-Api-Grpc");
             TryAddService(services, configurationRoot, "User-Api-SignalR");
             TryAddService(services, configurationRoot, "User-Api-Rest");
+
+            // Core management network services.
             TryAddService(services, configurationRoot, "Management-Portal");
             TryAddService(services, configurationRoot, "Management-Api-Grpc");
             TryAddService(services, configurationRoot, "Management-Api-SignalR");
             TryAddService(services, configurationRoot, "Management-Api-Rest");
 
+            // Setup network service (will only be enabled when the basic system settings are missing).
+            TryAddService(services, configurationRoot, "Setup-Portal");
+
             return services.ToArray();
         }
+
     }
 }
