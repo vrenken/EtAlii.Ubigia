2cfd326b34fc4c699510e28bd9f07b8fc76a3b47
Peter Vrenken <github@vrenken.eu>
2022-10-04 11:36:26 +0200

    Small layout improvement.

--- Source/Persistence/EtAlii.Ubigia.Persistence/Items/IItemStorage.cs
+++ Source/Persistence/EtAlii.Ubigia.Persistence/Items/IItemStorage.cs
@@ -7,8 +7,10 @@ namespace EtAlii.Ubigia.Persistence
 
     public interface IItemStorage
     {
-        void Store<T>(T item, Guid id, ContainerIdentifier container) where T : class;
-        Task<T> Retrieve<T>(Guid id, ContainerIdentifier container) where T : class;
+        void Store<T>(T item, Guid id, ContainerIdentifier container)
+            where T : class;
+        Task<T> Retrieve<T>(Guid id, ContainerIdentifier container)
+            where T : class;
         void Remove(Guid id, ContainerIdentifier container);
         Guid[] Get(ContainerIdentifier container);
         bool Has(Guid id, ContainerIdentifier container);
