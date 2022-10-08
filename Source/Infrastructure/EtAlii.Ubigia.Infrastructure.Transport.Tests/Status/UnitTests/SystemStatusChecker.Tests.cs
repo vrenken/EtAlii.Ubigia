1284fee87dba4ad098ddf6da948da88671923f7f
Peter Vrenken <github@vrenken.eu>
2022-10-08 16:18:04 +0200

    Added first SystemStatusChecker test.

---
+++ Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Transport.Tests/Status/UnitTests/SystemStatusChecker.Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace EtAlii.Ubigia.Infrastructure.Transport.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using EtAlii.xTechnology.Hosting;
+    using Xunit;
+
+    public class SystemStatusCheckerTests
+    {
+        [Fact]
+        public void SystemStatusChecker_Create()
+        {
+            // Arrange.
+
+            // Act.
+            var systemStatusChecker = new SystemStatusChecker();
+
+            // Assert.
+            Assert.NotNull(systemStatusChecker);
+        }
+
+        [Fact]
+        public void SystemStatusChecker_DetermineIfSystemIsOperational()
+        {
+            // Arrange.
+            var systemStatusChecker = new SystemStatusChecker();
+
+            // Act.
+            var act = new Action(() => systemStatusChecker.DetermineIfSystemIsOperational(new List<IService>(), null));
+
+            // Assert.
+            Assert.Throws<InvalidOperationException>(act);
+        }
+    }
+}
