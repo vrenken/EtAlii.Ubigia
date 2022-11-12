3d2cb6b1843acc62d7c8439236bb0cbb64a2ce7d
Peter Vrenken <github@vrenken.eu>
2022-11-12 20:05:23 +0100

    Updated .NET CLR version 7.0.100 + NuGet packages.

--- Source/Api/EtAlii.Ubigia/IsExternalInit.cs
+++ Source/Api/EtAlii.Ubigia/IsExternalInit.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
 
-#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETCOREAPP3_0 || NETCOREAPP3_1 || NET45 || NET451 || NET452 || NET6 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
+#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETCOREAPP3_0 || NETCOREAPP3_1 || NET45 || NET451 || NET452 || NET6 || NET7 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
 
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
