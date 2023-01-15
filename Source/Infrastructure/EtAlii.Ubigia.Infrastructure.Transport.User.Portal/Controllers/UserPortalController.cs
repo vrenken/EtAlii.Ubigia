a6a2120a634b4130041b7f2ebd83281b47140a36
Peter Vrenken <github@vrenken.eu>
2023-01-15 01:48:23 +0100

    Removed old user portal.

--- Source/Infrastructure/EtAlii.Ubigia.Infrastructure.Transport.User.Portal/Controllers/UserPortalController.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Ubigia
-
-namespace EtAlii.Ubigia.Infrastructure.Transport.User.Portal;
-
-using Microsoft.AspNetCore.Mvc;
-
-public abstract class UserPortalController : Controller
-{
-}
