303caa6b73f48d9324d8a5488450378d2099274e
Peter Vrenken <github@vrenken.eu>
2023-01-23 23:58:56 +0100

    Moved part of the DI constructs closer to the default Microsoft ServiceCollection methodology.

--- Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/_New/Container.Decorators.cs
+++ Source/Frameworks/EtAlii.xTechnology.MicroContainer/EtAlii.xTechnology.MicroContainer/_New/Container.Decorators.cs
@@ -4,9 +4,6 @@
 
 namespace EtAlii.xTechnology.MicroContainer
 {
-    using System;
-    using System.Linq;
-    using System.Reflection;
     using Microsoft.Extensions.DependencyInjection;
 
     /// <summary>
@@ -18,75 +15,9 @@ namespace EtAlii.xTechnology.MicroContainer
         /// <inheritdoc />
         public void RegisterDecorator<TService, TDecorator>()
             where TService : class
-            where TDecorator : TService
+            where TDecorator : class, TService
         {
-            var serviceType = typeof(TService);
-            var decoratorType = typeof(TDecorator);
-#if DEBUG
-            if (!serviceType.GetTypeInfo().IsInterface)
-            {
-                throw new InvalidOperationException($"Service Type should be an interface: {serviceType}");
-            }
-#endif
-            var oldDescriptor = _collection.SingleOrDefault(service => service.ServiceType == serviceType);
-            if (oldDescriptor == null)
-            {
-                throw new InvalidOperationException($"Service Type decorator could not be registered: {decoratorType}");
-            }
-
-            var index = _collection.IndexOf(oldDescriptor);
-
-            var newFactory = new Func<IServiceProvider, object>(provider =>
-            {
-                var decoree = GetInstance(provider, oldDescriptor);
-                var decorator = CreateDecorator(provider, decoratorType, decoree, serviceType);
-                return decorator;
-            });
-
-            _collection[index] = ServiceDescriptor.Describe(serviceType, newFactory, ServiceLifetime.Singleton);
-        }
-
-        private object CreateDecorator(IServiceProvider provider, Type decoratorType, object decoree, Type serviceType)
-        {
-            var constructors = decoratorType.GetTypeInfo().DeclaredConstructors
-                .Where(c => !c.IsStatic && c.IsPublic)
-                .ToArray();
-
-            if (constructors.Length > 1)
-            {
-                throw new InvalidOperationException("Multiple public constructors are not allowed for type: " + decoratorType);
-            }
-            if (constructors.Length == 0)
-            {
-                throw new InvalidOperationException("No constructors found for type: " + decoratorType);
-            }
-
-            var constructor = constructors[0];
-            var parameters = constructor.GetParameters();
-
-            var instances = new object[parameters.Length];
-
-            var parameterIndex = 0;
-            try
-            {
-                for (parameterIndex = 0; parameterIndex < parameters.Length; parameterIndex++)
-                {
-                    var parameterType = parameters[parameterIndex].ParameterType;
-                    if (parameterType == serviceType)
-                    {
-                        instances[parameterIndex] = decoree;
-                    }
-                    else
-                    {
-                        instances[parameterIndex] = ActivatorUtilities.GetServiceOrCreateInstance(provider, parameterType);
-                    }
-                }
-                return constructor.Invoke(instances);
-            }
-            catch (Exception e)
-            {
-                throw new InvalidOperationException($"Unable to create instance of type: {decoratorType} due to issues with parameter: {parameterIndex}", e);
-            }
+            _collection.RegisterDecorator<TService, TDecorator>();
         }
     }
 }
